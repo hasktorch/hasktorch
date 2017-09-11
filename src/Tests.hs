86dafa5f65c4774855477d6b9140755e03a5af18
austinvhuang <austinh@alum.mit.edu>
2017-09-11 05:44:09 -0400

    add more tests + initial circleci script

--- src/Tests.hs
+++ src/Tests.hs
@@ -14,16 +14,37 @@ import Test.Hspec
 
 tests = do
   hspec $ do
-    describe "Torch" $ do
+    describe "Tensor creation and access methods" $ do
       it "initializes empty tensor with 0 dimension" $ do
-        t0 <- c_THFloatTensor_new
-        TR.c_THFloatTensor_nDimension t0 `shouldBe` 0
-      it "initializes tensor with 1 dimension" $ do
-        t1 <- c_THFloatTensor_newWithSize1d 10
-        TR.c_THFloatTensor_nDimension t1 `shouldBe` 1
-      it "initializes 1D tensor with correct size" $ do
-        t1 <- TR.c_THFloatTensor_newWithSize1d 10
-        (TR.c_THFloatTensor_size t1 0) `shouldBe` 10
+        t <- c_THFloatTensor_new
+        TR.c_THFloatTensor_nDimension t `shouldBe` 0
+        c_THFloatTensor_free t
+      it "1D tensor has correct dimensions and sizes" $ do
+        t <- TR.c_THFloatTensor_newWithSize1d 10
+        TR.c_THFloatTensor_nDimension t `shouldBe` 1
+        TR.c_THFloatTensor_size t 0 `shouldBe` 10
+        c_THFloatTensor_free t
+      it "2D tensor has correct dimensions and sizes" $ do
+        t <- c_THFloatTensor_newWithSize2d 10 25
+        TR.c_THFloatTensor_nDimension t `shouldBe` 2
+        TR.c_THFloatTensor_size t 0 `shouldBe` 10
+        TR.c_THFloatTensor_size t 1 `shouldBe` 25
+        c_THFloatTensor_free t
+      it "3D tensor has correct dimensions and sizes" $ do
+        t <- c_THFloatTensor_newWithSize3d 10 25 5
+        TR.c_THFloatTensor_nDimension t `shouldBe` 3
+        TR.c_THFloatTensor_size t 0 `shouldBe` 10
+        TR.c_THFloatTensor_size t 1 `shouldBe` 25
+        TR.c_THFloatTensor_size t 2 `shouldBe` 5
+        c_THFloatTensor_free t
+      it "4D tensor has correct dimensions and sizes" $ do
+        t <- c_THFloatTensor_newWithSize4d 10 25 5 62
+        TR.c_THFloatTensor_nDimension t `shouldBe` 4
+        TR.c_THFloatTensor_size t 0 `shouldBe` 10
+        TR.c_THFloatTensor_size t 1 `shouldBe` 25
+        TR.c_THFloatTensor_size t 2 `shouldBe` 5
+        TR.c_THFloatTensor_size t 3 `shouldBe` 62
+        c_THFloatTensor_free t
 
 main = do
   tests
