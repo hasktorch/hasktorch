addf4fd51bac128bcb83d8bdce66e4da18b3bf86
austinvhuang <austinh@alum.mit.edu>
2017-09-12 20:58:27 -0400

    augment torch dependency build script for CI

--- vendor/buildTH.sh
+++ vendor/buildTH.sh
@@ -1,5 +1,31 @@
 #!/bin/bash -eu
 
 mkdir -p ./build
-rm -rf build/*
-cd ./build; cmake ../TH/CMakeLists.txt -B. -DCMAKE_CXX_COMPILER=g++-6 -DCMAKE_CC_COMPILER=gcc-6 -DCXX=g++-6 -DCC=gcc-6; make
+# rm -rf build/*
+
+if [ "$(uname)" == "Darwin" ]; then
+    if ! [ -x "$(command -v gcc-6)" ]; then
+        echo 'Error: gcc-6 is not installed, use homebrew to install it.' >&2
+        exit 1
+    fi
+    echo "Running as OSX ..."
+    CXX=g++-6
+    CC=gcc-6
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    if ! [ -x "$(command -v gcc)" ]; then
+        echo 'Error: gcc is not installed.' >&2
+        exit 1
+    fi
+    echo "Running as Linux ..."
+    CXX=g++
+    CC=gcc
+else
+    echo "Unknown OS"
+    exit
+fi
+
+echo "Compilers:"
+echo "  $CXX"
+echo "  $CC"
+
+cd ./build; cmake ../TH/CMakeLists.txt -B. -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CC_COMPILER=$CC -DCXX=$CXX -DCC=$CC; make
