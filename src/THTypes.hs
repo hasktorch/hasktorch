b20c337141d13a7699b2a1813736add18eab4be7
austinvhuang <austinh@alum.mit.edu>
2017-09-14 08:09:57 -0400

    fix and add to C Types

--- src/THTypes.hs
+++ src/THTypes.hs
@@ -14,32 +14,53 @@ type CTHDescBuff = Ptr ()
 type CTHByteTensor = ()      -- THTensor / THTensor.h
 type CTHByteStorage = ()     -- THStorage / THStorag
 type CTHByteLongStorage = () -- THLongStorage / THStorage.h
--- TODO : determine appropriate type for these:
 type CTHBytePtrDiff = CInt     -- ptrdiff_t / THStorage.h
 
+{- Char -}
 
-
-{- Float -}
-
-type CTHFloatTensor = ()      -- THTensor / THTensor.h
-type CTHFloatStorage = ()     -- THStorage / THStorag
-type CTHFloatLongStorage = () -- THLongStorage / THStorage.h
--- TODO : determine appropriate type for these:
-type CTHFloatPtrDiff = CInt     -- ptrdiff_t / THStorage.h
+type CTHCharTensor = ()      -- THTensor / THTensor.h
+type CTHCharStorage = ()     -- THStorage / THStorag
+type CTHCharLongStorage = () -- THLongStorage / THStorage.h
+type CTHCharPtrDiff = CInt     -- ptrdiff_t / THStorage.h
 
 {- Double -}
 
 type CTHDoubleTensor = ()      -- THTensor / THTensor.h
 type CTHDoubleStorage = ()     -- THStorage / THStorag
 type CTHDoubleLongStorage = () -- THLongStorage / THStorage.h
--- TODO : determine appropriate type for these:
 type CTHDoublePtrDiff = CInt     -- ptrdiff_t / THStorage.h
 
-type CTHDoubleTensor = ()      -- THTensor / THTensor.h
-type CTHDoubleStorage = ()     -- THStorage / THStorag
-type CTHDoubleLongStorage = () -- THLongStorage / THStorage.h
--- TODO : determine appropriate type for these:
-type CTHDoublePtrDiff = CInt     -- ptrdiff_t / THStorage.h
+{- Float -}
 
+type CTHFloatTensor = ()      -- THTensor / THTensor.h
+type CTHFloatStorage = ()     -- THStorage / THStorag
+type CTHFloatLongStorage = () -- THLongStorage / THStorage.h
+type CTHFloatPtrDiff = CInt     -- ptrdiff_t / THStorage.h
 
+{- Half -}
 
+type CTHHalfTensor = ()      -- THTensor / THTensor.h
+type CTHHalfStorage = ()     -- THStorage / THStorag
+type CTHHalfLongStorage = () -- THLongStorage / THStorage.h
+type CTHHalfPtrDiff = CInt     -- ptrdiff_t / THStorage.h
+
+{- Int -}
+
+type CTHIntTensor = ()      -- THTensor / THTensor.h
+type CTHIntStorage = ()     -- THStorage / THStorag
+type CTHIntLongStorage = () -- THLongStorage / THStorage.h
+type CTHIntPtrDiff = CInt     -- ptrdiff_t / THStorage.h
+
+{- Long -}
+
+type CTHLongTensor = ()      -- THTensor / THTensor.h
+type CTHLongStorage = ()     -- THStorage / THStorag
+type CTHLongLongStorage = () -- THLongStorage / THStorage.h
+type CTHLongPtrDiff = CInt     -- ptrdiff_t / THStorage.h
+
+{- Short -}
+
+type CTHShortTensor = ()      -- THTensor / THTensor.h
+type CTHShortStorage = ()     -- THStorage / THStorag
+type CTHShortLongStorage = () -- THLongStorage / THStorage.h
+type CTHShortPtrDiff = CInt     -- ptrdiff_t / THStorage.h
