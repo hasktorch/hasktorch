6d25aaa9995f88e8208b6cfb1669f1ffac3fc70f
austinvhuang <austinh@alum.mit.edu>
2017-09-24 20:49:54 -0400

    split tests, add stub for testing raw interface types

---
+++ tests/src/TestRawInterface.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+module TestRawInterface (testRawInterface) where
+
+import THDoubleTensor as TR
+import THDoubleTensorMath as TR
+import THTypes
+import TorchStructs
+
+import Foreign.C.Types
+import Test.Hspec
+
+testRawInterface = do
+  putStrLn "Raw Types"
+  putStrLn "Done"
+
