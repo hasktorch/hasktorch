6f25e4ccfd10f93d38c6b18eb65969467b355282
austinvhuang <austinh@alum.mit.edu>
2017-11-09 04:10:02 -0500

    minimal mnist downloader

--- examples/src/DownloadMNIST.hs
+++ examples/src/DownloadMNIST.hs
@@ -1,4 +1,37 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Data.Maybe (fromMaybe)
+import qualified Data.ByteString.Lazy as B
+import qualified Network.Browser as BR
+import Network.HTTP (rspCode, rspBody)
+import Network.URI (parseURI)
+
+downloadSite = "http://yann.lecun.com/exdb/mnist/"
+
+download :: FilePath -> String -> IO ()
+download outDir downloadFile = do
+  let outFile = outDir ++ downloadFile
+  let url = downloadSite ++ downloadFile
+  let uri = fromMaybe (error ("Invalid URI " ++ url))
+            (parseURI url)
+  (_, result) <- BR.browse $ do
+    BR.setMaxErrorRetries $ Just 3
+    BR.setAllowRedirects True
+    BR.request $ BR.defaultGETRequest_ uri
+  case (rspCode result) of
+    (2, 0, 0) -> B.writeFile outFile (rspBody result)
+    s -> error ( "Error: " ++ show s ++ " " ++ url) 
+
+run :: FilePath -> IO ()
+run dir = mapM_ (download dir) [
+  "train-labels-idx1-ubyte.gz",
+  "train-images-idx3-ubyte.gz",
+  "t10k-labels-idx1-ubyte.gz",
+  "t10k-images-idx3-ubyte.gz"
+  ]
+
 main = do
-  putStrLn "Done"
+  run "./"
+  putStrLn "Finished downloading"
