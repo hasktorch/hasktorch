0ef349e8d72007ae7cffca21acf254e620ea5e0a
austinvhuang <austinh@alum.mit.edu>
2017-11-12 15:37:40 -0500

    simplify layer representation by getting rid of Shape Kind in favor of type-level lists that mirror the tensor dimension specs

--- nn/src/Layer.hs
+++ nn/src/Layer.hs
@@ -17,66 +17,14 @@ import TensorUtils
 
 import Data.Singletons
 import Data.Singletons.Prelude
+import Data.Singletons.Prelude.List
 import Data.Singletons.TypeLits
 
--- datakinds lifts constructors to type level
-data Shape = L1 Nat | L2 Nat Nat | L3 Nat Nat Nat | L4 Nat Nat Nat Nat
+data S (d :: [Nat]) where
+  S :: TDS d -> S d
 
--- Define shape types
-data S (shapeType :: Shape) where
-  S1L :: (KnownNat o)                         => TDS '[o]          -> S ('L1 o)
-  S2L :: (KnownNat r, KnownNat c)             => TDS '[r, c]       -> S ('L2 r c)
-  S3L :: (KnownNat x, KnownNat y, KnownNat z) => TDS '[x, y, z]    -> S ('L3 x y z)
-  S4L :: (KnownNat a, KnownNat b,
-          KnownNat c, KnownNat d)             => TDS '[a, b, c, d] -> S ('L4 a b c d)
-
--- singleton machinery to apply a tensor transformation function to a layer
-
--- l1apply :: (KnownNat o) => (TDS '[o] -> TDS '[o]) -> S ('L1 o) -> S ('L1 o)
-l1apply :: (KnownNat o) => (TDS '[o] -> TDS '[o]) -> S ('L1 o) -> S ('L1 o)
-l1apply f (S1L x) = S1L (f x)
-
-l2apply :: (KnownNat r, KnownNat c) =>
-  (TDS '[r, c] -> TDS '[r, c]) -> S ('L2 r c) -> S ('L2 r c)
-l2apply f (S2L x) = S2L (f x)
-
-l3apply :: (KnownNat x, KnownNat y, KnownNat z) =>
-  (TDS '[x, y, z] -> TDS '[x, y, z]) -> S ('L3 x y z) -> S ('L3 x y z)
-l3apply f (S3L x) = S3L (f x)
-
-l4apply :: (KnownNat a, KnownNat b, KnownNat c, KnownNat d) =>
-  (TDS '[a, b, c, d] -> TDS '[a, b, c, d]) -> S ('L4 a b c d) -> S ('L4 a b c d)
-l4apply f (S4L x) = S4L (f x)
-
-test :: forall l2 ltype . Sing l2 -> S ltype -> S ltype
-test = undefined
-
-lapply :: forall d l . (SingI l) => (TDS d -> TDS d) -> S l -> S l
-lapply f layer = go (sing :: Sing l) layer
-  where
-    go :: forall l . SingI l => (Sing l) -> S l -> S l
-    -- TODO need proofs to specialize dimensions used by f
-    go L1Sing (S1L x) = l1apply undefined (S1L x)
-    go L2Sing (S2L x) = l2apply undefined (S2L x)
-    go L3Sing (S3L x) = l3apply undefined (S3L x)
-    go L4Sing (S4L x) = l4apply undefined (S4L x)
-
--- define singleton constructors: take type inputs, return value of type Sing [Shape]
-data instance Sing (shapeType :: Shape) where
-  L1Sing :: KnownNat a                                       => Sing ('L1 a)
-  L2Sing :: (KnownNat a, KnownNat b)                         => Sing ('L2 a b)
-  L3Sing :: (KnownNat a, KnownNat b, KnownNat c)             => Sing ('L3 a b c)
-  L4Sing :: (KnownNat a, KnownNat b, KnownNat c, KnownNat d) => Sing ('L4 a b c d)
-  
--- derive value-level singleton from layer shape types (L1...L4) via sing
-instance KnownNat a => SingI ('L1 a) where
-  sing = L1Sing
-instance (KnownNat a, KnownNat b) => SingI ('L2 a b) where
-  sing = L2Sing
-instance (KnownNat a, KnownNat b, KnownNat c) => SingI ('L3 a b c) where
-  sing = L3Sing
-instance (KnownNat a, KnownNat b, KnownNat c, KnownNat d) => SingI ('L4 a b c d) where
-  sing = L4Sing
+lmap :: forall d . (SingI d) => (TDS d -> TDS d) -> S d -> S d
+lmap f (S t) = S (f t)
 
 -- typeclass 1: layers can be updated and randomly instantiated
 class UpdateLayer layer where
@@ -85,7 +33,7 @@ class UpdateLayer layer where
   createRandom :: IO layer
 
 -- typeclass 2: layers can be run forward and backward propagated
-class UpdateLayer x => Layer x (i :: Shape) (o :: Shape) where
+class UpdateLayer x => Layer x (i :: [Nat]) (o :: [Nat]) where
   type Tape x i o :: *
   runForwards :: x -> S i -> (Tape x i o, S o)
   runBackwards :: x -> Tape x i o -> S o -> (Gradient x, S i)
@@ -98,7 +46,7 @@ data LearningParameters = LearningParameters {
   } deriving (Eq, Show)
 
 -- a network is a list of layers
-data Network :: [*] -> [Shape] -> * where
+data Network :: [*] -> [[Nat]] -> * where
   NNil :: SingI i => Network '[] '[i]
   NCons :: (SingI i, SingI h, Layer x i h) =>
     x -> (Network xs (h : hs)) -> Network (x : xs) (i : h : hs)
@@ -124,11 +72,11 @@ instance (a ~ b, SingI a) => Layer Logit a b where
   -- runForwards _ a = (a, logistic a)
   -- runBackwards _ a g = ((), logistic' a * g)
 
--- logistic :: Floating a => a -> a
--- logistic x = 1 / (1 + exp (-x))
--- logistic' x = (logistic x) * (1 - (logistic x))
+-- -- logistic :: Floating a => a -> a
+-- -- logistic x = 1 / (1 + exp (-x))
+-- -- logistic' x = (logistic x) * (1 - (logistic x))
 
-{- Tanh -}
+-- {- Tanh -}
 
 data Tanh = Tanh
   deriving Show
