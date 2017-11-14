cb3361b91bc7c6c191ed1b7bf451a61b27f4cfaf
stites <sam@stites.io>
2017-11-14 17:29:47 -0500

    include TensorRaw tests

--- core/src/TensorRaw.hs
+++ core/src/TensorRaw.hs
@@ -1,35 +1,68 @@
+{-# LANGUAGE LambdaCase #-}
 module TensorRaw (
+  tmap,
   dimFromRaw,
   dispRaw,
   fillRaw,
   fillRaw0,
   randInitRaw,
-  randInitRawTest,
   tensorRaw,
+  invlogit,
 
   TensorDoubleRaw,
   TensorLongRaw
   ) where
 
 import Data.Maybe (fromJust)
+import Debug.Trace
 
-import Foreign
-import Foreign.C.Types
-import Foreign.Ptr
-import Foreign.ForeignPtr( ForeignPtr, withForeignPtr, mallocForeignPtrArray,
-                           newForeignPtr )
+import Foreign (Ptr)
+import Foreign.C.Types (CLong, CDouble, CInt)
+import Foreign.ForeignPtr (ForeignPtr, withForeignPtr, mallocForeignPtrArray, newForeignPtr)
 import GHC.Ptr (FunPtr)
 import Numeric (showGFloat)
 import System.IO.Unsafe (unsafePerformIO)
+import Control.Monad (forM_)
 
-import Torch.Core.Internal (w2cl)
-import THTypes
-import THDoubleTensor
-import THDoubleTensorMath
-import THDoubleTensorRandom
-import THRandom
+import Torch.Core.Internal (w2cl, onDims)
+import THTypes (CTHDoubleTensor, CTHGenerator)
+import TensorTypes (TensorDim(..), TensorDoubleRaw, TensorLongRaw, TIdx(..), (^.), _1, _2, _3, _4)
 
-import TensorTypes
+import qualified THDoubleTensor as T
+import qualified THDoubleTensorMath as M (c_THDoubleTensor_sigmoid, c_THDoubleTensor_fill)
+import qualified THDoubleTensorRandom as R (c_THDoubleTensor_uniform)
+import qualified THRandom as R (c_THGenerator_new)
+
+
+-- | Map across a C TH Tensor -- this _could_ be rolled into Data.Foldable.toList
+tmap :: (CDouble -> b) -> Ptr CTHDoubleTensor -> [b]
+tmap f tensor =
+  case length size of
+    0 -> mempty
+    1 -> fmap (\t -> f (T.c_THDoubleTensor_get1d tensor (t ^. _1))) indexes
+    2 -> fmap (\t -> f (T.c_THDoubleTensor_get2d tensor (t ^. _1) (t ^. _2))) indexes
+    3 -> fmap (\t -> f (T.c_THDoubleTensor_get3d tensor (t ^. _1) (t ^. _2) (t ^. _3))) indexes
+    4 -> fmap (\t -> f (T.c_THDoubleTensor_get4d tensor (t ^. _1) (t ^. _2) (t ^. _3) (t ^. _4))) indexes
+    _ -> undefined
+  where
+    size :: [Int]
+    size = fmap (fromIntegral . T.c_THDoubleTensor_size tensor) [0 .. T.c_THDoubleTensor_nDimension tensor - 1]
+
+    indexes :: [TIdx CLong]
+    indexes = idx size
+
+    range :: Integral i => Int -> [i]
+    range mx = [0 .. fromIntegral mx - 1]
+
+    idx :: [Int] -> [TIdx CLong]
+    idx = \case
+      []               -> [I0]
+      [nx]             -> [I1 x | x <- range nx ]
+      [nx, ny]         -> [I2 x y | x <- range nx, y <- range ny ]
+      [nx, ny, nz]     -> [I3 x y z | x <- range nx, y <- range ny, z <- range nz]
+      [nx, ny, nz, nq] -> [I4 x y z q | x <- range nx, y <- range ny, z <- range nz, q <- range nq]
+      _ -> error "should not be run"
+
 
 -- |displaying raw tensor values
 dispRaw :: Ptr CTHDoubleTensor -> IO ()
@@ -37,12 +70,8 @@ dispRaw tensor
   | (length sz) == 0 = putStrLn "Empty Tensor"
   | (length sz) == 1 = do
       putStrLn ""
-      let indexes = [ fromIntegral idx :: CLong
-                    | idx <- [0..(sz !! 0 - 1)] ]
       putStr "[ "
-      mapM_ (\idx -> putStr $
-                     (showLim $ c_THDoubleTensor_get1d tensor idx) ++ " ")
-        indexes
+      forM_ indexes (\idx -> putStr $ (showLim $ T.c_THDoubleTensor_get1d tensor idx) ++ " ")
       putStrLn "]\n"
   | (length sz) == 2 = do
       putStrLn ""
@@ -51,7 +80,7 @@ dispRaw tensor
                   | r <- [0..(sz !! 0 - 1)], c <- [0..(sz !! 1 - 1)] ]
       putStr ("[ " :: String)
       mapM_ (\(r, c) -> do
-                let val = c_THDoubleTensor_get2d tensor r c
+                let val = T.c_THDoubleTensor_get2d tensor r c
                 if c == fromIntegral (sz !! 1) - 1
                   then do
                   putStrLn (((showLim val) ++ " ]") :: String)
@@ -63,26 +92,34 @@ dispRaw tensor
             ) pairs
   | otherwise = putStrLn "Can't print this yet."
   where
-    --size :: (Ptr CTHDoubleTensor) -> [Int]
+    sz :: [Int]
+    sz = size tensor
+
+    indexes :: [CLong]
+    indexes = [ fromIntegral idx | idx <- [0..(sz !! 0 - 1)] ]
+
+    size :: Ptr CTHDoubleTensor -> [Int]
     size t =
       fmap f [0..maxdim]
       where
-        maxdim = (c_THDoubleTensor_nDimension t) - 1
-        f x = fromIntegral (c_THDoubleTensor_size t x) :: Int
-    -- showLim :: RealFloat a => a -> String
+        maxdim = (T.c_THDoubleTensor_nDimension t) - 1
+        f x = fromIntegral (T.c_THDoubleTensor_size t x) :: Int
+
+    showLim :: RealFloat a => a -> String
     showLim x = showGFloat (Just 2) x ""
-    sz = size tensor
 
 -- |randomly initialize a tensor with uniform random values from a range
 -- TODO - finish implementation to handle sizes correctly
+randInitRaw
+  :: Ptr CTHGenerator
+  -> TensorDim Word
+  -> CDouble
+  -> CDouble
+  -> IO TensorDoubleRaw
 randInitRaw gen dims lower upper = do
   t <- tensorRaw dims 0.0
-  c_THDoubleTensor_uniform t gen lower upper
+  R.c_THDoubleTensor_uniform t gen lower upper
   pure t
-
-randInitRawTest = do
-  gen <- c_THGenerator_new
-  mapM_ (\_ -> dispRaw =<< (randInitRaw gen (D2 (2, 2)) (-1.0) 3.0)) [0..10]
 
 
 -- |Create a new (double) tensor of specified dimensions and fill it with 0
@@ -93,62 +130,63 @@ tensorRaw dims value = do
   fillRaw value newPtr
   pure newPtr
   where
-    go D0 = c_THDoubleTensor_new
-    go (D1 d1) = c_THDoubleTensor_newWithSize1d $ w2cl d1
-    go (D2 (d1, d2)) = c_THDoubleTensor_newWithSize2d
-                       (w2cl d1) (w2cl d2)
-    go (D3 (d1, d2, d3)) = c_THDoubleTensor_newWithSize3d
-                           (w2cl d1) (w2cl d2) (w2cl d3)
-    go (D4 (d1, d2, d3, d4)) = c_THDoubleTensor_newWithSize4d
-                               (w2cl d1) (w2cl d2) (w2cl d3) (w2cl d4)
+    go :: TensorDim Word -> IO TensorDoubleRaw
+    go = onDims w2cl
+      T.c_THDoubleTensor_new
+      T.c_THDoubleTensor_newWithSize1d
+      T.c_THDoubleTensor_newWithSize2d
+      T.c_THDoubleTensor_newWithSize3d
+      T.c_THDoubleTensor_newWithSize4d
 
 
 -- |apply a tensor transforming function to a tensor
-applyRaw ::
-  (TensorDoubleRaw -> TensorDoubleRaw -> IO ())
-  -> TensorDoubleRaw -> IO TensorDoubleRaw
+applyRaw
+  :: (TensorDoubleRaw -> TensorDoubleRaw -> IO ())
+  -> TensorDoubleRaw
+  -> IO TensorDoubleRaw
 applyRaw f t1 = do
-  r_ <- c_THDoubleTensor_new
+  r_ <- T.c_THDoubleTensor_new
   f r_ t1
   pure r_
 
 -- |apply inverse logit to all values of a tensor
 invlogit :: TensorDoubleRaw -> IO TensorDoubleRaw
-invlogit = applyRaw c_THDoubleTensor_sigmoid
+invlogit = applyRaw M.c_THDoubleTensor_sigmoid
 
 -- |Dimensions of a raw tensor as a list
-sizeRaw :: (Ptr CTHDoubleTensor) -> [Int]
+sizeRaw :: Ptr CTHDoubleTensor -> [Int]
 sizeRaw t =
   fmap f [0..maxdim]
   where
-    maxdim = (c_THDoubleTensor_nDimension t) - 1
-    f x = fromIntegral (c_THDoubleTensor_size t x) :: Int
+    maxdim :: CInt
+    maxdim = (T.c_THDoubleTensor_nDimension t) - 1
+
+    f :: CInt -> Int
+    f x = fromIntegral (T.c_THDoubleTensor_size t x)
 
 -- |Dimensions of a raw tensor as a TensorDim value
 dimFromRaw :: TensorDoubleRaw -> TensorDim Word
 dimFromRaw raw =
-  case (length sz) of 0 -> D0
-                      1 -> D1 (getN 0)
-                      2 -> D2 ((getN 0), (getN 1))
-                      3 -> D3 ((getN 0), (getN 1), (getN 2))
-                      4 -> D4 ((getN 0), (getN 1), (getN 2), (getN 3))
-                      _ -> undefined -- TODO - make this safe
+  case length sz of
+    0 -> D0
+    1 -> D1  (getN 0)
+    2 -> D2 ((getN 0), (getN 1))
+    3 -> D3 ((getN 0), (getN 1), (getN 2))
+    4 -> D4 ((getN 0), (getN 1), (getN 2), (getN 3))
+    _ -> undefined -- TODO - make this safe
   where
+    sz :: [Int]
     sz = sizeRaw raw
+
+    getN :: Int -> Word
     getN n = fromIntegral (sz !! n)
 
 -- |Returns a function that accepts a tensor and fills it with specified value
 -- and returns the IO context with the mutated tensor
 fillRaw :: Real a => a -> TensorDoubleRaw -> IO ()
-fillRaw value = (flip c_THDoubleTensor_fill) (realToFrac value)
+fillRaw value = (flip M.c_THDoubleTensor_fill) (realToFrac value)
 
 -- |Fill a raw Double tensor with 0.0
-fillRaw0 :: TensorDoubleRaw -> IO (TensorDoubleRaw)
+fillRaw0 :: TensorDoubleRaw -> IO TensorDoubleRaw
 fillRaw0 tensor = fillRaw 0.0 tensor >> pure tensor
 
-testRaw = do
-  tmp <- tensorRaw (D1 5) 25.0
-  dispRaw tmp
-  t2 <- invlogit =<< tensorRaw (D1 5) 25.0
-  dispRaw t2
-  dispRaw tmp
