74e13e305d2d7a8ee82145388e4fc8843d4b4815
austinvhuang <austinh@alum.mit.edu>
2017-11-26 14:41:04 -0500

    add error handler build to Makefile, prepare aten module

---
+++ aten/README.md
@@ -0,0 +1,1 @@
+# Experimental aten bindings
---
+++ aten/src/ProcessSpec.hs
@@ -0,0 +1,10 @@
+module ProcessSpec where
+
+import Data.Yaml
+
+-- yaml ATen spec
+spec = "../vendor/aten/src/ATen/Declarations.cwrap"
+
+main = do
+  putStrLn "Done"
+
