a34a36a4c35193b0e0424c0e715fafcd4dcf596d
austinvhuang <austinh@alum.mit.edu>
2017-12-02 13:23:57 -0500

    reorganized examples with hpack, moved NN prototypes to examples, added stub for SGD

---
+++ nn/src/Torch/NN/Experimental/SGD.hs
@@ -0,0 +1,10 @@
+module Torch.NN.Experimental.SGD (
+ ) where
+
+import Torch.Core.Tensor.Static.Double
+import Torch.Core.Tensor.Static.DoubleMath
+import Torch.Core.Tensor.Static.DoubleRandom
+import Conduit
+
+main = do
+  putStrLn "Done"
