efad8e31ead6842a4cef55e1540105c827b72e88
austinvhuang <austinh@alum.mit.edu>
2017-12-09 23:27:45 -0500

    add interactive exploratory test cases to MemorySpec

--- core/tests/MemorySpec.hs
+++ core/tests/MemorySpec.hs
@@ -2,6 +2,7 @@
 
 module MemorySpec (spec) where
 
+import Control.Exception (bracket)
 import Control.Monad (forM_)
 import Torch.Core.Tensor.Dynamic.Double
 import Torch.Core.Tensor.Types
@@ -14,30 +15,73 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  it "scenario: memoryTestSmall" memoryTestLarge
+  it "scenario: memoryTestSmall" memoryTestSmall
 
--- |Iteration - allocate a tensor, print a value, allocate another tensor... etc.
-memoryTest :: TensorDim Word -> Int -> IO ()
-memoryTest dim niter = do
+iterator = iteratorBracket
+
+-- |Leaks memory
+iteratorAssign :: TensorDim Word -> Int -> IO ()
+iteratorAssign dim niter = do
   putStrLn $ show (memSizeGB dim) ++ " GB per allocation x " ++ show niter
   forM_ [1..niter] $ \iter -> do
     putStr ("Iteration : " ++ show iter ++ " / ")
-    !t <- td_new_ dim
-    !x <- td_get (D4 (0, 0, 0, 0)) t
+    x <- td_get (D4 (0, 0, 0, 0)) (td_new dim)
     putStrLn $ "Printing dummy value: " ++ show x
-    -- td_free_ t
   putStrLn "Done"
 
+-- |Releases memory
+iteratorMonadic :: TensorDim Word -> Int -> IO ()
+iteratorMonadic dim niter = do
+  putStrLn $ show (memSizeGB dim) ++ " GB per allocation x " ++ show niter
+  forM_ [1..niter] $ \iter -> do
+    putStr ("Iteration : " ++ show iter ++ " / ")
+    x <- td_get (D4 (0, 0, 0, 0)) =<< td_new_ dim
+    putStrLn $ "Printing dummy value: " ++ show x
+  putStrLn "Done"
+
+-- |Releases memory
+iteratorBracket :: TensorDim Word -> Int -> IO ()
+iteratorBracket dim niter = do
+  putStrLn $ show (memSizeGB dim) ++ " GB per allocation x " ++ show niter
+  forM_ [1..niter] $ \iter ->
+    bracket (pure iter)
+    (\iter -> do
+       putStr ("Iteration : " ++ show iter ++ " / ")
+       x <- td_get (D4 (0, 0, 0, 0)) (td_new dim)
+       putStrLn $ "Printing dummy value: " ++ show x
+    )
+    (\x -> pure ())
+  putStrLn "Done"
+
+manualAlloc1 :: IO ()
+manualAlloc1 = do
+  putStrLn $ "Allocating"
+  let !t = td_new (D4 (200, 200, 200, 200))
+  x <- td_get (D4 (0, 0, 0, 0)) t
+  putStrLn $ "Printing dummy value: " ++ show x
+
+manualAlloc2 :: Double -> IO (TensorDouble)
+manualAlloc2 v = do
+  putStrLn $ "Allocating"
+  let !t = td_init (D4 (200, 200, 100, 100)) v
+  x <- td_get (D4 (0, 0, 0, 0)) t
+  putStrLn $ "Printing dummy value: " ++ show x
+  pure t
+
+pr :: TensorDouble -> IO ()
+pr t = do
+  x <- td_get (D4 (0, 0, 0, 0)) t
+  putStrLn $ "Printing dummy value: " ++ show x
 
 -- |Get size per allocation
 memSizeGB :: TensorDim Word -> Double
 memSizeGB dim = fromIntegral (product dim * 8) / 1000000000.0
 
 memoryTestLarge :: IO ()
-memoryTestLarge = memoryTest (D4 (200, 200, 200, 200)) 1000000 -- 12.8 GB x 1M = 12M GB
+memoryTestLarge = iterator (D4 (200, 200, 200, 200)) 1000000 -- 12.8 GB x 1M = 12M GB
 
 memoryTestSmall :: IO ()
-memoryTestSmall = memoryTest (D4 (100, 100, 100, 7)) 300 -- 50 MB x 300 = 15 GB
+memoryTestSmall = iterator (D4 (100, 100, 100, 7)) 300 -- 50 MB x 300 = 15 GB
 
 memoryTestFast :: IO ()
-memoryTestFast = memoryTest (D4 (50, 50, 50, 5)) 10000 -- 5 MB x 1000 = 5 GB
+memoryTestFast = iterator (D4 (50, 50, 50, 5)) 10000 -- 5 MB x 1000 = 5 GB
