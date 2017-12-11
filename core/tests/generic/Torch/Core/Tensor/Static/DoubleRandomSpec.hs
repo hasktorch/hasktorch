a75a356efe930aa8f3f88b78d34c23daba3da8a4
austinvhuang <austinh@alum.mit.edu>
2017-12-10 19:24:12 -0500

    Fix and add to DoubleRandomSpec.hs

--- core/tests/generic/Torch/Core/Tensor/Static/DoubleRandomSpec.hs
+++ core/tests/generic/Torch/Core/Tensor/Static/DoubleRandomSpec.hs
@@ -1,10 +1,10 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Torch.Core.Tensor.Static.DoubleRandomSpec (spec) where
 
 import Torch.Core.Tensor.Types (TensorDim(D1))
 import Torch.Core.Tensor.Static.Double
--- import Torch.Core.Tensor.Static.DoubleRandom (newRNG, tds_random)
 import Torch.Core.Tensor.Static.DoubleRandom
 
 import Torch.Prelude.Extras
@@ -13,11 +13,11 @@ main :: IO ()
 main = hspec spec
 
 spec :: Spec
-spec =
+spec = do
   describe "scenario" $
     it "runs this scenario as expected without crashing" testScenario
-  describe "tensor distributions" $ do
-    it "runs samples from a variety of distributions without crashing" $
+  describe "distributions" $ do
+    it "samples from a variety of distributions without crashing" $
       testTensorDistributions
 
 testScenario :: Property
@@ -29,10 +29,14 @@ testScenario = monadicIO $ do
 testTensorDistributions :: IO ()
 testTensorDistributions = do
   gen <- newRNG
-  t_unif:: TDS '[10] <- tds_uniform gen (-5.0) 5.0
-  t_norm :: TDS '[10] <- tds_normal gen 100.0 10.0
-  t_exp :: TDS '[10] <- tds_exponential gen 2.0
+  t_bern   :: TDS '[10] <- tds_bernoulli gen 0.8
+  t_unif   :: TDS '[10] <- tds_uniform gen (-5.0) 5.0
+  t_norm   :: TDS '[10] <- tds_normal gen 100.0 10.0
+  t_exp    :: TDS '[10] <- tds_exponential gen 2.0
+  t_cauchy :: TDS '[10] <- tds_cauchy gen 1.0 1.0
+  tds_p t_bern
   tds_p t_unif
   tds_p t_norm
   tds_p t_exp
+  tds_p t_cauchy
 
