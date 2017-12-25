928cfac4dc4886bf4049d44ad411286e12fc40f6
stites <sam@stites.io>
2017-12-25 00:59:05 -0500

    introduce dimensions

--- core/src/Torch/Core/Internal.hs
+++ core/src/Torch/Core/Internal.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Torch.Core.Internal
@@ -20,8 +21,16 @@ module Torch.Core.Internal
 
 import Foreign (Word, Ptr)
 import Foreign.C.Types (CLLong, CLong, CDouble)
-import Torch.Core.Tensor.Types (TensorDim(..))
 import Numeric (showGFloat)
+import Numeric.Dimensions (Dim(..))
+import Data.List (intercalate)
+import GHC.TypeLits(natVal)
+import Data.Sequence (Seq, (|>))
+import Data.Foldable (toList)
+import Control.Monad (fail)
+import Data.Exceptions.Safe
+
+import qualified Numeric.Dimensions as Dim
 
 w2cll :: Word -> CLLong
 w2cll = fromIntegral
@@ -37,22 +46,75 @@ i2cll = fromIntegral
 
 fi = fromIntegral
 
+data DimView
+  = D0
+  | D1  Int
+  | D2  Int Int
+  | D3  Int Int Int
+  | D4  Int Int Int Int
+  | D5  Int Int Int Int Int
+  | D6  Int Int Int Int Int Int
+  | D7  Int Int Int Int Int Int Int
+  | D8  Int Int Int Int Int Int Int Int
+  | D9  Int Int Int Int Int Int Int Int Int
+  | D10 Int Int Int Int Int Int Int Int Int Int
+
+showdim :: Dim ns -> String
+showdim = go mempty
+  where
+    printlist :: Seq String -> String
+    printlist acc = "TensorDim: [" ++ intercalate "," (toList acc) ++ "]"
+
+    go :: Seq String -> Dim ns -> String
+    go acc D         = printlist acc
+    go acc d@Dn      = printlist (acc |> show (dimVal d))
+    go acc (Dx d@Dn) = printlist (acc |> (">=" ++ show (dimVal d)))  -- primarily for this rendering change
+    go acc (d :* D)  = printlist (acc |> show (dimVal d))
+    go acc (d :* ds) = go (acc |> show (dimVal d)) ds
+
+-- Helper function to debug dimensions package
+dimvals :: Dim ns -> [Int]
+dimvals = go mempty
+  where
+    go :: Seq Int -> Dim ns -> [Int]
+    go acc D         = toList   acc
+    go acc d@Dn      = toList $ acc |> dimVal d
+    go acc (Dx d@Dn) = toList $ acc |> dimVal d
+    go acc (d :* D)  = toList $ acc |> dimVal d
+    go acc (d :* ds) = go (acc |> dimVal d) ds
+
+view :: Dim a -> DimView
+view d = case dimVals d of
+  [] -> D0
+  [d1] -> D1 d1
+  [d1,d2] -> D2 d1 d2
+  [d1,d2,d3] -> D3 d1 d2 d3
+  [d1,d2,d3,d4] -> D4 d1 d2 d3 d4
+  [d1,d2,d3,d4,d5] -> D5 d1 d2 d3 d4 d5
+  [d1,d2,d3,d4,d5,d6] -> D6 d1 d2 d3 d4 d5 d6
+  [d1,d2,d3,d4,d5,d6,d7] -> D7 d1 d2 d3 d4 d5 d6 d7
+  [d1,d2,d3,d4,d5,d6,d7,d8] -> D8 d1 d2 d3 d4 d5 d6 d7 d8
+  [d1,d2,d3,d4,d5,d6,d7,d8,d9] -> D9 d1 d2 d3 d4 d5 d6 d7 d8 d9
+  [d1,d2,d3,d4,d5,d6,d7,d8,d9,d10] -> D10 d1 d2 d3 d4 d5 d6 d7 d8 d9 d10
+  _  -> error "tensor rank is not accounted for in the view pattern"
+
 -- | simple helper to clean up common pattern matching on TensorDim
 onDims
-  :: (a0 -> a)
+  :: (Int -> a)
   -> b
   -> ( a -> b )
   -> ( a -> a -> b )
   -> ( a -> a -> a -> b )
   -> ( a -> a -> a -> a -> b )
-  -> TensorDim a0
+  -> Dim dims
   -> b
-onDims ap f0 f1 f2 f3 f4 = \case
+onDims ap f0 f1 f2 f3 f4 dim = case view dim of
   D0 -> f0
   D1 d1 -> f1 (ap d1)
-  D2 (d1, d2) -> f2 (ap d1) (ap d2)
-  D3 (d1, d2, d3) -> f3 (ap d1) (ap d2) (ap d3)
-  D4 (d1, d2, d3, d4) -> f4 (ap d1) (ap d2) (ap d3) (ap d4)
+  D2 d1 d2 -> f2 (ap d1) (ap d2)
+  D3 d1 d2 d3 -> f3 (ap d1) (ap d2) (ap d3)
+  D4 d1 d2 d3 d4 -> f4 (ap d1) (ap d2) (ap d3) (ap d4)
+
 
 
 -- | Show a real value with limited precision
