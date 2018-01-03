6375442c786f5a7e9c9242e326962924ecf7e68e
stites <sam@stites.io>
2018-01-03 17:45:05 -0500

    adjust Dynamic Generic module

--- core/src/generic/Torch/Core/Tensor/Dynamic/Generic.hs
+++ core/src/generic/Torch/Core/Tensor/Dynamic/Generic.hs
@@ -8,8 +8,14 @@
 {-# LANGUAGE TypeInType #-}
 {-# LANGUAGE ConstraintKinds #-}
 module Torch.Core.Tensor.Dynamic.Generic
-  ( TensorDouble(..)
-  , THTensor(..)
+  ( genericWrapRaw
+  , genericP
+  , genericToList
+  , genericFromListNd
+  , genericFromList1d
+  , genericResize
+  , genericGet
+  , genericDynamicDims
   ) where
 
 import Control.Monad (void)
@@ -20,7 +26,7 @@ import GHC.Exts (fromList, toList, IsList, Item)
 import System.IO.Unsafe (unsafePerformIO)
 
 import Torch.Core.Tensor.Dim (Dim(..), SomeDims(..), someDimsM)
-import Torch.Core.Tensor.Generic.Internal (CTHDoubleTensor, CTHFloatTensor, HaskType)
+import Torch.Core.Tensor.Generic.Internal (CTHDoubleTensor, CTHFloatTensor, HaskReal)
 import qualified THDoubleTensor as T
 import qualified THLongTensor as T
 import qualified Torch.Core.Tensor.Generic as Gen
@@ -39,20 +45,20 @@ instance IsList TensorFloat where
   toList = genericToList realToFrac
 
 genericWrapRaw :: GenericOps' t => Ptr (TensorPtrType t) -> IO t
-genericWrapRaw tensor = construct <$> newForeignPtr Gen._free tensor
+genericWrapRaw tensor = construct <$> newForeignPtr Gen.p_free tensor
 
-genericP :: (GenericOps' t, Show (HaskType' t)) => t -> IO ()
+genericP :: (GenericOps' t, Show (HaskReal' t)) => t -> IO ()
 genericP t = withForeignPtr (getForeign t) Gen.dispRaw
 
-genericToList :: GenericOps' t => (HaskType' t -> Item t) -> t -> [Item t]
+genericToList :: GenericOps' t => (HaskReal' t -> Item t) -> t -> [Item t]
 genericToList translate t = unsafePerformIO $ withForeignPtr (getForeign t) (pure . map translate . Gen.flatten)
 {-# NOINLINE genericToList #-}
 
 -- | Initialize a tensor of arbitrary dimension from a list
 -- FIXME(stites): This should go in MonadThrow
 genericFromListNd
-  :: (GenericMath' t, Num (HaskType' t), k ~ Nat)
-  => (Item t -> HaskType' t) -> Dim (d::[k]) -> [Item t] -> t
+  :: (GenericMath' t, Num (HaskReal' t), k ~ Nat)
+  => (Item t -> HaskReal' t) -> Dim (d::[k]) -> [Item t] -> t
 genericFromListNd translate d l =
   if fromIntegral (product (Dim.dimVals d)) == length l
   then genericResize (genericFromList1d translate l) d
@@ -60,8 +66,8 @@ genericFromListNd translate d l =
 
 -- | Initialize a 1D tensor from a list
 genericFromList1d
-  :: forall t k . (GenericMath' t, Num (HaskType' t), k ~ Nat)
-  => (Item t -> HaskType' t) -> [Item t] -> t
+  :: forall t k . (GenericMath' t, Num (HaskReal' t), k ~ Nat)
+  => (Item t -> HaskReal' t) -> [Item t] -> t
 genericFromList1d translate l = unsafePerformIO $ do
   sdims <- someDimsM [length l]
   let res = genericNew' sdims
@@ -69,25 +75,25 @@ genericFromList1d translate l = unsafePerformIO $ do
   pure res
  where
   mutTensor :: ForeignPtr (TensorPtrType t) -> (Int, Item t) -> IO ()
-  mutTensor t (idx, value) = withForeignPtr t $ \tp -> Gen.set1d tp (fromIntegral idx) (translate value)
+  mutTensor t (idx, value) = withForeignPtr t $ \tp -> Gen.c_set1d tp (fromIntegral idx) (translate value)
 {-# NOINLINE genericFromList1d #-}
 
 -- |Copy contents of tensor into a new one of specified size
 genericResize
-  :: forall t d . (GenericMath' t, Num (HaskType' t), k ~ Nat)
+  :: forall t d . (GenericMath' t, Num (HaskReal' t), k ~ Nat)
   => t -> Dim (d::[k]) -> t
 genericResize t d = unsafePerformIO $ do
   let resDummy = genericNew d :: t
-  newPtr <- withForeignPtr (getForeign t) Gen.newClone
-  newFPtr <- newForeignPtr Gen._free newPtr
-  with2ForeignPtrs newFPtr (getForeign resDummy) Gen.resizeAs
+  newPtr <- withForeignPtr (getForeign t) Gen.c_newClone
+  newFPtr <- newForeignPtr Gen.p_free newPtr
+  with2ForeignPtrs newFPtr (getForeign resDummy) Gen.c_resizeAs
   pure $ construct newFPtr
 {-# NOINLINE genericResize #-}
 
 with2ForeignPtrs :: ForeignPtr f -> ForeignPtr f -> (Ptr f -> Ptr f -> IO x) -> IO x
 with2ForeignPtrs fp1 fp2 fn = withForeignPtr fp1 (withForeignPtr fp2 . fn)
 
-genericGet :: (GenericOps' t, k ~ Nat) => (HaskType (TensorPtrType t) -> Item t) -> Dim (d::[k]) -> t -> Item t
+genericGet :: (GenericOps' t, k ~ Nat) => (HaskReal (TensorPtrType t) -> Item t) -> Dim (d::[k]) -> t -> Item t
 genericGet translate loc tensor = unsafePerformIO $ withForeignPtr
   (getForeign tensor)
   (\t -> pure . translate $ t `Gen.genericGet` loc)
@@ -95,51 +101,56 @@ genericGet translate loc tensor = unsafePerformIO $ withForeignPtr
 
 genericNewWithTensor :: (GenericOps' t) => t -> t
 genericNewWithTensor t = unsafePerformIO $ do
-  newPtr <- withForeignPtr (getForeign t) Gen.newWithTensor
-  newFPtr <- newForeignPtr Gen._free newPtr
+  newPtr <- withForeignPtr (getForeign t) Gen.c_newWithTensor
+  newFPtr <- newForeignPtr Gen.p_free newPtr
   pure $ construct newFPtr
 {-# NOINLINE genericNewWithTensor #-}
 
 -- |Create a new (double) tensor of specified dimensions and fill it with 0
 genericNew
-  :: (GenericMath' t, Num (HaskType' t), k ~ Nat)
+  :: (GenericMath' t, Num (HaskReal' t), k ~ Nat)
   => Dim (d::[k]) -> t
 genericNew dims = unsafePerformIO $ do
   newPtr <- Gen.constant dims 0
-  fPtr <- newForeignPtr Gen._free newPtr
+  fPtr <- newForeignPtr Gen.p_free newPtr
   void $ withForeignPtr fPtr Gen.fillZeros
   pure $ construct fPtr
 {-# NOINLINE genericNew #-}
 
 genericNew'
-  :: (GenericMath' t, Num (HaskType' t), k ~ Nat)
+  :: (GenericMath' t, Num (HaskReal' t), k ~ Nat)
   => SomeDims -> t
 genericNew' (SomeDims ds) = genericNew ds
 
 genericFree_ :: THTensor t => t -> IO ()
 genericFree_ t = finalizeForeignPtr $! getForeign t
 
-genericInit :: (GenericMath' t, k ~ Nat) => (Item t -> HaskType' t) -> Dim (d::[k]) -> Item t -> t
+genericInit :: (GenericMath' t, k ~ Nat) => (Item t -> HaskReal' t) -> Dim (d::[k]) -> Item t -> t
 genericInit translate ds val = unsafePerformIO $ do
   newPtr <- Gen.constant ds (translate val)
-  fPtr <- newForeignPtr Gen._free newPtr
+  fPtr <- newForeignPtr Gen.p_free newPtr
   withForeignPtr fPtr (Gen.inplaceFill translate val)
   pure $ construct fPtr
 {-# NOINLINE genericInit #-}
 
 genericTranspose :: GenericOps' t => Word -> Word -> t -> t
 genericTranspose (fromIntegral->dim1C) (fromIntegral->dim2C) t = unsafePerformIO $ do
-  newPtr <- withForeignPtr (getForeign t) (\p -> Gen.newTranspose p dim1C dim2C)
-  newFPtr <- newForeignPtr Gen._free newPtr
+  newPtr <- withForeignPtr (getForeign t) (\p -> Gen.c_newTranspose p dim1C dim2C)
+  newFPtr <- newForeignPtr Gen.p_free newPtr
   pure $ construct newFPtr
 {-# NOINLINE genericTranspose #-}
 
 genericTrans :: GenericOps' t => t -> t
 genericTrans t = unsafePerformIO $ do
-  newPtr <- withForeignPtr (getForeign t) (\p -> Gen.newTranspose p 1 0)
-  newFPtr <- newForeignPtr Gen._free newPtr
+  newPtr <- withForeignPtr (getForeign t) (\p -> Gen.c_newTranspose p 1 0)
+  newFPtr <- newForeignPtr Gen.p_free newPtr
   pure $ construct newFPtr
 {-# NOINLINE genericTrans #-}
+
+genericDynamicDims :: GenericOps' t => t -> SomeDims
+genericDynamicDims t = unsafePerformIO $
+  withForeignPtr (getForeign t) (pure . Gen.getDynamicDim)
+{-# NOINLINE genericDynamicDims #-}
 
 
 {-
@@ -189,7 +200,7 @@ apply1_ transformation mtx val = unsafePerformIO $ do
     res = td_new (tdDim mtx)
 {-# NOINLINE apply1_ #-}
 
--- |Generalize non-mutating collapse of a tensor to a constant or another tensor
+-- | Generalize non-mutating collapse of a tensor to a constant or another tensor
 apply0_ :: (Ptr CTHDoubleTensor -> a) -> TensorDouble -> IO a
 apply0_ operation tensor = do
   withForeignPtr (tdTensor tensor) (\t -> pure $ operation t)
--- core/src/generic/Torch/Core/Tensor/Dynamic/Generic/Internal.hs
+++ core/src/generic/Torch/Core/Tensor/Dynamic/Generic/Internal.hs
@@ -6,16 +6,21 @@ module Torch.Core.Tensor.Dynamic.Generic.Internal
   , TensorPtrType
   , GenericOps'
   , GenericMath'
-  , HaskType'
+  , HaskReal'
 
+  , TensorByte(..)
   , TensorDouble(..)
   , TensorFloat(..)
   ) where
 
 import Foreign (ForeignPtr)
 
-import Torch.Core.Tensor.Generic.Internal (CTHDoubleTensor, CTHFloatTensor, HaskType)
+import Torch.Core.Tensor.Generic.Internal
 import Torch.Core.Tensor.Generic (GenericOps, GenericMath)
+
+type GenericMath' t = (GenericOps (TensorPtrType t), GenericMath (TensorPtrType t), THTensor t)
+type GenericOps' t = (GenericOps (TensorPtrType t), THTensor t)
+type HaskReal' t = (HaskReal (TensorPtrType t))
 
 
 class THTensor t where
@@ -23,8 +28,23 @@ class THTensor t where
   getForeign :: t -> ForeignPtr (TensorPtrType t)
 
 type family TensorPtrType t
+type instance TensorPtrType TensorByte = CTHByteTensor
 type instance TensorPtrType TensorDouble = CTHDoubleTensor
 type instance TensorPtrType TensorFloat = CTHFloatTensor
+type instance TensorPtrType TensorInt = CTHIntTensor
+type instance TensorPtrType TensorLong = CTHLongTensor
+type instance TensorPtrType TensorShort = CTHShortTensor
+
+-------------------------------------------------------------------------------
+
+newtype TensorByte = TensorByte { tbTensor :: ForeignPtr CTHByteTensor }
+  deriving (Show, Eq)
+
+instance THTensor TensorByte where
+  construct = TensorByte
+  getForeign = tbTensor
+
+-------------------------------------------------------------------------------
 
 newtype TensorDouble = TensorDouble { tdTensor :: ForeignPtr CTHDoubleTensor }
   deriving (Show, Eq)
@@ -33,6 +53,8 @@ instance THTensor TensorDouble where
   construct = TensorDouble
   getForeign = tdTensor
 
+-------------------------------------------------------------------------------
+
 newtype TensorFloat = TensorFloat { tfTensor :: ForeignPtr CTHFloatTensor }
   deriving (Show, Eq)
 
@@ -40,8 +62,31 @@ instance THTensor TensorFloat where
   construct = TensorFloat
   getForeign = tfTensor
 
-type GenericMath' t = (GenericOps (TensorPtrType t), GenericMath (TensorPtrType t), THTensor t)
-type GenericOps' t = (GenericOps (TensorPtrType t), THTensor t)
-type HaskType' t = (HaskType (TensorPtrType t))
+-------------------------------------------------------------------------------
+
+newtype TensorInt = TensorInt { tiTensor :: ForeignPtr CTHIntTensor }
+  deriving (Show, Eq)
+
+instance THTensor TensorInt where
+  construct = TensorInt
+  getForeign = tiTensor
+
+-------------------------------------------------------------------------------
+
+newtype TensorLong = TensorLong { tlTensor :: ForeignPtr CTHLongTensor }
+  deriving (Show, Eq)
+
+instance THTensor TensorLong where
+  construct = TensorLong
+  getForeign = tlTensor
+
+-------------------------------------------------------------------------------
+
+newtype TensorShort = TensorShort { tsTensor :: ForeignPtr CTHShortTensor }
+  deriving (Show, Eq)
+
+instance THTensor TensorShort where
+  construct = TensorShort
+  getForeign = tsTensor
 
 
