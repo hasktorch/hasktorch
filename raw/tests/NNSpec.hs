8e0c8b0fce55f5e4cbe9687c5636432f747a93d6
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2018-01-24 12:53:49 +0100

    Actual minimal test for THNN
    Check the result of the operation

--- raw/tests/NNSpec.hs
+++ raw/tests/NNSpec.hs
@@ -26,23 +26,17 @@ testsFloat =
   describe "Float NN" $ do
     it "initializes tensor and perform NN_abs operation" $ do
       t1 <- c_THFloatTensor_newWithSize2d 2 2
-      T.c_THFloatTensor_fill t1 3.1
-      t2 <- c_THFloatTensor_newWithSize2d 2 2
-      T.c_THFloatTensor_fill t2 4.1
-      T.c_THFloatTensor_get2d t1 0 0 `shouldBe` (3.1 :: CFloat)
-      T.c_THNN_FloatAbs_updateOutput nullPtr t1 t2
+      T.c_THFloatTensor_fill t1 (3.0)
+      T.c_THNN_FloatAbs_updateOutput nullPtr t1 t1
+      T.c_THFloatTensor_sumall t1 `shouldBe` 12.0
       T.c_THFloatTensor_free t1
-      T.c_THFloatTensor_free t2
 
 testsDouble :: Spec
 testsDouble =
   describe "Double NN" $ do
     it "initializes tensor and perform NN_abs operation" $ do
       t1 <- c_THDoubleTensor_newWithSize2d 2 2
-      T.c_THDoubleTensor_fill t1 3.1
-      t2 <- c_THDoubleTensor_newWithSize2d 2 2
-      T.c_THDoubleTensor_fill t2 4.1
-      T.c_THDoubleTensor_get2d t1 0 0 `shouldBe` (3.1 :: CDouble)
-      T.c_THNN_DoubleAbs_updateOutput nullPtr t1 t2
+      T.c_THDoubleTensor_fill t1 (3.0)
+      T.c_THNN_DoubleAbs_updateOutput nullPtr t1 t1
+      T.c_THDoubleTensor_sumall t1 `shouldBe` 12.0
       T.c_THDoubleTensor_free t1
-      T.c_THDoubleTensor_free t2
