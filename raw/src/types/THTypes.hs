7dfe2cffbb1af60f6c3446d4f63b9a792f3ed655
Alejandro Catalina <AlejandroCatalina@users.noreply.github.com>
2018-01-28 22:50:53 +0100

    Fill typeclasses for both Storage and StorageCopy

--- raw/src/types/THTypes.hs
+++ raw/src/types/THTypes.hs
@@ -17,6 +17,22 @@ type CTHGenerator = C'THGenerator
 type CTHFile = ()
 type CTHHalf = CUShort
 
+type THLongBlas = CLong
+type THShortBlas = CShort
+type THIntBlas = CInt
+type THByteBlas = CChar
+type THHalfBlas = CShort
+type THFloatBlas = CFloat
+type THDoubleBlas = CDouble
+
+type THLongLapack = CLong
+type THShortLapack = CShort
+type THIntLapack = CInt
+type THByteLapack = CChar
+type THHalfLapack = CShort
+type THFloatLapack = CFloat
+type THDoubleLapack = CDouble
+
 -- in THNN.h
 -- typedef void THNNState
 type CTHDoubleNNState = ()
@@ -35,39 +51,47 @@ type CTHIntegerTensor = CInt
 
 type CTHByteTensor = C'THByteTensor
 type CTHByteStorage = C'THByteStorage
+type CTHByteStorageCopy = CTHByteStorage
 
 
 {- Char -}
 
 type CTHCharTensor = C'THCharTensor
 type CTHCharStorage = C'THCharStorage
+type CTHCharStorageCopy = CTHCharStorage
 
 {- Double -}
 
 type CTHDoubleTensor = C'THDoubleTensor
 type CTHDoubleStorage = C'THDoubleStorage
+type CTHDoubleStorageCopy = CTHDoubleStorage
 
 {- Float -}
 
 type CTHFloatTensor = C'THFloatTensor
 type CTHFloatStorage = C'THFloatStorage
+type CTHFloatStorageCopy = CTHFloatStorage
 
 {- Half -}
 
 type CTHHalfTensor = ()
 type CTHHalfStorage = ()
+type CTHHalfStorageCopy = ()
 
 {- Int -}
 
 type CTHIntTensor = C'THIntTensor
 type CTHIntStorage = C'THIntStorage
+type CTHIntStorageCopy = CTHIntStorage
 
 {- Long -}
 
 type CTHLongTensor = C'THLongTensor
 type CTHLongStorage = C'THLongStorage
+type CTHLongStorageCopy = CTHLongStorage
 
 {- Short -}
 
 type CTHShortTensor = C'THShortTensor
 type CTHShortStorage = C'THShortStorage
+type CTHShortStorageCopy = CTHShortStorage
