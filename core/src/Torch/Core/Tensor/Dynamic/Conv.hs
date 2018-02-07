7b19ba728d0e3809f04d8b929ec2316e2ff77415
stites <sam@stites.io>
2018-02-07 12:48:02 -0500

    add withTensors

--- core/src/Torch/Core/Tensor/Dynamic/Conv.hs
+++ core/src/Torch/Core/Tensor/Dynamic/Conv.hs
@@ -12,37 +12,29 @@ apply2DRevConv
   :: (Ptr CTensor -> CReal -> CReal -> Ptr CTensor -> Ptr CTensor -> CLLong -> CLLong -> IO ())
   -> Tensor -> HsReal -> HsReal -> Tensor -> Tensor -> Int64 -> Int64 -> IO ()
 apply2DRevConv fn res a0 a1 t0 t1 b c =
-    withForeignPtr (tensor res) $ \res' ->
-      withForeignPtr (tensor t0) $ \t0' ->
-        withForeignPtr (tensor t1) $ \t1' ->
-          fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c)
+  _with3Tensors res t0 t1 $ \res' t0' t1' ->
+    fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c)
 
 apply3DRevConv
   :: (Ptr CTensor -> CReal -> CReal -> Ptr CTensor -> Ptr CTensor -> CLLong -> CLLong -> CLLong -> IO ())
   -> Tensor -> HsReal -> HsReal -> Tensor -> Tensor -> Int64 -> Int64 -> Int64 -> IO ()
 apply3DRevConv fn res a0 a1 t0 t1 b c d =
-    withForeignPtr (tensor res) $ \res' ->
-      withForeignPtr (tensor t0) $ \t0' ->
-        withForeignPtr (tensor t1) $ \t1' ->
-          fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c) (CLLong d)
+  _with3Tensors res t0 t1 $ \res' t0' t1' ->
+    fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c) (CLLong d)
 
 apply2DConv
   :: (Ptr CTensor -> CReal -> CReal -> Ptr CTensor -> Ptr CTensor -> CLLong -> CLLong -> Ptr CChar -> Ptr CChar -> IO ())
   -> Tensor -> HsReal -> HsReal -> Tensor -> Tensor -> Int64 -> Int64 -> Ptr CChar -> Ptr CChar -> IO ()
 apply2DConv fn res a0 a1 t0 t1 b c cchars0 cchars1 =
-    withForeignPtr (tensor res) $ \res' ->
-      withForeignPtr (tensor t0) $ \t0' ->
-        withForeignPtr (tensor t1) $ \t1' ->
-          fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c) cchars0 cchars1
+  _with3Tensors res t0 t1 $ \res' t0' t1' ->
+    fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c) cchars0 cchars1
 
 apply3DConv
   :: (Ptr CTensor -> CReal -> CReal -> Ptr CTensor -> Ptr CTensor -> CLLong -> CLLong -> CLLong -> Ptr CChar -> Ptr CChar -> IO ())
   -> Tensor -> HsReal -> HsReal -> Tensor -> Tensor -> Int64 -> Int64 -> Int64 -> Ptr CChar -> Ptr CChar -> IO ()
 apply3DConv fn res a0 a1 t0 t1 b c d cchars0 cchars1 =
-    withForeignPtr (tensor res) $ \res' ->
-      withForeignPtr (tensor t0) $ \t0' ->
-        withForeignPtr (tensor t1) $ \t1' ->
-          fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c) (CLLong d) cchars0 cchars1
+  _with3Tensors res t0 t1 $ \res' t0' t1' ->
+    fn res' (hs2cReal a0) (hs2cReal a1) t0' t1' (CLLong b) (CLLong c) (CLLong d) cchars0 cchars1
 
 instance Class.TensorConv Tensor where
   conv2DRevger  = apply2DRevConv Sig.c_conv2DRevger
