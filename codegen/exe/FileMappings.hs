3e05d4f4830d4b9b61e491db86bd98bda92577e7
stites <sam@stites.io>
2018-03-04 19:31:06 -0500

    finish cleaned-up file mappings

--- codegen/exe/FileMappings.hs
+++ codegen/exe/FileMappings.hs
@@ -5,11 +5,8 @@ import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
 
-import CodeGenParse (THFunction, Parser, thParseGeneric)
 import CodeGen.Types -- (HModule, TemplateType, genericTypes)
-import RenderShared (makeModule, renderCHeaderFile, parseFile, IsTemplate(..))
-
-import CLIOptions
+import RenderShared (makeModule) -- , renderCHeaderFile, parseFile, IsTemplate(..))
 
 type HeaderFile = FilePath
 
@@ -28,18 +25,19 @@ thFiles = \case
     , mkTHGeneric  (ModuleSuffix "Tensor") (FileSuffix "TensorRandom")
     , mkTHGeneric' "Vector"
     ]
-
-  ConcreteFiles ->
-    [ (src ConcreteFiles <> "THFile.h"        , (makeModule TH (out ConcreteFiles) asfile "THFile.h" "File" "File"))
-    , (src ConcreteFiles <> "THDiskFile.h"    , (makeModule TH (out ConcreteFiles) asfile "THDiskFile.h" "DiskFile" "DiskFile"))
-    , (src ConcreteFiles <> "THAtomic.h"      , (makeModule TH (out ConcreteFiles) asfile "THDiskFile.h" "Atomic" "Atomic"))
-    , (src ConcreteFiles <> "THHalf.h"        , (makeModule TH (out ConcreteFiles) asfile "THHalf.h" "Half" "Half"))
-    , (src ConcreteFiles <> "THLogAdd.h"      , (makeModule TH (out ConcreteFiles) asfile "THLogAdd.h" "LogAdd" "LogAdd"))
-    , (src ConcreteFiles <> "THRandom.h"      , (makeModule TH (out ConcreteFiles) asfile "THRandom.h" "Random" "Random"))
-    , (src ConcreteFiles <> "THSize.h"        , (makeModule TH (out ConcreteFiles) asfile "THSize.h" "Size" "Size"))
-    , (src ConcreteFiles <> "THStorage.h"     , (makeModule TH (out ConcreteFiles) asfile "THStorage.h" "Storage" "Storage"))
-    , (src ConcreteFiles <> "THMemoryFile.h"  , (makeModule TH (out ConcreteFiles) asfile "THMemoryFile.h" "MemoryFile" "MemoryFile"))
+  ConcreteFiles -> map ($ ConcreteFiles)
+    [ mkTHFile' "File"
+    , mkTHFile' "DiskFile"
+    , mkTHFile' "Atomic"
+    , mkTHFile' "Half"
+    , mkTHFile' "LogAdd"
+    , mkTHFile' "Random"
+    , mkTHFile' "Size"
+    , mkTHFile' "Storage"
+    , mkTHFile' "MemoryFile"
     ]
+  ManagedFiles -> [] -- FIXME: check with austin that managed codegen is still a thing we want to do
+
  where
   out :: CodeGenType -> TextPath
   out = TextPath . T.pack . outDir TH
@@ -60,20 +58,22 @@ thFiles = \case
     hf :: FilePath
     hf = show lt <> T.unpack (textFileSuffix filesuff) <> ".h"
 
-  mkTHFile, mkTHGeneric
+  mkTHFile, mkTHGeneric, mkTHManaged
     :: ModuleSuffix
     -> FileSuffix
     -> CodeGenType
     -> (FilePath, TemplateType -> [THFunction] -> HModule)
   mkTHFile = mkTuple TH asfile
   mkTHGeneric = mkTuple TH astemplate
+  mkTHManaged = mkTuple TH astemplate
 
-  mkTHFile', mkTHGeneric'
+  mkTHFile', mkTHGeneric', mkTHManaged'
     :: Text
     -> CodeGenType
     -> (FilePath, TemplateType -> [THFunction] -> HModule)
   mkTHFile' suff = mkTHFile (ModuleSuffix suff) (FileSuffix suff)
   mkTHGeneric' suff = mkTHGeneric (ModuleSuffix suff) (FileSuffix suff)
+  mkTHManaged' suff = mkTHManaged (ModuleSuffix suff) (FileSuffix suff)
 
 astemplate = IsTemplate True
 asfile = IsTemplate False
