5830bf0348ea6c4221712e5b01f0bb5cdc23c314
stites <sam@stites.io>
2018-03-05 12:29:44 -0500

    reorder torch signatures

--- signatures/signed/src/Torch/Signature/Signed/Tensor/Math.hs
+++ signatures/signed/src/Torch/Signature/Signed/Tensor/Math.hs
@@ -1,9 +1,9 @@
-signature TensorMathSigned where
+signature Torch.Signature.Signed.Tensor.Math where
 
-import THTypes
+import Torch.Types.TH
 import Foreign
 import Foreign.C.Types
-import SigTypes
+import Torch.Signature.Types
 
 c_neg :: Ptr CTensor -> Ptr CTensor -> IO ()
 c_abs :: Ptr CTensor -> Ptr CTensor -> IO ()
