a257df52578bf9f5b15c70abb57ad45897b3a8bc
stites <sam@stites.io>
2018-03-19 17:36:40 -0400

    bump circle and pytorch (adjusting the build-aten.sh script as well)

--- vendor/build-aten.sh
+++ vendor/build-aten.sh
@@ -1,35 +1,39 @@
 #!/usr/bin/env bash
-set -eu
+#set -eu
 
-mkdir -p ./build
+function check_bin {
+  local BIN="$1"
+  if ! [ -x "$(command -v $BIN)" ]; then
+      echo "Error: $BIN is not installed." >&2
+      exit 1
+  fi
+}
 
 case "$(uname)" in
   "Darwin")
-    if ! [ -x "$(command -v gcc-7)" ]; then
-        echo 'Error: gcc-7 is not installed, use homebrew to install it.' >&2
-        exit 1
-    fi
-    echo "Running as OSX ..."
+    echo "Running as OSX"
+    check_bin "gcc-7"
     CXX=g++-7
     CC=gcc-7
     ;;
 
   "Linux")
-    if ! [ -x "$(command -v gcc)" ]; then
-        echo 'Error: gcc is not installed.' >&2
-        exit 1
+    if uname -v | grep "Ubuntu" &> /dev/null && lsb_release -a 2>&1 | grep " 17." &> /dev/null; then
+      echo "Running with gcc-4.8 on Ubuntu 17.xx -- see https://github.com/pytorch/pytorch/issues/5136 for details"
+      check_bin "gcc-4.8"
+      CXX=g++-4.8
+      CC=gcc-4.8
+    else
+      echo "Running as Linux with default gcc."
+      check_bin "gcc"
+      CXX=g++
+      CC=gcc
     fi
-    echo "Running as Linux ..."
-    CXX=g++
-    CC=gcc
     ;;
 
   "FreeBSD")
-    if ! [ -x "$(command -v gcc)" ]; then
-        echo 'Error: gcc is not installed.' >&2
-        exit 1
-    fi
-    echo "Running as FreeBSD..."
+    echo "Running as FreeBSD with default gcc."
+    check_bin "gcc"
     CXX=g++
     CC=gcc
     ;;
@@ -39,12 +43,46 @@ case "$(uname)" in
     ;;
   esac
 
-echo "Compilers:"
-echo "  $CXX"
-echo "  $CC"
+function build {
+  echo "Compilers:"
+  echo "  $CXX"
+  echo "  $CC"
 
-cd ./build; cmake ../aten/CMakeLists.txt -B. -DNO_CUDA=true -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CC_COMPILER=$CC -DCXX=$CXX -DCC=$CC; make; cd ..
-cp ./build/src/ATen/libATen.* ./build/
+  mkdir -p ./aten/build
+  cd ./aten/build
+  cmake .. -DCMAKE_C_COMPILER=$CC -DCMAKE_CXX_COMPILER=$CXX -DCMAKE_CC_COMPILER=$CC -DCXX=$CXX -DCC=$CC -Wno-dev
+  make install
+}
+
+function install {
+  mkdir build
+  cp ./aten/build/src/ATen/libATen.* ./build/
+}
+
+case "$1" in
+  ""|"all")
+    build
+    install
+    ;;
+  "build")
+    build
+    ;;
+  "install")
+    install
+    ;;
+  "clean")
+    rm -rf ./aten/build
+    ;;
+  "help"|*)
+    echo "./build-aten.sh -- build the aten library"
+    echo ""
+    echo "COMMANDS"
+    echo ""
+    echo "  all     - build, then install. Default command."
+    echo "  build   - build the aten library"
+    echo "  install - copy built binaries to the ./build folder"
+    echo "  help    - show this prompt"
+esac
 
 # if [ -x "$(command -v nm)" ]; then
 #     echo "Checking symbols in dylib:"
