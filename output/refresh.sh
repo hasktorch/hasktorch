90d95cb43b60b191b3387f2d7301cf84ee7728ee
stites <sam@stites.io>
2018-03-23 11:01:07 -0400

    sync all nn types

--- output/refresh.sh
+++ output/refresh.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 set -eu
 
-rsync -arv {.,..}/raw/th/src/
-rsync -arv {.,..}/raw/thc/src/
+function nuke {
+  lib=$(echo "$1" | tr '[:upper:]' '[:lower:]')
+  rm -rf ./raw/${lib}/src/
+  (
+    cd ..
+    cabal new-run hasktorch-codegen:ht-codegen -- --lib $1 --type concrete
+    cabal new-run hasktorch-codegen:ht-codegen -- --lib $1 --type generic
+  )
+  rm -rf ../raw/${lib}/src/
+  rsync -arv {.,..}/raw/${lib}/src/
+}
+
+nuke TH
+nuke THC
+nuke THNN
+nuke THCUNN
+
