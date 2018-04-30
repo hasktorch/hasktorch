4408526b9036b524f8f886b2f9ab612b836afb88
stites <sam@stites.io>
2018-04-30 18:43:01 -0400

    test nn abs function

--- core/tests/Torch/NN/Static/AbsSpec.hs
+++ core/tests/Torch/NN/Static/AbsSpec.hs
@@ -12,8 +12,18 @@ spec :: Spec
 spec = do
   describe "abs_updateOutput" $ do
     it "runs the absolute function" $ do
-      x :: DoubleTensor '[2, 4] <- fromList [-4, 4]
-      y <- abs_updateOutput x
-      y' :: [Double] <- tensordata y
-      y' `shouldSatisfy` (all (>0))
+      x :: DoubleTensor '[2, 4] <- fromList [-4..4]
+      y <- tensordata =<< abs_updateOutput x
+      y `shouldSatisfy` all (>= 0)
+
+  describe "abs_updateGradInput" $ do
+    it "returns the input gradient" $ do
+      x  :: DoubleTensor '[2, 4] <- fromList [-4..4]
+      go :: DoubleTensor '[2, 4] <- constant 1
+      rs <- tensordata (signum x)
+      ys <- tensordata =<< abs_updateGradInput x go
+      zip ys rs `shouldSatisfy` all eqSigns
+  where
+    eqSigns :: (Double, Double) -> Bool
+    eqSigns (y, r) = y == r || (r == 0 && y == 1)
 
