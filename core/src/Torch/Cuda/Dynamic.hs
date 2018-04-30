f29b9da4e8d841f6ab76ea5daf51bcc5b5baaf5e
stites <sam@stites.io>
2018-04-30 19:17:47 -0400

    add Floating and Fractional instances to Float and Double tensors

--- core/src/Torch/Cuda/Dynamic.hs
+++ core/src/Torch/Cuda/Dynamic.hs
@@ -23,3 +23,51 @@ import Torch.Cuda.Long.Dynamic  as X
 import Torch.Cuda.Double.Dynamic       as X
 import Torch.Cuda.Double.DynamicRandom as X
 
+import Torch.Dimensions as X
+import Torch.Types.THC as X
+
+-------------------------------------------------------------------------------
+
+import System.IO.Unsafe
+
+instance Fractional DoubleDynamic where
+  fromRational = unsafePerformIO . X.constant (dim :: Dim '[1]) . fromRational
+  {-# NOINLINE fromRational #-}
+
+  (/) = (^/^)
+  {-# NOINLINE (/) #-}
+
+instance Floating DoubleDynamic where
+  pi = unsafePerformIO $ X.constant (dim :: Dim '[1]) pi
+  {-# NOINLINE pi #-}
+
+  exp = unsafePerformIO . X.exp
+  {-# NOINLINE exp #-}
+
+  log = unsafePerformIO . X.log
+  {-# NOINLINE log #-}
+
+  sqrt = unsafePerformIO . X.sqrt
+  {-# NOINLINE sqrt #-}
+
+  sin = unsafePerformIO . X.sin
+  {-# NOINLINE sin #-}
+
+  cos = unsafePerformIO . X.cos
+  {-# NOINLINE cos #-}
+
+  asin = unsafePerformIO . X.asin
+  {-# NOINLINE asin #-}
+
+  acos = unsafePerformIO . X.acos
+  {-# NOINLINE acos #-}
+
+  atan = unsafePerformIO . X.atan
+  {-# NOINLINE atan #-}
+
+  sinh = unsafePerformIO . X.sinh
+  {-# NOINLINE sinh #-}
+
+  cosh = unsafePerformIO . X.cosh
+  {-# NOINLINE cosh #-}
+
