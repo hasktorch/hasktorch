f03330888252ceb0aa49ee7e6293ac2a8bc24b45
stites <sam@stites.io>
2018-06-05 19:26:22 -0400

    document - dynamic scan and mode

--- indef/src/Torch/Indef/Dynamic/Tensor/Math/Scan.hs
+++ indef/src/Torch/Indef/Dynamic/Tensor/Math/Scan.hs
@@ -1,12 +1,23 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module    :  Torch.Indef.Dynamic.Tensor.Math.Scan
+-- Copyright :  (c) Sam Stites 2017
+-- License   :  BSD3
+-- Maintainer:  sam@stites.io
+-- Stability :  experimental
+-- Portability: non-portable
+-------------------------------------------------------------------------------
 module Torch.Indef.Dynamic.Tensor.Math.Scan where
 
 import Torch.Indef.Types
 
 import qualified Torch.Sig.Tensor.Math.Scan as Sig
 
+-- | Mutate the first tensor to contain the cumulative sum of the elements in the second, performing the operation over the specified dimension.
 _cumsum :: Dynamic -> Dynamic -> DimVal -> IO ()
 _cumsum t0 t1 i0 = with2DynamicState t0 t1 $ shuffle3 Sig.c_cumsum (fromIntegral i0)
 
+-- | Mutate the first tensor to contain the cumulative product of the elements in the second, performing the operation over the specified dimension.
 _cumprod :: Dynamic -> Dynamic -> DimVal -> IO ()
 _cumprod t0 t1 i0 = with2DynamicState t0 t1 $ shuffle3 Sig.c_cumprod (fromIntegral i0)
 
--- indef/src/Torch/Indef/Dynamic/Tensor/Mode.hs
+++ indef/src/Torch/Indef/Dynamic/Tensor/Mode.hs
@@ -1,3 +1,12 @@
+-------------------------------------------------------------------------------
+-- |
+-- Module    :  Torch.Indef.Dynamic.Tensor.Mode
+-- Copyright :  (c) Sam Stites 2017
+-- License   :  BSD3
+-- Maintainer:  sam@stites.io
+-- Stability :  experimental
+-- Portability: non-portable
+-------------------------------------------------------------------------------
 module Torch.Indef.Dynamic.Tensor.Mode where
 
 import qualified Torch.Sig.Tensor.Mode as Sig
@@ -5,6 +14,9 @@ import qualified Torch.Indef.Index as Ix
 
 import Torch.Indef.Types
 
+-- | Mutate the tuple of a tensor and index tensor to contain the most frequent element in the specified dimension.
+--
+-- FIXME: if KeepDim is False, we need to return Nothing for the index tensor -- otherwise bad things may happen.
 _mode :: (Dynamic, IndexDynamic) -> Dynamic -> DimVal -> Maybe KeepDim -> IO ()
 _mode (t0, ix) t1 i0 i1 = with2DynamicState t0 t1 $ \s' t0' t1' ->
   Ix.withDynamicState ix $ \_ ix' ->
