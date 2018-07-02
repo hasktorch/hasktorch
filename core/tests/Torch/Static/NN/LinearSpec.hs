999cef5b2742169c2a9965c64a20f6a3dd46750a
stites <sam@stites.io>
2018-07-02 18:45:38 -0400

    add simple unit test for linear

--- core/tests/Torch/Static/NN/LinearSpec.hs
+++ core/tests/Torch/Static/NN/LinearSpec.hs
@@ -12,24 +12,40 @@ main = hspec spec
 
 spec :: Spec
 spec = do
-  let Just (x :: DoubleTensor '[2, 4]) = fromList [-4..4-1]
   describe "a single linear layer" $ do
-    it "runs the absolute function" $ do
-      y <- tensordata =<< abs_updateOutput x
-      y `shouldSatisfy` all (>= 0)
+    ll :: Linear 3 2 <- runIO $ mkLinear xavier
+    it "initializes with xavier correctly" $ do
+      w' <- tensordata (weights ll)
+      b' <- tensordata (bias ll)
+      w' `shouldSatisfy` all (== 1/3)
+      b' `shouldSatisfy` all (== 1/2)
 
--- linear
---   :: forall s i o
---   .  Reifies s W
---   => All KnownDim '[i,o]
---   => BVar s (Linear i o)
---   -> BVar s (Tensor '[i])
---   -> BVar s (Tensor '[o])
--- linear = liftOp2 $ op2 $ \l i -> (transpose2d (weights l) `mv` i + bias l, go l i)
---   where
---     go :: Linear i o -> Tensor '[i] -> Tensor '[o] -> (Linear i o, Tensor '[i])
---     go (Linear (w, b)) i gout = (Linear (i `outer` b', b'), w `mv` b')
---       where
---         b' = gout - b
+    describe "the forward pass" $ do
+      let y = constant 1 :: Tensor '[3]
+          (o, _) = backprop2 (linear undefined) ll y
+
+      it "performs matrix multipication as you would expect" $
+        tensordata o >>= (`shouldSatisfy` all (== 3/2))
+
+      it "leaves weights unchanged" $
+        tensordata (weights ll) >>= (`shouldSatisfy` all (== 1/3))
+
+      it "leaves bias unchanged" $
+        tensordata (bias ll) >>= (`shouldSatisfy` all (== 1/2))
+
+    describe "the backward pass" $ do
+      let y = constant 1 :: Tensor '[3]
+          lr = 1.0
+          (_, (ll', o)) = backprop2 (linear lr) ll y
+
+      it "returns updated weights" $ do
+        tensordata (weights ll') >>= (`shouldSatisfy` all (== 1/2))
+
+      it "returns updated bias" $ do
+        tensordata (bias ll') >>= (`shouldSatisfy` all (== 1/2))
+
+      it "returns the updated output tensor" $ do
+        -- let x = (weights ll) `mv` (constant lr - bias ll)
+        tensordata o >>= (`shouldSatisfy` all (== 1/3))
 
 
