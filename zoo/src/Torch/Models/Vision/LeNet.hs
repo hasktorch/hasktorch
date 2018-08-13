c61b262ec0e7ca4ddf6abdfc8a70f856325fc4a9
stites <sam@stites.io>
2018-08-13 15:04:05 -0400

    move LeNet to vision

--- zoo/src/Torch/Models/Vision/LeNet.hs
+++ zoo/src/Torch/Models/Vision/LeNet.hs
@@ -6,7 +6,7 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Normalise #-}
-module Torch.Models.LeNet where
+module Torch.Models.Vision.LeNet where
 
 import Data.Function ((&))
 import Data.List (intercalate)
@@ -18,11 +18,11 @@ import Data.Singletons.Prelude hiding (type (*), All)
 #ifdef CUDA
 import Numeric.Dimensions
 import Torch.Cuda.Double as Torch
-import Torch.Cuda.Double.NN.Linear (Linear(..), linear)
+import Torch.Cuda.Double.NN.Linear -- (Linear(..), linear)
 import qualified Torch.Cuda.Double.NN.Conv2d as NN
 #else
 import Torch.Double as Torch
-import Torch.Double.NN.Linear (Linear(..), linear)
+import Torch.Double.NN.Linear -- (Linear(..), linear)
 import qualified Torch.Double.NN.Conv2d as NN
 #endif
 
@@ -120,6 +120,8 @@ lenet lr arch inp
   & relu . linear lr (arch ^^. fc1)
   & relu . linear lr (arch ^^. fc2)
   &        linear lr (arch ^^. fc3)
+  -- & logSoftMax
+  & softmax
 
 -- Optionally, we can remove the explicit type and everything would be fine.
 -- Including it is quite a bit of work and requires pulling in the correct
@@ -164,7 +166,7 @@ lenetLayer lr conv inp
 lenetLayer1
   :: Reifies s W
   => Double                         -- ^ learning rate
-  -> BVar s (Conv2d 1 6 5 5)        -- ^ convolutional layer
+  -> BVar s (Conv2d 1 6 '(5,5) )        -- ^ convolutional layer
   -> BVar s (Tensor '[1, 32, 32])   -- ^ input
   -> BVar s (Tensor '[6, 14, 14])   -- ^ output
 lenetLayer1 = lenetLayer
@@ -172,10 +174,60 @@ lenetLayer1 = lenetLayer
 lenetLayer2
   :: Reifies s W
   => Double                          -- ^ learning rate
-  -> BVar s (Conv2d 6 16 5 5)        -- ^ convolutional layer
+  -> BVar s (Conv2d 6 16 '(5,5) )        -- ^ convolutional layer
   -> BVar s (Tensor '[ 6, 14, 14])   -- ^ input
   -> BVar s (Tensor '[16,  5,  5])   -- ^ output
 lenetLayer2 = lenetLayer
 -}
+
+lenetBatch lr arch inp
+  = lenetLayerBatch lr (arch ^^. conv1) inp
+  & lenetLayerBatch lr (arch ^^. conv2)
+
+  & flattenBPBatch
+
+  -- start fully connected network
+  & relu . linearBatch lr (arch ^^. fc1)
+  & relu . linearBatch lr (arch ^^. fc2)
+  &        linearBatch lr (arch ^^. fc3)
+  -- & logSoftMax
+  & softmaxN (dim :: Dim 1)
+
+
+lenetLayerBatch
+  :: forall inp h w ker ow oh s out mow moh step pad batch
+
+  -- backprop constraint to hold the wengert tape
+  .  Reifies s W
+
+  -- leave input, output and square kernel size variable so that we
+  -- can reuse the layer...
+  => All KnownDim '[batch,inp,out,ker]
+
+  -- FIXME: derive these from the signature (maybe assign them as args)
+  => pad ~ 0   --  default padding size
+  => step ~ 1  --  default step size for Conv2d
+
+  -- ...this means we need the constraints for conv2dMM and maxPooling2d
+  -- Note that oh and ow are then used as input to the maxPooling2d constraint.
+  => SpatialConvolutionC inp h  w ker ker step step pad pad  oh  ow
+  => SpatialDilationC       oh ow   2   2    2    2 pad pad mow moh 1 1 'True
+
+  -- Start withe parameters
+  => Double                            -- ^ learning rate for convolution layer
+  -> BVar s (Conv2d inp out '(ker,ker))   -- ^ convolutional layer
+  -> BVar s (Tensor '[batch, inp,   h,   w])  -- ^ input
+  -> BVar s (Tensor '[batch, out, moh, mow])  -- ^ output
+lenetLayerBatch lr conv inp
+  = NN.conv2dMMBatch
+      (Step2d    :: Step2d '(1,1))
+      (Padding2d :: Padding2d '(0,0))
+      lr conv inp
+  & relu
+  & maxPooling2dBatch
+      (Kernel2d  :: Kernel2d '(2,2))
+      (Step2d    :: Step2d '(2,2))
+      (Padding2d :: Padding2d '(0,0))
+      (sing      :: SBool 'True)
 
 
