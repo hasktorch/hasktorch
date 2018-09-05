392fa6fff9aca32ea19795cdb8fc32ecee60b665
stites <sam@stites.io>
2018-09-05 09:01:01 -0400

    fix TH finalizers (and, thus, storage backend bug)

--- raw/th/cbits/finalizers.c
+++ raw/th/cbits/finalizers.c
@@ -4,81 +4,81 @@
 // Byte types
 //================================================
 void free_ByteStorage(void* s, THByteStorage * x) {
-  THByteStorage_free(x);
+  THByteStorage_free(&x);
 };
 
 void free_ByteTensor(void* s, THByteTensor * x) {
-  THByteTensor_free(x);
+  THByteTensor_free(&x);
 };
 
 // Char types
 //================================================
 void free_CharStorage(void* s, THCharStorage * x) {
-  THCharStorage_free(x);
+  THCharStorage_free(&x);
 };
 
 void free_CharTensor(void* s, THCharTensor * x) {
-  THCharTensor_free(x);
+  THCharTensor_free(&x);
 };
 
 // Int types
 //================================================
 void free_IntStorage(void* s, THIntStorage * x) {
-  THIntStorage_free(x);
+  THIntStorage_free(&x);
 };
 
 void free_IntTensor(void* s, THIntTensor * x) {
-  THIntTensor_free(x);
+  THIntTensor_free(&x);
 };
 
 // Short types
 //================================================
 void free_ShortStorage(void* s, THShortStorage * x) {
-  THShortStorage_free(x);
+  THShortStorage_free(&x);
 };
 
 void free_ShortTensor(void* s, THShortTensor * x) {
-  THShortTensor_free(x);
+  THShortTensor_free(&x);
 };
 
 // Long types
 //================================================
 void free_LongStorage(void* s, THLongStorage * x) {
-  THLongStorage_free(x);
+  THLongStorage_free(&x);
 };
 
 void free_LongTensor(void* s, THLongTensor * x) {
-  THLongTensor_free(x);
+  THLongTensor_free(&x);
 };
 
 // Half types
 //================================================
 void free_HalfStorage(void* s, THHalfStorage * x) {
-  THHalfStorage_free(x);
+  THHalfStorage_free(&x);
 };
 
 void free_HalfTensor(void* s, THHalfTensor * x) {
-  THHalfTensor_free(x);
+  THHalfTensor_free(&x);
 };
 
 // Float types
 //================================================
 void free_FloatStorage(void* s, THFloatStorage * x) {
-  THFloatStorage_free(x);
+  THFloatStorage_free(&x);
 };
 
 void free_FloatTensor(void* s, THFloatTensor * x) {
-  THFloatTensor_free(x);
+  THFloatTensor_free(&x);
 };
 
 // Double types
 //================================================
 void free_DoubleStorage(void* s, THDoubleStorage * x) {
-  THDoubleStorage_free(x);
+  THDoubleStorage_free(&x);
 };
 
 void free_DoubleTensor(void* s, THDoubleTensor * x) {
-  THDoubleTensor_free(x);
+  THDoubleTensor_free(&x);
 };
 
 
