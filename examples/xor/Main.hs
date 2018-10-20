90331c38e0a8bc16979c9fb9918906881f56a15b
Sam Stites <sam@stites.io>
2018-10-20 12:20:57 -0400

    clean up and document xor example

--- examples/xor/Main.hs
+++ examples/xor/Main.hs
@@ -5,117 +5,32 @@ module Main where
 
 -- Other modules
 import Dense3 (linearBatchIO, reluIO)
-import Criterion (criterion)
 
 -- Dependencies
-import Text.Printf
-import Control.Monad
-import Control.Monad.ST
+import Text.Printf (printf)
+import Control.Monad (replicateM)
+import Control.Monad.ST (ST, runST)
 import System.Random.MWC (GenST)
-import qualified System.Random.MWC as MWC
-import qualified Data.Vector as V
+import qualified System.Random.MWC as MWC (toSeed, Seed, save, restore, uniformR)
+import qualified Data.Vector as V (fromList)
 
--- Torch imports
-import Torch.Double hiding (update)
-import Torch.Double.NN.Linear (Linear(..))
-import qualified Torch.Double.NN           as NN
-import qualified Torch.Double.NN.Linear    as Linear
-import qualified Torch.Long                as Long
-import qualified Torch.Long.Dynamic        as LDynamic
+-- Torch deps
+import Torch.Double
+  ( Tensor, HsReal                    -- Torch.Sig.Types
+  , unsafeMatrix, unsafeVector, get1d -- Torch.Indef.Static.Tensor
+  , constant                          -- Torch.Indef.Static.Tensor.Math
+  , (^*)                              -- Torch.Indef.Static.Pairwise
+  , uniform, ord2Tuple                -- Torch.Indef.Math.Random.TH
+  , manualSeed, newRNG                -- Torch.Core.Random
+  , eqTensor                          -- Torch.Indef.Static.CompareT
+  , allOf                             -- Torch.Indef.Mask
+  , mSECriterionIO                    -- Torch.Indef.Static.NN.Criterion
+  )
+import Torch.Double.NN.Linear (Linear(Linear))
 
 type MLP2 i h o = (Linear i h, Linear h o)
 
 type XORArch = MLP2 2 2 1
-
-xorForward
-  :: HsReal
-  -> XORArch
-  -> Tensor '[4, 2]  -- ^ input
-  -> IO (Tensor '[4, 1], Tensor '[4, 1] -> IO (XORArch, Tensor '[4, 2]))
-xorForward lr (l1, l2) i = do
-  (fc1out,    getl1gin) <- linearBatchIO 1 l1 i
-  (reluout, getrelugin) <- reluIO fc1out
-  (fc2out,    getl2gin) <- linearBatchIO 1 l2 reluout
-
-  pure (fc2out, \gout -> do
-    (l2, gin2) <- getl2gin gout
-    (l1, gin1) <- getl1gin =<< getrelugin gin2
-    pure ((l1, l2), gin1))
-
-loss :: HsReal -> XORArch -> Tensor '[4, 1] -> Tensor '[4, 2] -> IO (Tensor '[1], XORArch)
-loss = criterion mSECriterionIO xorForward
-
-update :: XORArch -> (HsReal, XORArch) -> XORArch
-update (l1, l2) (lr, (g1, g2)) = (l1 + (g1 ^* lr), l2 + (g2 ^* lr))
-
-mkExact :: IO XORArch
-mkExact = do
-  b1 <- unsafeVector [0, -1]
-  w2 <- unsafeMatrix [[1], [-2]]
-  pure (Linear (constant 1, b1), Linear (w2, constant 0))
-
-mkUniform :: IO XORArch
-mkUniform = do
-  g <- newRNG
-  manualSeed g 1
-  let Just rg = ord2Tuple (-3, 3)
-  l1 <- fmap Linear $ (,) <$> uniform g rg <*> uniform g rg
-  l2 <- fmap Linear $ (,) <$> uniform g rg <*> uniform g rg
-  pure (l1, l2)
-
-mkExactData :: IO (Tensor '[4, 2], Tensor '[4, 1])
-mkExactData =
-  (,)
-  <$> unsafeMatrix
-    [ [0, 0]
-    , [0, 1]
-    , [1, 0]
-    , [1, 1]
-    ]
-  <*>  unsafeMatrix
-    [ [0]
-    , [1]
-    , [1]
-    , [0]
-    ]
-
-mkRandomXOR :: GenST s -> ST s ((HsReal, HsReal), HsReal)
-mkRandomXOR g = do
-  x0 <- MWC.uniformR (0, 1) g
-  x1 <- MWC.uniformR (0, 1) g
-  pure ((fromIntegral x0, fromIntegral x1), x0 `xor` x1)
- where
-  xor :: Int -> Int -> HsReal
-  xor a b
-    = fromIntegral
-    . fromEnum
-    . odd
-    $ a + b
-
--- the formatting is a little funky
-xorLists :: GenST s -> Int -> ST s ([[HsReal]], [[HsReal]])
-xorLists g n =
-  foldl
-    (\memo ((x0, x1), y) -> ([x0,x1]:fst memo, [y]:snd memo))
-    ([], [])
-  <$>
-    replicateM n (mkRandomXOR g)
-
-xorBatchList
-  :: MWC.Seed -> Int -> (MWC.Seed, [[HsReal]], [[HsReal]])
-xorBatchList s n = runST $ do
-  g <- MWC.restore s
-  (xs, ys) <- xorLists g 4
-  s' <- MWC.save g
-  pure (s', xs, ys)
-
-xorBatch
-  :: MWC.Seed -> IO (MWC.Seed, Tensor '[4, 2], Tensor '[4, 1])
-xorBatch s =
-  (s',,) <$> unsafeMatrix xs <*> unsafeMatrix ys
- where
-  (s', xs, ys) = xorBatchList s 4
-
 
 main :: IO ()
 main = do
@@ -166,5 +81,112 @@ main = do
         (_, grad) <- loss lr net ys xs
         trainer lr n (c+1, s', update net (lr, grad))
 
+
+-- Forward + AD
+xorForward
+  :: HsReal
+  -> XORArch
+  -> Tensor '[4, 2]  -- ^ input
+  -> IO (Tensor '[4, 1], Tensor '[4, 1] -> IO (XORArch, Tensor '[4, 2]))
+xorForward lr (l1, l2) i = do
+  (fc1out,    getl1gin) <- linearBatchIO 1 l1 i
+  (reluout, getrelugin) <- reluIO fc1out
+  (fc2out,    getl2gin) <- linearBatchIO 1 l2 reluout
+
+  pure (fc2out, \gout -> do
+    (l2, gin2) <- getl2gin gout
+    (l1, gin1) <- getl1gin =<< getrelugin gin2
+    pure ((l1, l2), gin1))
+
+-- Forward + AD composed with the loss calculation
+loss :: HsReal -> XORArch -> Tensor '[4, 1] -> Tensor '[4, 2] -> IO (Tensor '[1], XORArch)
+loss lr net ys xs = do
+  (out, getArchGrad) <- xorForward lr net xs
+  (loss, getLossGrad) <- mSECriterionIO ys out
+  gnet <- fmap fst . getArchGrad =<< getLossGrad loss
+  pure (loss, gnet)
+
+-- Simple way to update a network with a multiple of the gradient
+update :: XORArch -> (HsReal, XORArch) -> XORArch
+update (l1, l2) (lr, (g1, g2)) = (l1 + (g1 ^* lr), l2 + (g2 ^* lr))
+
+-- ========================================================================= --
+-- Initialize an architecture
+-- ========================================================================= --
+
+-- make the exact solution to this architecture
+mkExact :: IO XORArch
+mkExact = do
+  b1 <- unsafeVector [0, -1]
+  w2 <- unsafeMatrix [[1], [-2]]
+  pure (Linear (constant 1, b1), Linear (w2, constant 0))
+
+-- make the stochastic initial weights for this architecture
+--
+-- FIXME: may require tuning
+mkUniform :: IO XORArch
+mkUniform = do
+  g <- newRNG
+  manualSeed g 1
+  let Just rg = ord2Tuple (-3, 3)
+  l1 <- fmap Linear $ (,) <$> uniform g rg <*> uniform g rg
+  l2 <- fmap Linear $ (,) <$> uniform g rg <*> uniform g rg
+  pure (l1, l2)
+
+-- ========================================================================= --
+-- Build datasets
+-- ========================================================================= --
+
+-- make an exact datapoint of all 4 examples of XOR
+mkExactData :: IO (Tensor '[4, 2], Tensor '[4, 1])
+mkExactData =
+  (,)
+  <$> unsafeMatrix
+    [ [0, 0]
+    , [0, 1]
+    , [1, 0]
+    , [1, 1]
+    ]
+  <*>  unsafeMatrix
+    [ [0]
+    , [1]
+    , [1]
+    , [0]
+    ]
+
+-- make a trainable batch of XOR samples (with uniform distribution of the 4 examples)
+xorBatch
+  :: MWC.Seed
+  -> IO (MWC.Seed, Tensor '[4, 2], Tensor '[4, 1])
+xorBatch s =
+  (s',,) <$> unsafeMatrix xs <*> unsafeMatrix ys
+ where
+  (s', xs, ys) = runST $ do
+    g <- MWC.restore s
+    (xs, ys) <- xorLists g 4
+    (, xs, ys) <$> MWC.save g
+
+  -- Make n-number of XOR examples in ST
+  xorLists :: GenST s -> Int -> ST s ([[HsReal]], [[HsReal]])
+  xorLists g n =
+    foldl
+      (\memo ((x0, x1), y) -> ([x0,x1]:fst memo, [y]:snd memo))
+      ([], [])
+    <$>
+      replicateM n (mkRandomXOR g)
+
+  -- make a uniform random datapoint of XOR
+  mkRandomXOR :: GenST s -> ST s ((HsReal, HsReal), HsReal)
+  mkRandomXOR g = do
+    x0 <- MWC.uniformR (0, 1) g
+    x1 <- MWC.uniformR (0, 1) g
+    pure ((fromIntegral x0, fromIntegral x1), x0 `xor` x1)
+   where
+    xor :: Int -> Int -> HsReal
+    xor a b
+      = fromIntegral
+      . fromEnum
+      . odd
+      $ a + b
 
 
