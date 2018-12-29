9b9ca20a01b0f9342736379a4716fcc6bbb5d1dc
austinvhuang <austinh@alum.mit.edu>
2018-12-29 15:44:09 -0500

    initialize repo, add latest pytorch commit as submodule, pull-in dependencies, link to yaml specs

---
+++ src/Main.hs
@@ -0,0 +1,10 @@
+module Main where
+
+import Data.Yaml
+import qualified Language.C.Inline.Cpp as C
+import qualified Language.C.Inline.Cpp.Exceptions as C
+import Text.Megaparsec
+
+main :: IO ()
+main = do
+  putStrLn "Done"
