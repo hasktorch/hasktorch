f00b0d6fe607128c432127ac7fb184c4d95927fc
austinvhuang <austinh@alum.mit.edu>
2018-12-29 16:46:29 -0500

    prototype yaml parser data model

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,58 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import GHC.Generics
 import Data.Yaml
+
+import qualified Data.Yaml as Y
 import qualified Language.C.Inline.Cpp as C
 import qualified Language.C.Inline.Cpp.Exceptions as C
-import Text.Megaparsec
+import qualified Text.Megaparsec as M
+import Text.Show.Prettyprint
+
+{- native_functions.yaml -}
+
+data Dispatch = Dispatch {
+  cpu :: Maybe String -- FIXME: how to use generics with capital "CPU"?
+  , gpu :: Maybe String -- FIXME: how to use generics with capital "GPU"?
+  , sparseCPU :: Maybe String
+  , sparseCUDA :: Maybe String
+} deriving (Show, Generic)
+
+data NativeFunction = NativeFunction {
+  func :: String
+  , variants :: Maybe String
+  , python_module :: Maybe String
+  , device_guard :: Maybe Bool
+  , dispatch :: Maybe Dispatch
+  , requires_tensor :: Maybe Bool
+} deriving (Show, Generic)
+
+instance FromJSON Function
+instance FromJSON Dispatch
+
+{- derivatives.yaml -}
+
+data Derivative = Derivative {
+  name :: String
+  , grad_output :: Maybe String
+  , output_differentiability :: [Bool]
+  , self :: Maybe String
+  , tensors :: Maybe String
+
+} deriving (Show, Generic)
+
+instance FromJSON Dispatch
+
+{- Execution -}
 
 main :: IO ()
 main = do
+  file <- Y.decodeFileEither "spec/small_test.yaml" :: IO (Either ParseException [Function])
+  -- putStrLn (maybe "Error" show file)
+  -- putStrLn (show file)
+  prettyPrint file
+
   putStrLn "Done"
