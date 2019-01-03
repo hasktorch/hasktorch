86726e1a1961a8d55905f1bd2e90b3ee9c076564
austinvhuang <austinh@alum.mit.edu>
2019-01-02 23:22:36 -0500

    re-organized libtorch into its own directories, clean up libtorch-test build pipeline scripts

---
+++ deps/clean-deps.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -eu
+
+rm -rf libtorch
+
+case "$(uname)" in
+  "Darwin")
+    rm -rf mklml_mac_2019.0.1.20181227
+    ;;
+  "Linux")
+    rm -rf mklml_lnx_2019.0.1.20181227
+    ;;
+esac
--- deps/get-deps.sh
+++ deps/get-deps.sh
@@ -3,9 +3,8 @@
 # - Retrieves a prebuilt libtorch binary per https://pytorch.org/cppdocs/installing.html
 # - Retrieves a release binary for mkl https://github.com/intel/mkl-dnn/releases
 #   which is a runtime dependency that is not package w/ libtorch
-# - Sets LD_LIBRARY_PATH (if run using `source`)
 
-printf "Note: run this script using 'source ./get-deps.sh' to set LD_LIBRARY_PATH\n====\n\n"
+set -eu
 
 case "$(uname)" in
   "Darwin")
@@ -14,7 +13,8 @@ case "$(uname)" in
     rm libtorch-macos-1.0.0.zip
     wget https://github.com/intel/mkl-dnn/releases/download/v0.17.2/mklml_mac_2019.0.1.20181227.tgz
     tar -xzf mklml_mac_2019.0.1.20181227.tgz
-    export LD_LIBRARY_PATH=$(pwd)/mklml_mac_2019.0.1.20181227/lib:$(pwd)/libtorch/lib:$LD_LIBRARY_PATH
+    rm -f mklml_mac_2019.0.1.20181227.tgz
+    rm -f mklml_mac_2019.0.1.20181227.tgz.1
     ;;
   "Linux")
     wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
@@ -22,6 +22,7 @@ case "$(uname)" in
     rm libtorch-shared-with-deps-latest.zip
     wget https://github.com/intel/mkl-dnn/releases/download/v0.17.2/mklml_lnx_2019.0.1.20181227.tgz
     tar -xzf mklml_lnx_2019.0.1.20181227.tgz
-    export LD_LIBRARY_PATH=$(pwd)/mklml_lnx_2019.0.1.20181227/lib:$(pwd)/libtorch/lib:$LD_LIBRARY_PATH
+    rm -f mklml_lnx_2019.0.1.20181227.tgz
+    rm -f mklml_mac_2019.0.1.20181227.tgz.1
     ;;
 esac
