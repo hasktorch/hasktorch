bc62cef2e87fda238686ba167e4f7bafe980e44a
Sam Stites <sam@stites.io>
2019-01-28 17:22:29 -0500

    use cuda-9 in hasktorch-ffi

--- build.nix
+++ build.nix
@@ -2,10 +2,38 @@
 let
   config = {
     allowUnfree = true;
-    packageOverrides = pkgs: rec {
+    packageOverrides = pkgs: let
+
+      # cudatoolkit_9_0 must use gcc < 6
+      stdenv5 = pkgs.overrideCC pkgs.stdenv pkgs.gcc5;
+      gfortran-gcc5 = pkgs.wrapCC (pkgs.gcc5.cc.override {
+          name = "gfortran-gcc5";
+          langFortran = true;
+          langCC = false;
+          langC = false;
+          langObjC = false;
+          langObjCpp = false;
+          profiledCompiler = false;
+      });
+
+    in rec {
+      cmake_3_12_2 = pkgs.libsForQt5.callPackage ./ffi/deps/cmake-nix-3_12_2 {};
+      magma = (pkgs.callPackage ./ffi/deps/magma-nix {}).magma.override {
+        mklSupport = true;
+        cudatoolkit = pkgs.cudatoolkit_9_0;
+        cmake = cmake_3_12_2;
+        stdenv = stdenv5;
+        gfortran = gfortran-gcc5;
+      };
+
       hasktorch-aten =
-        pkgs.callPackage ./ffi/deps/hasktorch-aten.nix
-          { inherit (pkgs.python36Packages) typing pyaml; inherit cudaSupport mklSupport; };
+        pkgs.callPackage ./ffi/deps/hasktorch-aten.nix {
+          inherit (pkgs.python36Packages) typing pyaml;
+          inherit cudaSupport mklSupport;
+          magma_cudatoolkit_9_0 = magma;
+          stdenv = stdenv5;
+          gfortran = gfortran-gcc5;
+        };
 
       haskell = pkgs.haskell // {
         packages = pkgs.haskell.packages // {
