94e4cc42df4461f331af3e867386950bb4e650ea
Adam Paszke <adam.paszke@gmail.com>
2019-05-30 09:38:40 +0200

    Update libtorch URL for macOS to 1.1 (#40)

--- deps/get-deps.sh
+++ deps/get-deps.sh
@@ -8,9 +8,9 @@ set -eu
 
 case "$(uname)" in
   "Darwin")
-    wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.0.0.zip
-    unzip libtorch-macos-1.0.0.zip
-    rm libtorch-macos-1.0.0.zip
+    wget https://download.pytorch.org/libtorch/cpu/libtorch-macos-1.1.0.zip
+    unzip libtorch-macos-1.1.0.zip
+    rm libtorch-macos-1.1.0.zip
     wget https://github.com/intel/mkl-dnn/releases/download/v0.17.2/mklml_mac_2019.0.1.20181227.tgz
     tar -xzf mklml_mac_2019.0.1.20181227.tgz
     rm -f mklml_mac_2019.0.1.20181227.tgz
