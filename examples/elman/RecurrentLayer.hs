c7bd3105436bc0eb24fa78b45bb7e2be6d614eef
AdLucem <ghosalatreyee@gmail.com>
2019-06-18 18:26:43 +0530

    the elman cell example works for one timestep

--- examples/elman/RecurrentLayer.hs
+++ examples/elman/RecurrentLayer.hs
@@ -10,10 +10,22 @@ import Torch.Functions
 import Torch.TensorOptions
 import Torch.Autograd
 
+import ATen.Cast
+import qualified ATen.Managed.Native as ATen
+
+import System.IO.Unsafe
 import Control.Monad.State.Strict
 import Data.List (foldl', scanl', intersperse)
 
 import Parameters
+
+
+transpose :: Tensor -> Int -> Int -> Tensor
+transpose t a b = unsafePerformIO $ (cast3 ATen.transpose_tll) t a b
+
+
+transpose2D :: Tensor -> Tensor
+transpose2D t = transpose t 0 1
 
 
 data RecurrentSpec = RecurrentSpec { in_features :: Int, hidden_features :: Int, nonlinearitySpec :: Tensor -> Tensor }
@@ -27,9 +39,9 @@ data Recurrent = Recurrent { weight_ih :: Parameter, bias_ih :: Parameter,
 instance Randomizable RecurrentSpec Recurrent where
   sample RecurrentSpec{..} = do
       w_ih <- makeIndependent =<< randn' [in_features, hidden_features]
-      b_ih <- makeIndependent =<< randn' [in_features]
+      b_ih <- makeIndependent =<< randn' [1, in_features]
       w_hh <- makeIndependent =<< randn' [hidden_features, hidden_features]
-      b_hh <- makeIndependent =<< randn' [hidden_features]
+      b_hh <- makeIndependent =<< randn' [1, hidden_features]
       return $ Recurrent w_ih b_ih w_hh b_hh
 
 
@@ -49,11 +61,24 @@ instance Show Recurrent where
     (show $ toDependent weight_hh) ++ "\n" ++
     (show $ toDependent bias_hh)
 
+
 recurrent :: Recurrent -> Tensor -> Tensor -> Tensor
 recurrent Recurrent{..} input hidden =
-  Torch.Functions.tanh (ih + hh)
+  h' (inputGate weight_ih bias_ih input) 
+     (hiddenGate weight_hh bias_hh hidden)
+
   where
-    ih = (matmul input (toDependent weight_ih)) + (toDependent bias_ih)
-    hh = (matmul hidden (toDependent weight_hh)) + (toDependent bias_hh)
 
+    inputGate weight bias input = 
+      (matmul 
+        (toDependent weight) 
+        (transpose2D input)) + 
+      (transpose2D $ toDependent bias)
 
+    hiddenGate weight bias hidden = 
+      (matmul 
+        (toDependent weight) 
+        (transpose2D hidden)) + 
+      (transpose2D $ toDependent bias)
+
+    h' ig hg = transpose2D $ Torch.Functions.tanh (ig + hg)
