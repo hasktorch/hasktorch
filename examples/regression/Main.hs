6d43ae2aec0408f309d5d6e97657302afd7d8e97
austinvhuang <austinh@alum.mit.edu>
2019-06-24 23:50:16 -0400

    move linear layer defn and sgd to hasktorch/, use appropriate constructors for independent tensors

--- examples/regression/Main.hs
+++ examples/regression/Main.hs
@@ -5,78 +5,52 @@ module Main where
 
 import Control.Monad (foldM)
 
-import Torch.Tensor (Tensor)
+import Torch.Tensor
 import Torch.DType (DType (Float))
 import Torch.TensorFactories (ones', rand', randn')
 import Torch.Functions
 import Torch.Autograd
 import Torch.NN
 
-{- Types -}
+batch_size = 64
+num_iters = 10000
+num_features = 3
 
-data LinearSpec = LinearSpec { in_features :: Int, out_features :: Int }
-  deriving (Show, Eq)
-
-data Linear = Linear { weight :: Parameter, bias :: Parameter } deriving Show
-
-{- Instances -}
-
-instance Randomizable LinearSpec Linear where
-  sample LinearSpec{..} = do
-      w <- makeIndependent =<< randn' [in_features, out_features]
-      b <- makeIndependent =<< randn' [out_features]
-      return $ Linear w b
-
-instance Parameterized Linear where
-  flattenParameters Linear{..} = [weight, bias]
-  replaceOwnParameters _ = do
-    weight <- nextParameter
-    bias <- nextParameter
-    return $ Linear{..}
-
-{- Forward functions -}
-
-linear :: Linear -> Tensor -> Tensor
-linear Linear{..} input = (matmul input dWeight) + dBias
+model :: Linear -> Tensor -> Tensor
+model Linear{..} input = (matmul input depWeight) + depBias
   where
-    (dWeight, dBias) = (toDependent weight, toDependent bias)
-  
-{- Optimization -}
+    (depWeight, depBias) = (toDependent weight, toDependent bias)
 
-sgd :: Tensor -> [Parameter] -> [Tensor] -> [Tensor]
-sgd lr parameters gradients = zipWith step depParameters gradients
-  where 
-    step p dp = p - (lr * dp)
-    depParameters = (map toDependent parameters)
+groundTruth :: Tensor -> IO Tensor
+groundTruth t = do 
+    weight <- makeIndependent $ 5.0 * ones' [num_features, 1]
+    bias <- makeIndependent $ 2.5 * ones' []
+    pure $ model Linear { weight = weight, bias = bias} t
 
-{- Main -}
+printParams :: Linear -> IO ()
+printParams trained = do
+    putStrLn "Parameters:"
+    print $ toDependent $ weight trained
+    putStrLn "Bias:"
+    print $ toDependent $ bias trained
 
 main :: IO ()
 main = do
     init <- sample $ LinearSpec { in_features = num_features, out_features = 1 } 
     trained <- foldLoop init num_iters $ \state i -> do
-        input <- rand' [batch_size, num_features] >>= return . (toDType Float)
-        let expected_output = squeezeAll $ groundTruth input
-        let output = squeezeAll $ linear state input
-        let loss = mse_loss output expected_output
-        let flat_parameters = flattenParameters state
-        let gradients = grad loss flat_parameters
+        input <- randn' [batch_size, num_features] >>= return . (toDType Float)
+        expected_output <- squeezeAll <$> groundTruth input
+        let output = squeezeAll $ model state input
+            loss = mse_loss output expected_output
+            flat_parameters = flattenParameters state
+            gradients = grad loss flat_parameters
         if i `mod` 500 == 0 then
           putStrLn $ "Loss: " ++ show loss
         else
           pure ()
         new_flat_parameters <- mapM makeIndependent $ sgd 5e-3 flat_parameters gradients
         return $ replaceParameters state $ new_flat_parameters
-    putStrLn "Parameters:"
-    print $ toDependent $ weight trained
-    putStrLn "Bias:"
-    print $ toDependent $ bias trained
+    printParams trained
     pure ()
   where
-    batch_size = 64
-    num_iters = 10000
-    num_features = 3
     foldLoop x count block = foldM block x [1..count]
-    groundTruth :: Tensor -> Tensor
-    groundTruth = linear Linear { weight = IndependentTensor $ 5.0 * ones' [num_features, 1],
-                                    bias = IndependentTensor $ 2.5 * ones' [] }
--- hasktorch/src/Torch/NN.hs
+++ hasktorch/src/Torch/NN.hs
@@ -1,11 +1,15 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Torch.NN where
 
 import Control.Monad.State.Strict
 
 import Torch.Autograd
+import Torch.Tensor
+import Torch.TensorFactories (ones', rand', randn')
+import Torch.Functions
 
 type Parameter = IndependentTensor
 type ParamStream a = State [Parameter] a
@@ -32,3 +36,27 @@ class Randomizable spec f | spec -> f where
   sample :: spec -> IO f
 
 class (Randomizable spec f, Parameterized f) => Module spec f
+
+data LinearSpec = LinearSpec { in_features :: Int, out_features :: Int }
+  deriving (Show, Eq)
+
+data Linear = Linear { weight :: Parameter, bias :: Parameter } deriving Show
+
+instance Randomizable LinearSpec Linear where
+  sample LinearSpec{..} = do
+      w <- makeIndependent =<< randn' [in_features, out_features]
+      b <- makeIndependent =<< randn' [out_features]
+      return $ Linear w b
+
+instance Parameterized Linear where
+  flattenParameters Linear{..} = [weight, bias]
+  replaceOwnParameters _ = do
+    weight <- nextParameter
+    bias <- nextParameter
+    return $ Linear{..}
+
+sgd :: Tensor -> [Parameter] -> [Tensor] -> [Tensor]
+sgd lr parameters gradients = zipWith step depParameters gradients
+  where 
+    step p dp = p - (lr * dp)
+    depParameters = (map toDependent parameters)
