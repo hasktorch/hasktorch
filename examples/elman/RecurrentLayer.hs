13c60dbfe8e4a71a55574d7f5017036d71e53abf
AdLucem <ghosalatreyee@gmail.com>
2019-07-08 23:20:53 +0530

    wrote types and ff functions for gru, elman, lstm

--- examples/elman/RecurrentLayer.hs
+++ examples/elman/RecurrentLayer.hs
@@ -14,88 +14,79 @@ import Torch.NN
 import Control.Monad.State.Strict
 import Data.List (foldl', scanl', intersperse)
 
+------------- General Recurrent Structures ------------------
+class RecurrentCell a where
+  -- cell ff function written by hand,
+  -- to demonstrate how the cell works
+  nextState :: a -> Tensor -> Tensor -> Tensor
+  -- TODO: there should also be a `forward` function here
+  -- that uses the rnn forward functions from ATen
+  -- but I'll implement that when I can make sense
+  -- of the ATen function arguments
 
-class (Randomizable a, Parameterized a) => Recurrent a where
-    run :: a -> Tensor -> Tensor -> Tensor
-    runOverTimesteps :: a -> Tensor -> Tensor -> Tensor
+
+data RecurrentGate = RecurrentGate {
+  inputWt :: Parameter,
+  hiddenWt :: Parameter,
+  bias :: Parameter
+  nonLinearity :: Tensor -> Tensor
+}
+
+gate :: RecurrentGate -> Tensor -> Tensor -> Tensor
+gate RecurrentGate{..} input hidden = 
+  nonLinearity $ (mul input inputWt) + (mul hidden hiddenWt) + bias 
+  where
+    mul features wts = matmul (toDependent weight) (transpose2D features)
+------------------------------------------------------------
 
 
 -- Specifying the shape of the recurrent layer
 data RecurrentSpec = RecurrentSpec { in_features :: Int, hidden_features :: Int, nonlinearitySpec :: Tensor -> Tensor }
 
--- Recurrent layer type holding the weights for the layer
---data Recurrent = Recurrent { weight_ih :: Parameter, bias_ih :: Parameter,
---                             weight_hh :: Parameter, bias_hh :: Parameter,
---                             nonLinearity :: Tensor -> Tensor
---                        }
+----------------- Elman Cell -------------------
+type ElmanCell = RecurrentGate                            }
 
+instance RecurrentCell ElmanCell where
+  nextState cell input hidden = gate cell input hidden
+------------------------------------------------
 
-data GRUCell = GRUCell {weight_ir :: Parameter, bias_ir :: Parameter,
-                        weight_iz :: Parameter, bias_iz :: Parameter,
-                        weight_in :: Parameter, bias_in :: Parameter,
-                        weight_hr :: Parameter, bias_hr :: Parameter,
-                        weight_hz :: Parameter, bias_hz :: Parameter,
-                        weight_hn :: Parameter, bias_hn :: Parameter
-                      }
+----------------- LSTM Cell ---------------------
+data LSTMCell = LSTMCell {
+  input_gate   :: RecurrentGate,
+  forget_gate  :: RecurrentGate,
+  output_gate  :: RecurrentGate,
+  hidden_gate  :: RecurrentGate,
+  cell_state   :: Parameter 
+}
 
-
--- Typeclass that shows that the layer weights can be randomly initialized
-instance Randomizable RecurrentSpec Recurrent where
-  sample RecurrentSpec{..} = do
-      w_ih <- makeIndependent =<< randn' [in_features, hidden_features]
-      b_ih <- makeIndependent =<< randn' [1, in_features]
-      w_hh <- makeIndependent =<< randn' [hidden_features, hidden_features]
-      b_hh <- makeIndependent =<< randn' [1, hidden_features]
-      return $ Recurrent w_ih b_ih w_hh b_hh nonlinearitySpec
-
-
--- Typeclass that allows us to manipulate and update the layer weights
-instance Parameterized Recurrent where
-  flattenParameters Recurrent{..} = [weight_ih, bias_ih, weight_hh, bias_hh]
-  replaceOwnParameters _ = do
-    weight_ih <- nextParameter
-    bias_ih   <- nextParameter
-    weight_hh <- nextParameter
-    bias_hh   <- nextParameter
-    -- TODO: how do I get around this??
-    let nonLinearity = Torch.Functions.tanh
-    return $ Recurrent{..}
-
-instance Show Recurrent where
-  show Recurrent{..} =
-    (show $ toDependent weight_ih) ++ "\n" ++
-    (show $ toDependent bias_ih) ++ "\n" ++
-    (show $ toDependent weight_hh) ++ "\n" ++
-    (show $ toDependent bias_hh)
-
-
--- The layer 'function', i.e: passes an input through the layer
--- and returns output
-recurrent :: Recurrent  -- current state of layer
-          -> Tensor     -- input tensor
-          -> Tensor     -- previous hidden state
-          -> Tensor     -- output tensor
-recurrent Recurrent{..} input hidden =
-  h' (linear weight_ih bias_ih input)
-     (linear weight_hh bias_hh hidden)
-
+newCellState :: LSTMCell -> Tensor -> Tensor -> Tensor
+newCellState LSTMCell{..} input hidden =
+  (fg * cell_state) + (ig * c')
   where
+    ig = gate input_gate input hidden
+    fg = gate forget_gate input hidden
+    c' = gate hidden_gate input hidden
 
-    linear weight bias features = 
-      (matmul
-        (toDependent weight)
-        (transpose2D features)) +
-      (transpose2D $ toDependent bias)
-
-    -- combined result of input and hidden gate
-    h' ig hg = transpose2D $ nonLinearity (ig + hg)
-
-
--- Running the same layer over multiple timesteps
--- where no. of timesteps <= length of input sequence
-runOverTimesteps :: Tensor -> Recurrent -> Int -> Tensor -> Tensor
-runOverTimesteps inp layer 0 hidden = hidden
-runOverTimesteps inp layer n hidden =
-    runOverTimesteps inp layer (n-1) $ recurrent layer inp' hidden
+instance RecurrentCell LSTMCell where
+  nextState cell input hidden =
+    og * (Torch.Functions.tanh cNew)  
     where
-        inp' = reshape (select inp 0 (n-1)) [1, 2]
+      og = gate (output_gate cell) input hidden
+      cNew = newCellState cell input hidden
+------------------------------------------------
+
+------------------ GRU Cell ----------------------
+data GRUCell = GRUCell {
+  reset_gate :: RecurrentGate,
+  update_gate :: RecurrentGate,
+  hidden_gate :: RecurrentGate
+}
+
+instance RecurrentCell GRUCell where
+  nextState GRUCell{..} input hidden =
+    (ug * hidden) + ((1 - ug) * h')
+    where 
+      rg = gate reset_gate input hidden
+      ug = gate update_gate input hidden
+      h' = gate hidden_gate input (rg * hidden)
+--------------------------------------------------
