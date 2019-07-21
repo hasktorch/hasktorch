0b047faeaa0df3c53fcd26afb0e552a248981880
austinvhuang <austinh@alum.mit.edu>
2019-07-21 12:03:30 -0400

    get rid of global constants for gp data, add function signatures

--- examples/gaussian_process/Main.hs
+++ examples/gaussian_process/Main.hs
@@ -14,16 +14,6 @@ import Torch.Functions
 import Torch.Autograd
 import Torch.NN
 
--- Axis points
-axisDim = 7
-tRange = (*) scale <$> (fromIntegral <$> [0 .. (axisDim - 1)])
-    where scale = 0.1
-
--- Observed data points
-dataDim = 3
-dataPredictors = [0.1, 0.3, 0.6] :: [Float]
-dataValues = [-2.3, 1.5, -4] :: [Float]
-
 -- | construct pairs of points on the axis
 makeAxis :: [Float] -> [Float] -> (Tensor, Tensor)
 makeAxis axis1 axis2 = (t, t')
@@ -42,18 +32,21 @@ kernel1d_rbf sigma length t t' = (sigma'^2) * exp eterm
 
 -- | derive a covariance matrix from the kernel for points on the axis
 makeCovmatrix :: [Float] -> [Float] -> Tensor
-makeCovmatrix axis1 axis2 = reshape (kernel1d_rbf 1.0 1.0 t t') [length axis1, length axis2]
+makeCovmatrix axis1 axis2 = 
+    reshape (kernel1d_rbf 1.0 1.0 t t') [length axis1, length axis2]
     where
       (t, t') = makeAxis axis1 axis2
 
 -- | Multivariate 0-mean normal via cholesky decomposition
-mvnCholesky :: Tensor -> Int -> IO Tensor
-mvnCholesky cov n = do
+mvnCholesky :: Tensor -> Int -> Int -> IO Tensor
+mvnCholesky cov axisDim n = do
     samples <- randn' [axisDim, n]
     pure $ matmul l samples
     where 
       l = cholesky cov Upper
 
+-- | Compute posterior mean and covariance parameters based on observed data y
+condition :: Tensor -> Tensor -> Tensor -> Tensor -> Tensor -> Tensor -> (Tensor, Tensor)
 condition muX muY covXX covXY covYY y =
     (postMu, postCov)
     where
@@ -63,7 +56,11 @@ condition muX muY covXX covXY covYY y =
         postCov = covXX - (matmul covXY (matmul invY covYX))
 
 
-computePosterior dataPredictors dataValues = do
+-- | Given observations + points of interest derive covariance terms and condition on observation
+computePosterior :: [Float] -> [Float] -> [Float] -> IO (Tensor, Tensor)
+computePosterior dataPredictors dataValues tRange = do
+    let dataDim = length dataPredictors
+    let axisDim = length tRange
 
     -- multivariate normal parameters for axis locations
     let priorMuAxis = zeros' [axisDim, 1]
@@ -85,31 +82,39 @@ computePosterior dataPredictors dataValues = do
                 priorMuAxis priorMuData 
                 priorCov crossCov obsCov
                 obsVals
-
     pure $ (postMu, postCov)
 
+main :: IO ()
 main = do
-
     -- Setup prediction axis
     let cov =  makeCovmatrix tRange tRange
     putStrLn $ "Predictor values\n" ++ show tRange
     putStrLn $ "\nCovariance based on radial basis function\n" ++ show cov
 
-    putStrLn "prior"
     -- Prior GP, take 4 example samples
+    putStrLn "prior"
     let reg = 0.01 * (eye'  axisDim axisDim) -- regularization
-    mvnSampPrior <- mvnCholesky (cov + reg) 4
+    mvnSampPrior <- mvnCholesky (cov + reg) axisDim 4
     putStrLn $ "\nGP Samples (prior,  rows = values, cols = realizations)\n"
         ++ show mvnSampPrior
 
     -- Observations
     putStrLn $ "\nObservations: predictor coordinates\n" ++ show dataPredictors
     putStrLn $ "\nObservations: values\n" ++ show dataValues
-    (postMu, postCov) <- computePosterior dataPredictors dataValues
+    (postMu, postCov) <- computePosterior dataPredictors dataValues tRange
 
     -- Conditional GP
     putStrLn $ "\nConditional mu (posterior)\n" ++ show postMu
     putStrLn $ "\nConditional covariance (posterior)\n" ++ show postCov
-    mvnSampPost <- mvnCholesky (postCov + reg) 1
+    mvnSampPost <- mvnCholesky (postCov + reg) (length tRange) 1
     putStrLn "\nGP Conditional Samples (posterior, rows = values, cols = realizations)"
     print (postMu + mvnSampPost)
+
+    where 
+      -- Axis points
+      scale = 0.1
+      axisDim = 7
+      tRange = (*) scale <$> (fromIntegral <$> [0 .. (axisDim - 1)])
+      -- Observed data points
+      dataPredictors = [0.1, 0.3, 0.6] :: [Float]
+      dataValues = [-2.3, 1.5, -4] :: [Float]
