bbcaee8d0b65a88b9913bb53b9045fa62b754fe5
Ian-Woo Kim <ianwookim@gmail.com>
2019-08-31 01:27:44 -0700

    pin nixpkgs revision.

--- shell.nix
+++ shell.nix
@@ -1,4 +1,14 @@
-{ pkgs ? import <nixpkgs> { config.allowUnfree = true; config.allowUnsupportedSystem = true; } }:
+{ pkgs ?
+   let
+     # revision: https://github.com/NixOS/nixpkgs/pull/65041
+     rev = "9420c33455c5b3e0876813bdd739bc75b3ccbd8a";
+     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+     # nix-prefetch-url --unpack ${url}
+     sha256 = "0l82qfcmip0mfijcxssaliyxayfh0c11bk66yhwna1ixc0s0jjd2";
+     nixpkgs = builtins.fetchTarball { inherit url sha256; };
+   in
+     import nixpkgs { config.allowUnfree = true; config.allowUnsupportedSystem = true; }
+}:
 
 with pkgs;
 
