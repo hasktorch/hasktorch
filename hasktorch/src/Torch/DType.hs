b3622674bd8e7ad56fd932d28b6f152a9ff3c5ba
Junji Hashimoto <junji.hashimoto@gmail.com>
2019-09-02 01:30:07 +0900

    Add Bool of DType

--- hasktorch/src/Torch/DType.hs
+++ hasktorch/src/Torch/DType.hs
@@ -12,8 +12,11 @@ import Data.Int
 import Data.Word
 import Data.Reflection
 
-data DType = UInt8 | Int8 | Int16 | Int32 | Int64 | Half | Float | Double
+data DType = Bool | UInt8 | Int8 | Int16 | Int32 | Int64 | Half | Float | Double
   deriving (Eq, Show)
+
+instance Reifies Bool DType where
+  reflect _ = Bool
 
 instance Reifies Word8 DType where
   reflect _ = UInt8
@@ -40,6 +43,7 @@ instance Reifies Double DType where
   reflect _ = Double
 
 instance Castable DType ATen.ScalarType where
+  cast Bool   f = f ATen.kBool
   cast UInt8  f = f ATen.kByte
   cast Int8   f = f ATen.kChar
   cast Int16  f = f ATen.kShort
@@ -50,6 +54,7 @@ instance Castable DType ATen.ScalarType where
   cast Double f = f ATen.kDouble
 
   uncast x f
+    | x == ATen.kBool = f Bool
     | x == ATen.kByte = f UInt8
     | x == ATen.kChar = f Int8
     | x == ATen.kShort = f Int16
@@ -61,6 +66,7 @@ instance Castable DType ATen.ScalarType where
 
 
 isIntegral :: DType -> Bool
+isIntegral Bool   = True
 isIntegral UInt8  = True
 isIntegral Int8   = True
 isIntegral Int16  = True
