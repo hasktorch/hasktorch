6c9530f5cb1ded31d7d05a9102e47ccbd58f1199
Torsten Scholak <torsten.scholak@googlemail.com>
2019-09-03 18:22:05 -0400

    fix doctests

--- hasktorch/test/doctests.hs
+++ hasktorch/test/doctests.hs
@@ -1,14 +1,27 @@
 module Main where
 
-import Test.DocTest
+import           Test.DocTest
 
 main :: IO ()
-main = do
-  doctest $
-    [
-      "-XOverloadedStrings",
-      "-XDataKinds",
-      "-XTypeFamilies",
-      "-XTypeApplications",
-      "src/Torch/Static/Native.hs"
-    ]
+main = doctest
+  [ "-XOverloadedStrings"
+  , "-XDataKinds"
+  , "-XTypeFamilies"
+  , "-XTypeApplications"
+      -- , "-isrc"
+  , "src/Torch/Cast.hs"
+  , "src/Torch/Layout.hs"
+  , "src/Torch/Backend.hs"
+  , "src/Torch/Scalar.hs"
+  , "src/Torch/DType.hs"
+  , "src/Torch/TensorOptions.hs"
+  , "src/Torch/Tensor.hs"
+  , "src/Torch/TensorFactories.hs"
+  , "src/Torch/Autograd.hs"
+  , "src/Torch/Functions/Native.hs"
+  , "src/Torch/Functions.hs"
+  , "src/Torch/NN.hs"
+  , "src/Torch/Static/Factories.hs"
+  , "src/Torch/Static.hs"
+  , "src/Torch/Static/Native.hs"
+  ]
