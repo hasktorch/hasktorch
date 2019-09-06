455ba9a93004d7cdc36c5772f7e5ffa595fa8046
Torsten Scholak <torsten.scholak@googlemail.com>
2019-09-05 20:11:08 -0400

    add hasktorch-naperian

--- default.nix
+++ default.nix
@@ -6,13 +6,16 @@ in
       libtorch-ffi_cpu
       hasktorch_cpu
       hasktorch-examples_cpu
+      hasktorch-naperian_cpu
     ;
 
     ${shared.nullIfDarwin "libtorch-ffi_cudatoolkit_9_2"} = shared.libtorch-ffi_cudatoolkit_9_2;
     ${shared.nullIfDarwin "hasktorch_cudatoolkit_9_2"} = shared.hasktorch_cudatoolkit_9_2;
     ${shared.nullIfDarwin "hasktorch-examples_cudatoolkit_9_2"} = shared.hasktorch-examples_cudatoolkit_9_2;
+    ${shared.nullIfDarwin "hasktorch-naperian_cudatoolkit_9_2"} = shared.hasktorch-naperian_cudatoolkit_9_2;
 
     ${shared.nullIfDarwin "libtorch-ffi_cudatoolkit_10_1"} = shared.libtorch-ffi_cudatoolkit_10_1;
     ${shared.nullIfDarwin "hasktorch_cudatoolkit_10_1"} = shared.hasktorch_cudatoolkit_10_1;
     ${shared.nullIfDarwin "hasktorch-examples_cudatoolkit_10_1"} = shared.hasktorch-examples_cudatoolkit_10_1;
+    ${shared.nullIfDarwin "hasktorch-naperian_cudatoolkit_10_1"} = shared.hasktorch-naperian_cudatoolkit_10_1;
   }
