843a61975a09db2df3caf1001743a62c07f1bdca
Junji Hashimoto <junji.hashimoto@gmail.com>
2019-09-13 19:57:56 +0900

    Use cabal-doctest for nix-build

--- hasktorch/test/doctests.hs
+++ hasktorch/test/doctests.hs
@@ -1,14 +1,18 @@
 module Main where
 
-import Test.DocTest
+import Build_doctests (flags, pkgs, module_sources)
+import Data.Foldable (traverse_)
+import Test.DocTest (doctest)
 
 main :: IO ()
 main = do
-  doctest $
-    [
+  traverse_ putStrLn args
+  doctest args
+  where
+    args = flags ++ pkgs ++ module_sources ++ [
       "-XOverloadedStrings",
       "-XDataKinds",
       "-XTypeFamilies",
-      "-XTypeApplications",
-      "src/Torch/Static/Native.hs"
-    ]
+      "-XTypeApplications"
+      ]
+
