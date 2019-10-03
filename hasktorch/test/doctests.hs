d73d3fc99f6da57092d8f55e3d5dc6f155e63cde
Torsten Scholak <torsten.scholak@googlemail.com>
2019-10-03 19:00:39 -0400

    add compiler plugins to doctests

--- hasktorch/test/doctests.hs
+++ hasktorch/test/doctests.hs
@@ -9,6 +9,10 @@ main = doctest
   , "-XDataKinds"
   , "-XTypeFamilies"
   , "-XTypeApplications"
+  , "-fplugin GHC.TypeLits.Normalise"
+  , "-fplugin GHC.TypeLits.KnownNat.Solver"
+  , "-fplugin GHC.TypeLits.Extra.Solver"
+  , "-fconstraint-solver-iterations=0"
   , "-isrc"
   , "src/Torch/Static/Native"
   ]
