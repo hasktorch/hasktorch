ae88b976e2d368208342f851b8bbd341f210ba84
Junji Hashimoto <junji.hashimoto@gree.net>
2019-10-07 13:31:04 +0900

    Recover and modifiy mnist-example to match current style

---
+++ examples/mnist/Image.hs
@@ -0,0 +1,76 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TupleSections #-}
+
+module Image where
+
+import Data.Int
+import Data.Word
+import Codec.Compression.GZip (decompress)
+import qualified Data.ByteString.Lazy as BS
+
+-- Torch deps
+
+import GHC.TypeLits
+
+import Torch.Static
+import Torch.Static.Native
+import qualified Torch.DType as D
+import qualified Torch.Tensor as D
+
+
+data MnistData =
+  MnistData
+  { images :: BS.ByteString
+  , labels :: BS.ByteString
+  }
+
+type DataDim = 784
+type ClassDim = 10
+
+getLabels :: forall n. KnownNat n => MnistData -> [Int] -> Tensor 'D.Float '[n,ClassDim]
+getLabels mnist imageIdxs = UnsafeMkTensor $ D.asTensor $ map (getLabel mnist) $ take (natValI @n) imageIdxs
+
+getLabel :: MnistData -> Int -> [Float]
+getLabel mnist imageIdx =
+  let v = fromIntegral $ BS.index (labels mnist) ((fromIntegral imageIdx) + 8) :: Word8
+  in case v of
+       1 -> [1,0,0,0,0,0,0,0,0,0]
+       2 -> [0,1,0,0,0,0,0,0,0,0]
+       3 -> [0,0,1,0,0,0,0,0,0,0]
+       4 -> [0,0,0,1,0,0,0,0,0,0]
+       5 -> [0,0,0,0,1,0,0,0,0,0]
+       6 -> [0,0,0,0,0,1,0,0,0,0]
+       7 -> [0,0,0,0,0,0,1,0,0,0]
+       8 -> [0,0,0,0,0,0,0,1,0,0]
+       9 -> [0,0,0,0,0,0,0,0,1,0]
+       _ -> [0,0,0,0,0,0,0,0,0,1]
+
+getImage :: MnistData -> Int -> Tensor 'D.Float '[DataDim]
+getImage mnist imageIdx =
+  let imageBS = [fromIntegral $ BS.index (images mnist) ((fromIntegral imageIdx) * 28^2 + 16 + r) | r <- [0..28^2 - 1]] :: [Double]
+      (tensor :: Tensor 'D.Float '[DataDim]) = UnsafeMkTensor $ D.asTensor imageBS
+  in tensor
+
+getImages :: forall n. KnownNat n => MnistData -> [Int] -> Tensor 'D.Float '[n, DataDim]
+getImages mnist imageIdxs = UnsafeMkTensor $ D.asTensor $ map image $ take (natValI @n) imageIdxs
+  where
+    image idx = [fromIntegral $ BS.index (images mnist) ((fromIntegral idx) * 28^2 + 16 + r) | r <- [0..28^2 - 1]] :: [Double]
+
+length :: MnistData -> Int
+length mnist = fromIntegral $ (BS.length (labels mnist)) - 8
+
+
+initMnist :: IO (MnistData, MnistData)
+initMnist = do
+  let path = "data"
+  imagesBS <- decompress <$> BS.readFile (path <>  "/" <> "train-images-idx3-ubyte.gz")
+  labelsBS <- decompress <$> BS.readFile (path <>  "/" <> "train-labels-idx1-ubyte.gz")
+  testImagesBS <- decompress <$> BS.readFile (path <>  "/" <> "t10k-images-idx3-ubyte.gz")
+  testLabelsBS <- decompress <$> BS.readFile (path <>  "/" <> "t10k-labels-idx1-ubyte.gz")
+  return (MnistData imagesBS labelsBS, MnistData testImagesBS testLabelsBS)
+
---
+++ examples/mnist/Main.hs
@@ -0,0 +1,138 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE UndecidableSuperClasses #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE ConstraintKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE NoStarIsType #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE OverloadedLists #-}
+
+module Main where
+
+import           Prelude                 hiding ( tanh )
+import           Control.Monad                  ( foldM
+                                                , when
+                                                )
+import           Data.List                      ( foldl'
+                                                , scanl'
+                                                , intersperse
+                                                )
+import           GHC.Generics
+import           GHC.TypeLits
+
+import           Torch.Static
+import           Torch.Static.Native     hiding ( linear )
+import           Torch.Static.Factories
+import qualified Torch.Autograd                as A
+import qualified Torch.NN                      as A
+import qualified Torch.DType                   as D
+import qualified Torch.Tensor                  as D
+import qualified Torch.Functions               as D
+import qualified Torch.TensorFactories         as D
+import qualified Image                         as I
+import           System.Random
+
+--------------------------------------------------------------------------------
+-- Multi-Layer Perceptron (MLP)
+--------------------------------------------------------------------------------
+
+
+newtype Parameter dtype shape = Parameter A.IndependentTensor deriving (Show)
+
+toDependent :: Parameter dtype shape -> Tensor dtype shape
+toDependent (Parameter t) = UnsafeMkTensor $ A.toDependent t
+
+instance A.Parameterized (Parameter dtype shape) where
+  flattenParameters (Parameter x) = [x]
+  replaceOwnParameters _ = Parameter <$> A.nextParameter
+
+data LinearSpec (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) = LinearSpec
+  deriving (Show, Eq)
+
+data Linear (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) =
+  Linear { weight :: Parameter dtype '[inputFeatures, outputFeatures]
+         , bias :: Parameter dtype '[outputFeatures]
+         } deriving (Show, Generic)
+
+linear
+  :: Linear dtype inputFeatures outputFeatures
+  -> Tensor dtype '[batchSize, inputFeatures]
+  -> Tensor dtype '[batchSize, outputFeatures]
+linear Linear {..} input =
+  add (mm input (toDependent weight)) (toDependent bias)
+
+makeIndependent :: Tensor dtype shape -> IO (Parameter dtype shape)
+makeIndependent t = Parameter <$> A.makeIndependent (toDynamic t)
+
+instance A.Parameterized (Linear dtype inputFeatures outputFeatures)
+
+instance (KnownDType dtype, KnownNat inputFeatures, KnownNat outputFeatures) => A.Randomizable (LinearSpec dtype inputFeatures outputFeatures) (Linear dtype inputFeatures outputFeatures) where
+  sample LinearSpec =
+    Linear <$> (makeIndependent =<< randn) <*> (makeIndependent =<< randn)
+
+data MLPSpec (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) (hiddenFeatures :: Nat) = MLPSpec
+
+data MLP (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) (hiddenFeatures :: Nat) =
+  MLP { layer0 :: Linear dtype inputFeatures hiddenFeatures
+      , layer1 :: Linear dtype hiddenFeatures hiddenFeatures
+      , layer2 :: Linear dtype hiddenFeatures outputFeatures
+      } deriving (Show, Generic)
+
+instance A.Parameterized (MLP dtype inputFeatures outputFeatures hiddenFeatures)
+
+instance (KnownDType dtype, KnownNat inputFeatures, KnownNat outputFeatures, KnownNat hiddenFeatures) => A.Randomizable (MLPSpec dtype inputFeatures outputFeatures hiddenFeatures) (MLP dtype inputFeatures outputFeatures hiddenFeatures) where
+  sample MLPSpec =
+    MLP <$> A.sample LinearSpec <*> A.sample LinearSpec <*> A.sample LinearSpec
+
+mlp
+  :: MLP dtype inputFeatures outputFeatures hiddenFeatures
+  -> Tensor dtype '[batchSize, inputFeatures]
+  -> Tensor dtype '[batchSize, outputFeatures]
+mlp MLP {..} = linear layer2 . relu . linear layer1 . relu . linear layer0
+
+model
+  :: MLP dtype inputFeatures outputFeatures hiddenFeatures
+  -> Tensor dtype '[batchSize, inputFeatures]
+  -> Tensor dtype '[batchSize, outputFeatures]
+model = (sigmoid .) . mlp
+
+foldLoop
+  :: forall a b m . (Num a, Enum a, Monad m) => b -> a -> (b -> a -> m b) -> m b
+foldLoop x count block = foldM block x ([1 .. count] :: [a])
+
+type BatchSize = 64
+
+main = do
+  let numIters = 100
+      seed = mkStdGen 123
+  (trainingData, testData) <- I.initMnist
+  init    <- A.sample (MLPSpec :: MLPSpec 'D.Float I.DataDim I.ClassDim 128)
+  let randomIndexes = map (`div` natValI @I.DataDim) $ randoms seed
+  
+  trained <- foldLoop (init,randomIndexes) numIters $ \(state, idxs) i -> do
+    let (indexes,nextIndexes) = (take (natValI @I.DataDim) idxs, drop (natValI @I.DataDim) idxs)
+    let input           = I.getImages @BatchSize trainingData indexes 
+    let expected_output = I.getLabels @BatchSize trainingData indexes
+    let actual_output   = model state $ input
+    let loss            = mse_loss actual_output expected_output
+
+    let flat_parameters = A.flattenParameters state
+    let gradients       = A.grad (toDynamic loss) flat_parameters
+
+    when (i `mod` 2500 == 0) (print loss)
+
+    new_flat_parameters <- mapM A.makeIndependent
+      $ A.sgd 1e-1 flat_parameters gradients
+    return $ (A.replaceParameters state new_flat_parameters, nextIndexes)
+  print trained
