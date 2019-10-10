eb18b55268af13a22154865a729549fab3bd952c
Junji Hashimoto <junji.hashimoto@gmail.com>
2019-10-10 18:34:02 +0900

    Merge static typed Parameter

--- examples/static-mnist/Main.hs
+++ examples/static-mnist/Main.hs
@@ -31,6 +31,7 @@ import qualified ATen.Managed.Type.Tensor      as ATen
 import           Torch.Static
 import           Torch.Static.Native     hiding ( linear )
 import           Torch.Static.Factories
+import           Torch.Static.NN
 import qualified Torch.Autograd                as A
 import qualified Torch.NN                      as A
 import qualified Torch.DType                   as D
@@ -43,39 +44,12 @@ import qualified Image                         as I
 -- Multi-Layer Perceptron (MLP)
 --------------------------------------------------------------------------------
 
-
-newtype Parameter dtype shape = Parameter A.IndependentTensor deriving (Show)
-
-toDependent :: Parameter dtype shape -> Tensor dtype shape
-toDependent (Parameter t) = UnsafeMkTensor $ A.toDependent t
-
-instance A.Parameterized (Parameter dtype shape) where
-  flattenParameters (Parameter x) = [x]
-  replaceOwnParameters _ = Parameter <$> A.nextParameter
-
-data LinearSpec (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) = LinearSpec
-  deriving (Show, Eq)
-
-data Linear (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) =
-  Linear { weight :: Parameter dtype '[inputFeatures, outputFeatures]
-         , bias :: Parameter dtype '[outputFeatures]
-         } deriving (Show, Generic)
-
 linear
   :: Linear dtype inputFeatures outputFeatures
   -> Tensor dtype '[batchSize, inputFeatures]
   -> Tensor dtype '[batchSize, outputFeatures]
 linear Linear {..} input =
   add (mm input (toDependent weight)) (toDependent bias)
-
-makeIndependent :: Tensor dtype shape -> IO (Parameter dtype shape)
-makeIndependent t = Parameter <$> A.makeIndependent (toDynamic t)
-
-instance A.Parameterized (Linear dtype inputFeatures outputFeatures)
-
-instance (KnownDType dtype, KnownNat inputFeatures, KnownNat outputFeatures) => A.Randomizable (LinearSpec dtype inputFeatures outputFeatures) (Linear dtype inputFeatures outputFeatures) where
-  sample LinearSpec =
-    Linear <$> (makeIndependent =<< randn) <*> (makeIndependent =<< randn)
 
 data MLPSpec (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) (hiddenFeatures :: Nat) = MLPSpec
 
---
+++ hasktorch/src/Torch/Static/NN.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE AllowAmbiguousTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Torch.Static.NN where
+
+import Control.Monad.State.Strict
+
+import GHC.TypeLits
+import GHC.Generics
+import Torch.Static
+import Torch.Static.Factories
+import Torch.Static.Native
+import qualified Torch.NN as A
+import qualified Torch.Autograd as A
+import qualified Torch.Tensor as A
+import qualified Torch.DType as D
+
+newtype Parameter dtype shape = Parameter A.IndependentTensor deriving (Show)
+
+toDependent :: Parameter dtype shape -> Tensor dtype shape
+toDependent (Parameter t) = UnsafeMkTensor $ A.toDependent t
+
+instance A.Parameterized (Parameter dtype shape) where
+  flattenParameters (Parameter x) = [x]
+  replaceOwnParameters _ = Parameter <$> A.nextParameter
+
+data LinearSpec (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) = LinearSpec
+  deriving (Show, Eq)
+
+data Linear (dtype :: D.DType) (inputFeatures :: Nat) (outputFeatures :: Nat) =
+  Linear { weight :: Parameter dtype '[inputFeatures, outputFeatures]
+         , bias :: Parameter dtype '[outputFeatures]
+         } deriving (Show, Generic)
+
+makeIndependent :: Tensor dtype shape -> IO (Parameter dtype shape)
+makeIndependent t = Parameter <$> A.makeIndependent (toDynamic t)
+
+instance A.Parameterized (Linear dtype inputFeatures outputFeatures)
+
+instance (KnownDType dtype, KnownNat inputFeatures, KnownNat outputFeatures) => A.Randomizable (LinearSpec dtype inputFeatures outputFeatures) (Linear dtype inputFeatures outputFeatures) where
+  sample LinearSpec =
+    Linear <$> (makeIndependent =<< randn) <*> (makeIndependent =<< randn)
