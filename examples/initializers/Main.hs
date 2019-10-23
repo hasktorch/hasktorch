4c08c3fec256e268840256d79b34dcd0d173b915
austinvhuang <austinh@alum.mit.edu>
2019-10-22 23:51:57 -0400

    start fan calculation, running into static-mnist dependency issue with stack ghci

--- examples/initializers/Main.hs
+++ examples/initializers/Main.hs
@@ -1,7 +1,13 @@
 module Main where
 
+import Torch.Tensor
+import Torch.TensorFactories
+
 data NonLinearity = Linear | Sigmoid | Tanh | Relu | LeakyRelu
 
+data Fan = FanIn | FanOut
+
+calculateGain :: NonLinearity -> Maybe Float -> Float
 calculateGain Linear _ = 1.0
 calculateGain Sigmoid _ = 1.0
 calculateGain Tanh _ = 5.0 / 3
@@ -10,5 +16,11 @@ calculateGain LeakyRelu param = sqrt (2.0 / (1.0 + (negativeSlope param) ^^ 2))
         negativeSlope Nothing = 0.01
         negativeSlope (Just value) = value
 
+calculateFan :: Tensor -> Fan -> Float
+calculateFan t mode = undefined
+
+main :: IO ()
 main = do
-    print "Done"
+    putStrLn "Linear default gain"
+    print $ calculateGain LeakyRelu (Just $ sqrt 5.0)
+    putStrLn "Done"
