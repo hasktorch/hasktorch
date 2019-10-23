3cc1504e33f30a88c0f3e9bca54751b150bc0fb1
austinvhuang <austinh@alum.mit.edu>
2019-10-23 00:21:24 -0400

    implement fan in/out calculation (needs checking

--- examples/initializers/Main.hs
+++ examples/initializers/Main.hs
@@ -16,11 +16,32 @@ calculateGain LeakyRelu param = sqrt (2.0 / (1.0 + (negativeSlope param) ^^ 2))
         negativeSlope Nothing = 0.01
         negativeSlope (Just value) = value
 
-calculateFan :: Tensor -> Fan -> Float
-calculateFan t mode = undefined
+calculateFan :: Tensor -> Fan -> (Int, Int)
+calculateFan t mode =
+    if dim t < 2 then
+        error "Fan in and fan out can not be computed for tensor with fewer than 2 dimensions"
+    else if dim t == 2 then
+        (size t 1, size t 0)
+        else 
+            (numInputFmaps * receptiveFieldSize,
+            numOutputFmaps * receptiveFieldSize)
+    where
+        numInputFmaps = size t 1
+        numOutputFmaps = size t 0
+        receptiveFieldSize = numel (select t 0 0)
+
+kaimingUniform t a mode nonlinearity = undefined
+    where 
+        fan = calculateFan t FanIn 
+        gain = calculateGain nonlinearity a
+        -- std = gain / sqrt fan
 
 main :: IO ()
 main = do
     putStrLn "Linear default gain"
     print $ calculateGain LeakyRelu (Just $ sqrt 5.0)
+    let foo = asTensor [[1.0 :: Float, 20.0]]
+    print $ calculateFan foo FanIn
+    let bar = asTensor [[[1.0 :: Float, 20.0]]]
+    print $ calculateFan bar FanIn
     putStrLn "Done"
