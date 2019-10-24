a27e16bfb1a8cfe21b7cb2b68c453117348fd573
o1lo01ol1o <tim.pierson@gmail.com>
2019-10-23 22:36:20 -0400

    Reorg

--- hasktorch/src/Torch/Typed/NN/Recurrent/Cell/LSTM.hs
+++ hasktorch/src/Torch/Typed/NN/Recurrent/Cell/LSTM.hs
@@ -22,7 +22,7 @@
 {-# OPTIONS_GHC -fplugin GHC.TypeLits.Extra.Solver #-}
 {-# OPTIONS_GHC -fconstraint-solver-iterations=0 #-}
 
-module Torch.Typed.NN.Recurrent.LSTM where
+module Torch.Typed.NN.Recurrent.Cell.LSTM where
 
 import           Control.Monad                  ( foldM
                                                 , when
@@ -35,7 +35,7 @@ import           Data.List                      ( foldl'
                                                 , intersperse
                                                 , scanl'
                                                 )
-import           Data.Maybe                     ( catMaybes )
+import           Data.Maybe                     ( catMaybes, fromMaybe )
 import           Data.Reflection
 import           Foreign.ForeignPtr
 import           GHC.Generics
@@ -60,86 +60,66 @@ import           Torch.Typed.Native      hiding ( linear )
 import           Torch.Typed.NN
 
 
-
+-- | A specification for a long, short-term memory cell.
+-- 
 data LSTMCellSpec (dtype :: D.DType) (inputDim :: Nat) (hiddenDim :: Nat) =
-    LSTMCellRandomInitState -- ^ Random initial memory cells and hiddenstate values
-    | LSTMCellZeroInitState -- ^ Zero-valued initial memory cells and hiddenstate values
+    LSTMCellSpec -- ^ Weights and biases are drawn from the standard normal distibution (having mean 0 and variance 1)
     deriving (Show, Eq, Ord, Generic, Enum, Bounded)
 
+-- | A long, short-term memory cell.
+-- 
 data LSTMCell (dtype :: D.DType) (inputDim :: Nat) (hiddenDim :: Nat) =  LSTMCell {
-    lSTMCell_init_h :: Parameter dtype '[hiddenDim] -- ^ initial hidden state
-    , lSTMCell_init_c :: Parameter dtype '[hiddenDim] -- ^ initial cell state
-    , lSTMCell_w_ih  :: Parameter dtype '[4 * hiddenDim, inputDim]  -- ^ input-to-hidden weights
-    , lSTMCell_w_hh :: Parameter dtype '[4 * hiddenDim, hiddenDim] -- ^ hidden-to-hidden weights
-    , lSTMCell_b_ih :: Parameter dtype '[4 * hiddenDim] -- ^ input-to-hidden bias
-    , lSTMCell_b_hh :: Parameter dtype '[4 * hiddenDim] -- ^ hidden-to-hidden bias
+      lstmCell_w_ih  :: Parameter dtype '[4 * hiddenDim, inputDim] -- ^ input-to-hidden weights
+    , lstmCell_w_hh :: Parameter dtype '[4 * hiddenDim, hiddenDim] -- ^ hidden-to-hidden weights
+    , lstmCell_b_ih :: Parameter dtype '[4 * hiddenDim] -- ^ input-to-hidden bias
+    , lstmCell_b_hh :: Parameter dtype '[4 * hiddenDim] -- ^ hidden-to-hidden bias
     } deriving Generic
 
 instance (KnownDType dtype, KnownNat inputDim, KnownNat hiddenDim) => A.Randomizable (LSTMCellSpec dtype inputDim hiddenDim) (LSTMCell dtype inputDim hiddenDim) where
-    sample LSTMCellRandomInitState =
+    sample LSTMCellSpec =
         LSTMCell
             <$> (makeIndependent =<< randn)
-            <*> (makeIndependent =<< randn)
-            <*> (makeIndependent =<< randn)
-            <*> (makeIndependent =<< randn)
-            <*> (makeIndependent =<< randn)
-            <*> (makeIndependent =<< randn)
-    sample LSTMCellZeroInitState =
-        LSTMCell
-            <$> (makeIndependent =<< pure zeros)
-            <*> (makeIndependent =<< pure zeros)
-            <*> (makeIndependent =<< randn)
             <*> (makeIndependent =<< randn)
             <*> (makeIndependent =<< randn)
             <*> (makeIndependent =<< randn)
 
 instance A.Parameterized (LSTMCell d i h)
 
--- | A single recurrent step
+-- | A single recurrent step of an `LSTMCell`
 -- 
 forwardStep
     :: forall d i h b
      . (KnownDType d, KnownNat i, KnownNat h, KnownNat b)
     => LSTMCell d i h -- ^ The cell
     -> Tensor d '[b, i] -- ^ The input
-    -> Maybe (Tensor d '[b, h], Tensor d '[b, h]) -- ^ The current (Hidden, Cell) state
+    -> (Tensor d '[b, h], Tensor d '[b, h]) -- ^ The current (Hidden, Cell) state
     -> (Tensor d '[b, h], Tensor d '[b, h]) -- ^ The subequent (Hidden, Cell) state
-forwardStep LSTMCell {..} input Nothing = lstm_cell
-    input
-    [ expand @'[b, h] False $ toDependent lSTMCell_init_h
-    , expand @'[b, h] False $ toDependent lSTMCell_init_c
-    ]
-    (toDependent lSTMCell_w_ih)
-    (toDependent lSTMCell_w_hh)
-    (toDependent lSTMCell_b_ih)
-    (toDependent lSTMCell_b_hh)
-
-forwardStep LSTMCell {..} input (Just (hs, cs)) = lstm_cell
+forwardStep LSTMCell {..} input (hs, cs) = lstm_cell
     input
     [hs, cs]
-    (toDependent lSTMCell_w_ih)
-    (toDependent lSTMCell_w_hh)
-    (toDependent lSTMCell_b_ih)
-    (toDependent lSTMCell_b_hh)
-
--- TODO: should we alias these functions with their Prelude ancesters?
+    (toDependent lstmCell_w_ih)
+    (toDependent lstmCell_w_hh)
+    (toDependent lstmCell_b_ih)
+    (toDependent lstmCell_b_hh)
 
 -- | foldl' for lists of tensors unsing an `LSTMCell`
 --
 forward
     :: (KnownDType d, KnownNat i, KnownNat h, KnownNat b)
     => LSTMCell d i h
+    -> (Tensor d '[b, h], Tensor d '[b, h]) 
     -> [Tensor d '[b, i]]
-    -> Maybe (Tensor d '[b, h], Tensor d '[b, h])
-forward lc inputs =
-    foldl' (\acc i -> Just $ forwardStep lc i acc) Nothing inputs
+    -> (Tensor d '[b, h], Tensor d '[b, h])
+forward lc st inputs =
+    foldl' (\acc i -> forwardStep lc i acc) st inputs
 
 -- | scanl' for lists of tensors unsing an `LSTMCell`
 --
 forwardScan
     :: (KnownDType d, KnownNat i, KnownNat h, KnownNat b)
     => LSTMCell d i h
+    -> (Tensor d '[b, h], Tensor d '[b, h]) 
     -> [Tensor d '[b, i]]
     -> [(Tensor d '[b, h], Tensor d '[b, h])]
-forwardScan lc inputs =
-    catMaybes $ scanl' (\acc i -> Just $ forwardStep lc i acc) Nothing inputs
+forwardScan lc st inputs =
+    scanl' (\acc i -> forwardStep lc i acc) st inputs
