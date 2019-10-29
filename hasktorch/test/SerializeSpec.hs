5edf4d58e9977382086c374a8bc657bfe0c26e9f
Junji Hashimoto <junji.hashimoto@gmail.com>
2019-10-29 22:29:40 +0900

    Clean up after reading test.pt

--- hasktorch/test/SerializeSpec.hs
+++ hasktorch/test/SerializeSpec.hs
@@ -2,6 +2,7 @@
 module SerializeSpec(spec) where
 
 import Test.Hspec
+import System.Directory (removeFile)
 
 import Torch.Tensor
 import Torch.Serialize
@@ -14,6 +15,7 @@ spec = do
         v = [3, 4, 5] :: [Float]
     save [(asTensor i),(asTensor v)] "test.pt"
     tensors <- load "test.pt"
+    removeFile "test.pt"
     length tensors `shouldBe` 2
     let [ii,vv] = tensors
     (asValue ii :: [[Int]]) `shouldBe` i
