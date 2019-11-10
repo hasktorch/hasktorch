0470f5dc75cfc20a6996d1eba7c43e4c34a54fd0
austinvhuang <austinh@alum.mit.edu>
2019-11-10 10:23:30 -0500

    start untyped mnist example

---
+++ examples/mnist-mlp/Main.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module Main where
+
+import GHC.Generics
+import Torch.Autograd as A
+import Torch.Tensor
+import Torch.NN
+
+import qualified Image as I
+
+data MLPSpec = MLPSpec {
+    inputFeatures :: Int,
+    outputFeatures :: Int,
+    hiddenFeatures0 :: Int,
+    hiddenFeatures1 :: Int
+    } deriving (Show, Eq)
+
+data MLP = MLP { 
+    l0 :: Linear,
+    l1 :: Linear,
+    l2 :: Linear
+    } deriving (Generic, Show)
+
+instance Parameterized MLP
+instance Randomizable MLPSpec MLP where
+    sample MLPSpec {..} = 
+        MLP
+            <$> sample (LinearSpec inputFeatures hiddenFeatures0)
+            <*> sample (LinearSpec hiddenFeatures0 hiddenFeatures1)
+            <*> sample (LinearSpec hiddenFeatures1 outputFeatures)
+
+train = undefined
+
+main :: IO ()
+main = do
+    (trainData, testData) <- I.initMnist
+    let spec = MLPSpec 768 10 512 256 
+    init <- sample spec
+    putStrLn "Done"
---
+++ examples/mnist-mlp/README.md
@@ -0,0 +1,1 @@
+# Untyped MNIST Example
