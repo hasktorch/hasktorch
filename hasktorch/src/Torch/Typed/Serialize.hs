e8f1a08879364b080f3ba08aa8df0ee8b38f071e
Torsten Scholak <torsten.scholak@googlemail.com>
2019-12-15 19:57:18 -0500

    sort out compile-time warning

--- hasktorch/src/Torch/Typed/Serialize.hs
+++ hasktorch/src/Torch/Typed/Serialize.hs
@@ -1,28 +1,32 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Torch.Typed.Serialize where
 
-import Data.HList
-import           Foreign.ForeignPtr
+import           Data.HList
 
-import qualified Torch.Managed.Serialize as S
-import Torch.Typed.Tensor
 import qualified ATen.Class                    as ATen
 import qualified ATen.Cast                     as ATen
 import qualified ATen.Type                     as ATen
+import qualified Torch.Managed.Serialize       as S
+import qualified Torch.Tensor                  as D
+import           Torch.Typed.Tensor
 
+-- | save list of tensors to file
 save
   :: forall tensors
-   . (ATen.Castable (HList tensors) (ForeignPtr ATen.TensorList))
-  => HList tensors
-  -> FilePath
+   . ATen.Castable (HList tensors) [D.ATenTensor]
+  => HList tensors -- ^ list of input tensors
+  -> FilePath -- ^ file
   -> IO ()
-save inputs file = ATen.cast2 S.save inputs file
+save = ATen.cast2 S.save
 
+-- | load list of tensors from file
 load
   :: forall tensors
-   . (ATen.Castable (HList tensors) (ForeignPtr ATen.TensorList))
-  => FilePath
+   . ATen.Castable (HList tensors) [D.ATenTensor]
+  => FilePath -- ^ file
   -> IO (HList tensors)
-load file = ATen.cast1 S.load file
+load = ATen.cast1 S.load
