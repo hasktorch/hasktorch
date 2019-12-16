97da3164506a37913b97761ad20652319eb9d984
Torsten Scholak <torsten.scholak@googlemail.com>
2019-12-15 19:57:41 -0500

    format

--- hasktorch/src/Torch/Typed/Autograd.hs
+++ hasktorch/src/Torch/Typed/Autograd.hs
@@ -31,7 +31,6 @@ import           Torch.Typed.Parameter
 class HasGrad a b | a -> b where
   -- | calculate gradients of a zero-dimensional tensor with respect to a list of parameters
   grad :: forall dtype device . Tensor device dtype '[] -> a -> b
-
   toDependent :: a -> b
 
 -- instance HasGrad (Tensor dtype device shape) (Tensor dtype device shape) where
@@ -39,7 +38,6 @@ class HasGrad a b | a -> b where
 --     Torch.Managed.Autograd.grad
 --     loss
 --     [Torch.Typed.Autograd.toDependent input]
-
 --   toDependent = id
 
 instance HasGrad (Parameter dtype device shape) (Tensor dtype device shape) where
@@ -51,14 +49,16 @@ instance HasGrad (Parameter dtype device shape) (Tensor dtype device shape) wher
 
 instance HasGrad (HList '[]) (HList '[])  where
   grad _ = id
-
   toDependent = id
 
-instance (HasGrad a b, HasGrad (HList as) (HList bs), ATen.Castable (HList (b ': bs)) [D.ATenTensor]) => HasGrad (HList (a ': as)) (HList (b ': bs)) where
+instance
+  ( HasGrad a b
+  , HasGrad (HList as) (HList bs)
+  , ATen.Castable (HList (b ': bs)) [D.ATenTensor]
+  ) => HasGrad (HList (a ': as)) (HList (b ': bs)) where
   grad loss inputs = unsafePerformIO $ ATen.cast2
     Torch.Managed.Autograd.grad
     loss
     (Torch.Typed.Autograd.toDependent inputs)
-
   toDependent (a :. as) =
     Torch.Typed.Autograd.toDependent a :. Torch.Typed.Autograd.toDependent as
