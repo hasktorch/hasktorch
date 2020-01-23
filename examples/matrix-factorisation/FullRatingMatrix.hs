c3e88421fd3c440e1c83a52aaff81135fc9acb87
Saeed Hadikhanloo <Saeed.Hadikhanloo@gmail.com>
2020-01-23 11:19:30 +0100

    example of sparse rating matrix

--- examples/matrix-factorisation/FullRatingMatrix.hs
+++ examples/matrix-factorisation/FullRatingMatrix.hs
@@ -2,57 +2,19 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE RecordWildCards #-}
 
--- In matrix factorization, we aim to write a given matrix R (of size n x m) as
--- a product U * V where U is n x k and V is k x m for some k. This is done in
--- this example by minimizing a loss function which is the mean square of the
--- difference between U * V and R.
-
 module Main where
 
 import Control.Monad (foldM, when)
+import MF
 import Torch.Autograd (grad, makeIndependent, toDependent)
 import Torch.DType (DType (Float))
 import Torch.Functional (matmul, mse_loss)
 import Torch.Internal.Managed.Type.Context (manual_seed_L)
 import Torch.NN
-  ( Parameter,
-    Randomizable,
-    sample,
+  ( sample,
     sgd,
   )
 import Torch.Tensor (Tensor, asTensor, shape)
-import Torch.TensorFactories (randn')
-
-data MatrixFactSpec
-  = MatrixFactSpec
-      { dim1 :: Int, -- that is n when R has n x m shape
-        dim2 :: Int, -- that is m when R has n x m shape
-        common_dim :: Int -- that is k when U has n x k shape
-      }
-  deriving (Show, Eq)
-
-data MatrixFact = MatrixFact {u :: Parameter, v :: Parameter}
-
-instance Randomizable MatrixFactSpec MatrixFact where
-  sample (MatrixFactSpec n m k) = do
-    u <- makeIndependent =<< randn' [n, k]
-    v <- makeIndependent =<< randn' [k, m]
-    pure $ MatrixFact u v
-
-flattenParameters :: MatrixFact -> [Parameter]
-flattenParameters (MatrixFact u v) = [u, v]
-
-replaceParameters :: MatrixFact -> [Parameter] -> MatrixFact
-replaceParameters state params_list = MatrixFact u v
-  where
-    u = params_list !! 0
-    v = params_list !! 1
-
-mulMF :: MatrixFact -> Tensor
-mulMF (MatrixFact u v) = matmul u' v'
-  where
-    u' = toDependent u
-    v' = toDependent v
 
 r =
   asTensor
