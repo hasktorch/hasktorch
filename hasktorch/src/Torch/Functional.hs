c92014f8ee372abb26617aa1fd706108abb80791
Shiv Kumar <championballer08@gmail.com>
2020-02-23 07:16:27 +0530

    Added parameter descriptions wrt review on #289

--- hasktorch/src/Torch/Functional.hs
+++ hasktorch/src/Torch/Functional.hs
@@ -130,8 +130,8 @@ abs t = unsafePerformIO $ (cast1 ATen.abs_t) t
 -- Computes the fractional portion of each element in input.
 -- out_i = input_i - (floor . abs) input_i * (sign input_i)
 frac 
- :: Tensor 
- -> Tensor
+ :: Tensor -- ^ input
+ -> Tensor -- ^ output
 frac _self = unsafePerformIO $ (cast1 ATen.frac_t) _self
 
 keepdim KeepDim = True
@@ -140,10 +140,10 @@ keepdim RemoveDim = False
 -- | argmax
 -- Returns the indices of the maximum value of all elements in the input tensor.
 argmax 
- :: Dim 
- -> KeepDim 
- -> Tensor 
- -> Tensor
+ :: Dim -- ^ the dimension to reduce
+ -> KeepDim -- ^ whether the output tensor has dim retained or not
+ -> Tensor -- ^ input
+ -> Tensor -- ^ output
 argmax (Dim d) k t = unsafePerformIO $ (cast3 ATen.argmax_tlb) t d (keepdim k)
 
 -- | add
@@ -200,35 +200,39 @@ median
 median t = unsafePerformIO $ (cast1 ATen.median_t) t
 
 -- | addScalar
+-- Adds each element of the input input with the scalar and returns a new resulting tensor.
 addScalar 
  :: Scalar a 
- => Tensor 
- -> a 
- -> Tensor
+ => Tensor -- ^ input
+ -> a -- ^ summand
+ -> Tensor -- ^ output
 addScalar t a = unsafePerformIO $ (cast2 ATen.add_ts) t a
 
 -- | subScalar
+-- Subtracts each element of the input input with the scalar and returns a new resulting tensor.
 subScalar 
  :: Scalar a 
- => Tensor 
- -> a 
- -> Tensor
+ => Tensor -- ^ input
+ -> a -- ^ subtrahend
+ -> Tensor -- ^ output
 subScalar t a = unsafePerformIO $ (cast2 ATen.sub_ts) t a
 
 -- | mulScalar
+-- Multiplies each element of the input input with the scalar and returns a new resulting tensor.
 mulScalar 
  :: Scalar a 
- => Tensor 
- -> a 
- -> Tensor
+ => Tensor -- ^ input
+ -> a -- ^ multiplier
+ -> Tensor -- ^ output
 mulScalar t a = unsafePerformIO $ (cast2 ATen.mul_ts) t a
 
 -- | divScalar
+-- Divides each element of the input input with the scalar and returns a new resulting tensor.
 divScalar 
  :: Scalar a 
- => Tensor 
- -> a 
- -> Tensor
+ => Tensor -- ^ input
+ -> a -- ^ divisor
+ -> Tensor -- ^ output
 divScalar t a = unsafePerformIO $ (cast2 ATen.div_ts) t a
 
 -- | matmul 
@@ -242,9 +246,9 @@ divScalar t a = unsafePerformIO $ (cast2 ATen.div_ts) t a
 -- If the first argument is 2-dimensional and the second argument is 1-dimensional, the matrix-vector product is returned.
 -- If both arguments are at least 1-dimensional and at least one argument is N-dimensional (where N > 2), then a batched matrix multiply is returned. If the first argument is 1-dimensional, a 1 is prepended to its dimension for the purpose of the batched matrix multiply and removed after. If the second argument is 1-dimensional, a 1 is appended to its dimension for the purpose of the batched matrix multiple and removed after. The non-matrix (i.e. batch) dimensions are broadcasted (and thus must be broadcastable). For example, if input is a (j \times 1 \times n \times m)(j×1×n×m) tensor and other is a (k \times m \times p)(k×m×p) tensor, out will be an (j \times k \times n \times p)(j×k×n×p) tensor.
 matmul 
- :: Tensor 
- -> Tensor 
- -> Tensor
+ :: Tensor -- ^ first tensor for matrix multiplication 
+ -> Tensor -- ^ second tensor for matrix multiplication
+ -> Tensor -- ^ output
 matmul a b = unsafePerformIO $ (cast2 ATen.matmul_tt) a b
 
 -- | erf
@@ -283,10 +287,11 @@ log10 t = unsafePerformIO $ (cast1 ATen.log10_t) t
 
 -- | pow
 -- Takes the power of each element in input with exponent and returns a tensor with the result.
-pow :: Scalar a 
-    => Tensor -- ^ input
-    -> a -- ^ exponent
-    -> Tensor -- ^ output
+pow 
+ :: Scalar a 
+ => Tensor -- ^ input
+ -> a -- ^ exponent
+ -> Tensor -- ^ output
 pow t s = unsafePerformIO $ (cast2 ATen.pow_ts) t s
 
 -- | powt
@@ -482,12 +487,22 @@ squeezeAll
     -> Tensor -- ^ output
 squeezeAll t = unsafePerformIO $ (cast1 ATen.squeeze_t) t
 
--- | binary_cross_entropy_loss
-binaryCrossEntropyLoss :: Tensor -> Tensor -> Tensor -> Reduction-> Tensor
+-- | binaryCrossEntropyLoss
+-- Function that measures the Binary Cross Entropy between the target and the output.
+binaryCrossEntropyLoss 
+ :: Tensor -- ^ input
+ -> Tensor -- ^ target
+ -> Tensor -- ^ weight
+ -> Reduction -- ^ Specifies the reduction to apply to the output
+ -> Tensor -- ^ output
 binaryCrossEntropyLoss t target weight reduction = unsafePerformIO $ (cast4 ATen.binary_cross_entropy_tttl) t target weight reduction
 
--- | BCE with weights defaulted to 1.0 & reduction defaulted to ReduceMean
-binaryCrossEntropyLoss' :: Tensor -> Tensor -> Tensor
+-- | binaryCrossEntropyLoss'
+-- BCE with weights defaulted to 1.0 & reduction defaulted to ReduceMean
+binaryCrossEntropyLoss' 
+ :: Tensor -- ^ input
+ -> Tensor -- ^ target
+ -> Tensor -- ^ output
 binaryCrossEntropyLoss' t target = unsafePerformIO $ (cast4 ATen.binary_cross_entropy_tttl) t target (onesLike target) ReduceMean
 
 -- | mse_loss
@@ -673,12 +688,18 @@ adaptiveAvgPool1d outputSize input = unsafePerformIO
 
 -- | adaptiveAvgPool2d
 -- Applies a 2D adaptive average pooling over an input signal composed of several input planes.
-adaptiveAvgPool2d :: Tensor -> (Int,Int) -> Tensor
+adaptiveAvgPool2d 
+ :: Tensor -- ^ input 
+ -> (Int,Int) -- ^ output size (Height * Width)
+ -> Tensor -- ^ output
 adaptiveAvgPool2d _self _output_size = unsafePerformIO $ (cast2 ATen.adaptive_avg_pool2d_tl) _self _output_size
 
 -- | adaptiveAvgPool3d
 -- Applies a 3D adaptive average pooling over an input signal composed of several input planes.
-adaptiveAvgPool3d :: Tensor -> (Int,Int,Int) -> Tensor
+adaptiveAvgPool3d 
+ :: Tensor -- ^ input
+ -> (Int,Int,Int) -- ^ output size (Depth * Height * Width)
+ -> Tensor -- ^ output
 adaptiveAvgPool3d _self _output_size = unsafePerformIO $ (cast2 ATen.adaptive_avg_pool3d_tl) _self _output_size
 
 bitwiseNot 
