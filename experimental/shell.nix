98185f63864e3e73f959c4a189e754a308f25b43
Junji Hashimoto <junji.hashimoto@gmail.com>
2020-03-11 05:02:00 +0000

    Add experimental/*.nix

---
+++ experimental/shell.nix
@@ -0,0 +1,11 @@
+{ cudaVersion ? null }:
+let
+  shared = import ../nix/shared.nix { };
+in
+assert cudaVersion == null || cudaVersion == 9 || cudaVersion == 10;
+
+if cudaVersion == null
+then shared.shell-hasktorch-experimental_cpu
+else if cudaVersion == 9
+  then shared.shell-hasktorch-experimental_cudatoolkit_9_2
+  else shared.shell-hasktorch-experimental_cudatoolkit_10_1
