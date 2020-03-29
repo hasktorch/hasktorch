90eba186e9352e28449698aaaaa7ad1094cfcfe1
championballer <championballer08@gmail.com>
2020-03-29 14:14:23 +0530

    Updated Autograd example with activation functions and added derivation in readme

--- examples/autograd/Main.hs
+++ examples/autograd/Main.hs
@@ -11,15 +11,16 @@ printTensor s t = do
 
 main :: IO()
 main = do
- xi <- makeIndependent $ asTensor([[1],[2],[3],[4]]::[[Float]])
+ xi <- makeIndependent $ asTensor([[-1],[2],[-3],[4]]::[[Float]])
  wi1 <- makeIndependent $ ones' [4,3]
  wi2 <- makeIndependent $ ones' [3,1]
  let x = toDependent xi
      w1 = toDependent wi1
      w2 = toDependent wi2
-     h1 = (transpose2D x) `matmul` w1
-     h2 = h1 `matmul` w2
- let gradients = grad h2 [wi1,wi2]
+     h1 = relu $ (transpose2D x) `matmul` w1
+     h2 = sigmoid $ h1 `matmul` w2
+     loss = (h2-0)^2
+ let gradients = grad loss [wi1,wi2]
  
  printTensor "The input features:" x
  printTensor "The output of the first layer:" h1
