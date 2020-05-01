aec1d08818222a4f1340081309cd41f03f8fdb33
Suraj Kumar <surajk732000@gmail.com>
2020-05-01 12:37:03 +0000

    Refactor code

--- examples/alexNet/Main.hs
+++ examples/alexNet/Main.hs
@@ -4,29 +4,26 @@
 
 module Main where
 
-import           AlexNet
+import           AlexNet                                        (AlexNetBB, alexnetBackBoneSpec, alexnetBBForward)
 
-import           GHC.Generics
-import           Control.Monad                                 (when)
-import           Text.Regex.TDFA
-import           System.Directory
-import           System.Random                                 hiding (split)
-import           Data.List
-import qualified Data.Map.Strict                     as S
+import           Control.Monad                                  (when)
+import           Text.Regex.TDFA                                ((=~))
+import           System.Directory                               (getDirectoryContents)
+import           System.Random                                  (randomRIO)
+import           Data.List                                      (nub)
 import qualified GHC.List                            as G
+import qualified Data.Map.Strict                     as S
 
-import           Torch.Optim
-import           Torch.Autograd
-import           Torch.Serialize
-import           Torch.NN
-import           Torch.Functional
-import           Torch.TensorFactories
-import           Torch.TensorOptions
-import           Torch.Tensor
+import           Torch.Optim                                    (foldLoop, runStep, mkAdam, Adam)
+import           Torch.Autograd                                 (IndependentTensor (..))
+import           Torch.Serialize                                (load)
+import           Torch.Tensor                                   (Tensor, reshape, asTensor, toInt, shape)
+import           Torch.Functional.Internal                      (repeatInterleaveScalar)
+import           Torch.NN                            as N
+import           Torch.Functional                    as F
 import qualified Torch.DType                         as D
 import qualified Torch.Vision                        as V
-import qualified Torch.Typed.Vision                  as V      hiding (getImages')
-import qualified Torch.Functional.Internal           as I
+import qualified Torch.Typed.Vision                  as V       hiding (getImages')
 
 data DataSet = DataSet {
     images  :: [Tensor],
@@ -36,7 +33,7 @@ data DataSet = DataSet {
 
 shuffle :: [a] -> IO[a]
 shuffle x = if length x < 2 then return x else do
-    i <- System.Random.randomRIO (0, length(x)-1)
+    i <- randomRIO (0, length(x)-1)
     r <- shuffle (take i x ++ drop (i+1) x)
     return (x!!i : r)
 
@@ -57,20 +54,20 @@ createLabels string = firstMatch
     where (firstMatch, _, _) = (string =~ "_[0-9]+.jpg" :: (String, String, String))
 
 -- normalizeT :: Tensor -> Tensor
--- normalizeT t = (t `sub` (uns mean)) `Torch.Functional.div` (uns stddev)
+-- normalizeT t = (t `F.sub` (uns mean)) `F.div` (uns stddev)
 --     where 
 --         mean = asTensor ([0.485, 0.456, 0.406] :: [Float])
 --         stddev = asTensor ([0.229, 0.224, 0.225] :: [Float])
---         uns t = unsqueeze (Dim 2) $ unsqueeze (Dim 1) t
+--         uns t = F.unsqueeze (F.Dim 2) $ F.unsqueeze (F.Dim 1) t
 
 normalize :: Tensor -> Tensor
-normalize img = unsqueeze (Dim 0) (cat (Dim 0) [r', g', b'])
+normalize img = F.unsqueeze (F.Dim 0) (F.cat (F.Dim 0) [r', g', b'])
     where
-        img' = divScalar (255.0::Float) img
-        [r, g, b] = split 1 (Dim 0) (reshape [3, 224, 224] img')
-        r' = divScalar (0.229::Float) (subScalar (0.485::Float) r) 
-        g' = divScalar (0.224::Float) (subScalar (0.456::Float) g) 
-        b' = divScalar (0.225::Float) (subScalar (0.406::Float) b) 
+        img' = F.divScalar (255.0::Float) img
+        [r, g, b] = F.split 1 (F.Dim 0) (reshape [3, 224, 224] img')
+        r' = F.divScalar (0.229::Float) (F.subScalar (0.485::Float) r) 
+        g' = F.divScalar (0.224::Float) (F.subScalar (0.456::Float) g) 
+        b' = F.divScalar (0.225::Float) (F.subScalar (0.406::Float) b) 
 
 extract' :: Maybe a -> a
 extract' (Just x) = x          
@@ -82,17 +79,17 @@ labelToTensor unique  = map (extract' . (m S.!?))
 
 createDataSet :: [FilePath]  -> [String] -> IO DataSet
 createDataSet directorylist classes = do
-    readList <- mapM (V.readImageAsRGB8 . ("alexNet/simg/" ++ )) directorylist
+    readList <- mapM (V.readImageAsRGB8 . ("alexNet/images/" ++ )) directorylist
     let listIL = extractImagesNLabels directorylist readList
         labelList = map (createLabels . fst) listIL 
-        imageList = map (toDType D.Float . V.hwc2chw . snd) listIL
+        imageList = map (F.toDType D.Float . V.hwc2chw . snd) listIL
         batchSize = 16 :: Int
-        rszdImgs =  map (upsampleBilinear2d (224, 224) True) imageList
+        rszdImgs =  map (F.upsampleBilinear2d (224, 224) True) imageList
         nrmlzdImgs = map normalize rszdImgs
         prcssdLbls = labelToTensor classes labelList
         
-        images = split  batchSize (Dim 0) $ cat (Dim 0) nrmlzdImgs
-        labels = split  batchSize (Dim 0) (asTensor prcssdLbls)
+        images = F.split  batchSize (F.Dim 0) $ F.cat (F.Dim 0) nrmlzdImgs
+        labels = F.split  batchSize (F.Dim 0) (asTensor prcssdLbls)
         dataset = DataSet images labels classes
     return dataset
 
@@ -102,52 +99,52 @@ fromMNIST ds = do
         batchSize = 16 :: Int
         labelT = V.getLabels' nImages ds [0..(nImages-1)]
     imagesT <- V.getImages' nImages 784 ds [0..(nImages-1)]
-    let rimages = I.repeatInterleaveScalar (reshape [nImages, 1, 28, 28] imagesT) 3 1
-        rszdImgs = upsampleBilinear2d (224, 224) True rimages
-        nrmlzdImgs = map normalize $ split 1 (Dim 0) rszdImgs
+    let rimages = repeatInterleaveScalar (reshape [nImages, 1, 28, 28] imagesT) 3 1
+        rszdImgs = F.upsampleBilinear2d (224, 224) True rimages
+        nrmlzdImgs = map normalize $ F.split 1 (F.Dim 0) rszdImgs
 
-        images = split batchSize (Dim 0) (cat (Dim 0) nrmlzdImgs)
-        labels = split batchSize (Dim 0) labelT
+        images = F.split batchSize (F.Dim 0) (F.cat (F.Dim 0) nrmlzdImgs)
+        labels = F.split batchSize (F.Dim 0) labelT
         dataset = DataSet images labels $ map show [0..9]
     return dataset
 
 calcLoss :: [Tensor] -> [Tensor] -> [Tensor]
-calcLoss targets predictions = take 10 $ map (\(t, p)-> nllLoss' t p) $ zip targets predictions
+calcLoss targets predictions = take 10 $ map (\(t, p)-> F.nllLoss' t p) $ zip targets predictions
 
-oneEpoch :: DataSet -> AlexNetBB -> Linear -> Adam -> IO (Linear, Adam)
+oneEpoch :: DataSet -> AlexNetBB -> N.Linear -> Adam -> IO (N.Linear, Adam)
 oneEpoch dataset pretrainedbb model optim = do
     (newEpochModel, newEpochOptim) <- foldLoop (model, optim) (length $ labels dataset) $
         \(iterModel, iterOptim) iter -> do
             let imageBatch = (images dataset) !! (iter - 1) 
                 labelBatch = (labels dataset) !! (iter - 1) 
-                scores = logSoftmax (Dim 1) $ linear iterModel $ alexnetBBForward pretrainedbb imageBatch
-                loss = nllLoss' labelBatch scores
+                scores = F.logSoftmax (F.Dim 1) $ N.linear iterModel $ alexnetBBForward pretrainedbb imageBatch
+                loss = F.nllLoss' labelBatch scores
             when (iter `mod` 30 == 0) $ do
                 putStrLn $ "Iteration: " ++ show iter ++ " | Loss for current mini-batch: " ++ show loss
             (newParam, newOptim) <- runStep iterModel iterOptim loss 1e-4
-            pure (replaceParameters iterModel newParam, newOptim)
+            pure (N.replaceParameters iterModel newParam, newOptim)
     return (newEpochModel, newEpochOptim)
 
-train :: DataSet -> DataSet -> AlexNetBB -> IO Linear
+train :: DataSet -> DataSet -> AlexNetBB -> IO N.Linear
 train trainDataset valDataset pretrainedbb = do
     let uniqueClasses = classes trainDataset
-    initModel <- sample $ LinearSpec 4096 $ length uniqueClasses
-    let initOptim = mkAdam 0 0.9 0.999 (flattenParameters initModel) 
+    initModel <- sample $ N.LinearSpec 4096 $ length uniqueClasses
+    let initOptim = mkAdam 0 0.9 0.999 (N.flattenParameters initModel) 
     (trained, _) <- foldLoop (initModel, initOptim) 5 $
         \(state, optim) iter -> do
-            print $ calcLoss (labels valDataset) $ map ((logSoftmax (Dim 1)) . (linear state) . (alexnetBBForward pretrainedbb)) $ images valDataset
+            print $ calcLoss (labels valDataset) $ map ((F.logSoftmax (F.Dim 1)) . (N.linear state) . (alexnetBBForward pretrainedbb)) $ images valDataset
             oneEpoch trainDataset pretrainedbb state optim     
     return trained
 
-evaluate :: Tensor -> AlexNetBB -> Linear -> Tensor
-evaluate images pretrainedbb trainedFL = argmax (Dim (-1)) KeepDim $ softmax (Dim (-1)) unNormalizedScores
+evaluate :: Tensor -> AlexNetBB -> N.Linear -> Tensor
+evaluate images pretrainedbb trainedFL = F.argmax (F.Dim (-1)) F.KeepDim $ F.softmax (F.Dim (-1)) unNormalizedScores
     where
-        unNormalizedScores =  linear trainedFL $ alexnetBBForward pretrainedbb images
+        unNormalizedScores =  N.linear trainedFL $ alexnetBBForward pretrainedbb images
 
-calcAccuracy :: (Tensor, Tensor) -> AlexNetBB -> Linear -> Float
+calcAccuracy :: (Tensor, Tensor) -> AlexNetBB -> N.Linear -> Float
 calcAccuracy (targets, inputs)  pretrainedbb trainedFL = correctPreds / totalPreds
     where
-        correctPreds = fromIntegral (toInt $ sumAll $ (flattenAll predictions) ==. targets) :: Float
+        correctPreds = fromIntegral (toInt $ F.sumAll $ F.eq (F.flattenAll predictions) targets) :: Float
         totalPreds = fromIntegral (head $ shape targets) :: Float
         predictions = evaluate inputs pretrainedbb trainedFL
 
@@ -156,27 +153,23 @@ main = do
     alexnetBB <- sample alexnetBackBoneSpec
     
     let bbparams = map IndependentTensor $ init $ init pretrainedANParams
-        pretrainedbb = replaceParameters alexnetBB bbparams
+        pretrainedbb = N.replaceParameters alexnetBB bbparams
     
-    directoryList <- getDirectoryContents "alexNet/simg"
+    directoryList <- getDirectoryContents "alexNet/images"
     let filteredDirectoryList = filter (\x-> if x=="." || x==".." then False else True) directoryList
     (trainList, testList) <- splitList filteredDirectoryList 0.9
     
     let classes = nub $ map createLabels filteredDirectoryList
     trainDataSet <- createDataSet trainList classes
     testDataSet <- createDataSet testList classes
+
+    -- (trainData, testData) <- V.initMnist "datasets/mnist"
+    -- trainDataSet <- fromMNIST trainData
+    -- testDataSet <- fromMNIST testData
+
     trainedFinalLayer <- train trainDataSet testDataSet pretrainedbb
 
     print $ "Accuray on train-set:" ++ show ((foldl (\acc t-> acc + (calcAccuracy t pretrainedbb trainedFinalLayer))  0 $ zip (labels trainDataSet) (images trainDataSet)) / (fromIntegral $  length $ labels trainDataSet :: Float))
     print $ "Accuray on test-set:" ++ show ((foldl (\acc t-> acc + (calcAccuracy t pretrainedbb trainedFinalLayer))  0 $ zip (labels testDataSet) (images testDataSet)) / (fromIntegral $  length $ labels testDataSet :: Float))
-    
-    -- (trainData, testData) <- V.initMnist "datasets/mnist"
-    -- mnistTrainDS <- fromMNIST trainData
-    -- mnistTestDS <- fromMNIST testData
-    -- trainedFinalLayer <- train mnistTrainDS mnistTestDS pretrainedbb
-
-    -- print $ "Accuray on train-set:" ++ show ((foldl (\acc t-> acc + (calcAccuracy t pretrainedbb trainedFinalLayer))  0 $ zip (labels mnistTrainDS) (images mnistTrainDS)) / (fromIntegral $  length $ labels mnistTrainDS :: Float))
-    -- print $ "Accuray on test-set:" ++ show ((foldl (\acc t-> acc + (calcAccuracy t pretrainedbb trainedFinalLayer))  0 $ zip (labels mnistTestDS) (images mnistTestDS)) / (fromIntegral $  length $ labels mnistTestDS :: Float))
 
     putStrLn "Done"
-    
