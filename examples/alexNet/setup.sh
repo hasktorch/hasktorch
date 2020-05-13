c00b7e972ff7387c38dbf6ecd8822595aba3f02f
Suraj Kumar <surajk732000@gmail.com>
2020-05-13 14:24:53 +0530

    Update artifact URL

--- examples/alexNet/setup.sh
+++ examples/alexNet/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lH-GwVcNY0Jmf10zMRhGbbXaTe-LIH76' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lH-GwVcNY0Jmf10zMRhGbbXaTe-LIH76" -O alexNet.pt && rm -rf /tmp/cookies.txt
+wget https://github.com/hasktorch/hasktorch-artifacts/releases/download/alexnet/alexNet.pt
 wget http://www.robots.ox.ac.uk/~vgg/data/pets/data/images.tar.gz
 tar -xf images.tar.gz
 rm images.tar.gz
