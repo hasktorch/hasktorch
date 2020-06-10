feffa2788367df12933b413511640c9cf4eb103c
Torsten Scholak <torsten.scholak@googlemail.com>
2020-06-10 18:19:18 -0400

    make compilation with haskell.nix work, add shell

--- default.nix
+++ default.nix
@@ -1,59 +1,84 @@
-{ haskellNix ? import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {}
+let sources = import ./nix/sources.nix; in
+{ haskellNix ? import sources.haskell-nix { sourcesOverride = sources; }
+, haskellNixOverlays ? (pkgsNew: pkgsOld: { haskell-nix = pkgsOld.haskell-nix // { hackageSrc = sources.hackage-nix; }; })
 , nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
 , haskellCompiler ? "ghc883"
 }:
-  let
-    libtorchSrc = pkgs:
-      let src = pkgs.fetchFromGitHub {
-            owner  = "stites";
-            repo   = "pytorch-world";
-            rev    = "6dc929a791918fff065bb40cbc3db8a62beb2a30";
-            sha256 = "140a2l1l1qnf7v2s1lblrr02mc0knsqpi06f25xj3qchpawbjd4c";
-      };
-      in (pkgs.callPackage "${src}/libtorch/release.nix" { });
+let
+  libtorchSrc = pkgs:
+    let src = pkgs.fetchFromGitHub {
+      owner = "stites";
+      repo = "pytorch-world";
+      rev = "6dc929a791918fff065bb40cbc3db8a62beb2a30";
+      sha256 = "140a2l1l1qnf7v2s1lblrr02mc0knsqpi06f25xj3qchpawbjd4c";
+    };
+    in (pkgs.callPackage "${src}/libtorch/release.nix" { });
 
-    libtorchOverlayCpu = pkgsNew: pkgsOld: 
-      let libtorch = (libtorchSrc pkgsOld).libtorch_cpu; in {
-        c10 = libtorch;
-        torch = libtorch;
-        torch_cpu = libtorch;
-      };
+  libtorchOverlayCpu = pkgsNew: pkgsOld:
+    let libtorch = (libtorchSrc pkgsOld).libtorch_cpu; in
+    {
+      c10 = libtorch;
+      torch = libtorch;
+      torch_cpu = libtorch;
+    };
 
-    libtorchOverlayCuda92 = pkgsNew: pkgsOld: 
-      let libtorch = (libtorchSrc pkgsOld).libtorch_cudatoolkit_9_2; in {
-        c10 = libtorch;
-        torch = libtorch;
-        torch_cpu = libtorch;
-        torch_cuda = libtorch;
-      };
+  libtorchOverlayCuda92 = pkgsNew: pkgsOld:
+    let libtorch = (libtorchSrc pkgsOld).libtorch_cudatoolkit_9_2; in
+    {
+      c10 = libtorch;
+      torch = libtorch;
+      torch_cpu = libtorch;
+      torch_cuda = libtorch;
+    };
 
-    libtorchOverlayCuda102 = pkgsNew: pkgsOld: 
-      let libtorch = (libtorchSrc pkgsOld).libtorch_cudatoolkit_10_2; in {
-        c10 = libtorch;
-        torch = libtorch;
-        torch_cpu = libtorch;
-        torch_cuda = libtorch;
-      };
+  libtorchOverlayCuda102 = pkgsNew: pkgsOld:
+    let libtorch = (libtorchSrc pkgsOld).libtorch_cudatoolkit_10_2; in
+    {
+      c10 = libtorch;
+      torch = libtorch;
+      torch_cpu = libtorch;
+      torch_cuda = libtorch;
+    };
 
-  hsPkgs = 
+  hsPkgs =
     { overlays
-    , pkgs ? (import nixpkgsSrc (haskellNix.nixpkgsArgs // { overlays = haskellNix.overlays ++ overlays; }))
-    }: pkgs.haskell-nix.cabalProject {
+    , pkgs ? (import nixpkgsSrc (haskellNix.nixpkgsArgs // { overlays = haskellNix.overlays ++ [ haskellNixOverlays ] ++ overlays; }))
+    , USE_CUDA ? false
+    , USE_GCC ? !USE_CUDA && pkgs.stdenv.hostPlatform.system == "x86_64-darwin"
+    }: if USE_CUDA && pkgs.stdenv.hostPlatform.system == "x86_64-darwin" then null else
+    pkgs.haskell-nix.cabalProject {
       src = pkgs.haskell-nix.haskellLib.cleanGit { name = "hasktorch"; src = ./.; };
       compiler-nix-name = haskellCompiler;
       modules = [
         ({ config, ... }: {
-          packages.libtorch-ffi.configureFlags = [ "--extra-include-dirs=${pkgs.torch}/include/torch/csrc/api/include" ];
+          packages.libtorch-ffi.configureFlags = [
+            "--extra-lib-dirs=${pkgs.torch}/lib"
+            "--extra-include-dirs=${pkgs.torch}/include"
+            "--extra-include-dirs=${pkgs.torch}/include/torch/csrc/api/include"
+          ];
+          packages.libtorch-ffi.flags.cuda = USE_CUDA;
+          packages.libtorch-ffi.flags.gcc = USE_GCC;
         })
       ];
     };
 
   hsPkgsCpu = hsPkgs { overlays = [ libtorchOverlayCpu ]; };
-  hsPkgsCuda92 = hsPkgs { overlays =  [ libtorchOverlayCuda92 ]; };
-  hsPkgsCuda102 = hsPkgs { overlays =  [ libtorchOverlayCuda102 ]; };
+  hsPkgsCuda92 = hsPkgs { overlays = [ libtorchOverlayCuda92 ]; USE_CUDA = true; };
+  hsPkgsCuda102 = hsPkgs { overlays = [ libtorchOverlayCuda102 ]; USE_CUDA = true; };
+
+  # f = x: y: if x == null then null else y;
+  # g = x: y: if x == null then null else x.y;
 in
 {
-  libtorchFfiCpu = hsPkgsCpu.libtorch-ffi;
+  inherit hsPkgsCpu;
+  inherit hsPkgsCuda92;
+  inherit hsPkgsCuda102;
+
+  # libtorchFFICpu = hsPkgsCpu.libtorch-ffi;
+  # ${f hsPkgsCuda92 "libtorchFFICuda92"} = g hsPkgsCuda92 "libtorch-ffi";
+  # ${f hsPkgsCuda102 "libtorchFFICuda102"} = g hsPkgsCuda102 "libtorch-ffi";
+  # libtorchFFICuda102 = hsPkgsCuda102.libtorch-ffi;
+  # hasktorchCpu = hsPkgsCpu.hasktorch;
 }
 
 
--- shell.nix
+++ shell.nix
@@ -1,1 +1,19 @@
-(import ./default.nix).shell
+args@{ haskellCompiler ? "ghc883", ...}:
+
+let
+  sources = import ./nix/sources.nix;
+  niv = (import sources.niv { }).niv;
+  hls = (import sources.hls-nix { }).hpkgs.haskell-language-server;
+  defaultHsPkgs = (import ./default.nix args).hsPkgsCuda102;
+in
+
+defaultHsPkgs.shellFor {
+  withHoogle = true;
+  tools = { cabal = "3.2.0.0"; };
+  buildInputs = [
+    niv
+    hls
+  ];
+  exactDeps = true;
+}
+
