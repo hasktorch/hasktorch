52739a76bf86447b1524779499e7c0429e583cd9
Torsten Scholak <torsten.scholak@googlemail.com>
2020-06-10 12:01:54 -0400

    move to a completely different approach

--- default.nix
+++ default.nix
@@ -1,82 +1,60 @@
-let
-  haskellNix = import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {};
-
-  nixpkgsSrc = haskellNix.sources.nixpkgs-2003;
-
-  libtorchSrc = pkgs:
-    let src = pkgs.fetchFromGitHub {
-          owner  = "stites";
-          repo   = "pytorch-world";
-          rev    = "6dc929a791918fff065bb40cbc3db8a62beb2a30";
-          sha256 = "140a2l1l1qnf7v2s1lblrr02mc0knsqpi06f25xj3qchpawbjd4c";
-    };
-    in (pkgs.callPackage "${src}/libtorch/release.nix" { });
-
-  libtorchOverlay = pkgsNew: pkgsOld: {
-    inherit (libtorchSrc pkgsOld)
-      libtorch_cpu
-      libtorch_cudatoolkit_9_2
-      libtorch_cudatoolkit_10_2
-    ;
-    # c10 = pkgsNew.libtorch_cpu;
-  };
-
-  nixpkgsArgs = haskellNix.nixpkgsArgs // { overlays = haskellNix.overlays ++ [ libtorchOverlay ]; };
-
-in
-
-{ pkgs ? import nixpkgsSrc nixpkgsArgs
+{ haskellNix ? import (builtins.fetchTarball https://github.com/input-output-hk/haskell.nix/archive/master.tar.gz) {}
+, nixpkgsSrc ? haskellNix.sources.nixpkgs-2003
 , haskellCompiler ? "ghc883"
 }:
+  let
+    libtorchSrc = pkgs:
+      let src = pkgs.fetchFromGitHub {
+            owner  = "stites";
+            repo   = "pytorch-world";
+            rev    = "6dc929a791918fff065bb40cbc3db8a62beb2a30";
+            sha256 = "140a2l1l1qnf7v2s1lblrr02mc0knsqpi06f25xj3qchpawbjd4c";
+      };
+      in (pkgs.callPackage "${src}/libtorch/release.nix" { });
 
-let
+    libtorchOverlayCpu = pkgsNew: pkgsOld: 
+      let libtorch = (libtorchSrc pkgsOld).libtorch_cpu; in {
+        c10 = libtorch;
+        torch = libtorch;
+        torch_cpu = libtorch;
+      };
 
-  # ghcide = (import sources.ghcide-nix {})."ghcide-${haskellCompiler}";
+    libtorchOverlayCuda92 = pkgsNew: pkgsOld: 
+      let libtorch = (libtorchSrc pkgsOld).libtorch_cudatoolkit_9_2; in {
+        c10 = libtorch;
+        torch = libtorch;
+        torch_cpu = libtorch;
+        torch_cuda = libtorch;
+      };
 
-  # 'cabalProject' generates a package set based on a cabal.project (and the corresponding .cabal files)
-  hsPkgs = let pkgsNew = pkgs // { overlays = pkgs.overlays ++ [ (pkgsNew: pkgsOld: { c10 = pkgsOld.libtorch_cpu; }) ]; }; in
-    pkgsNew.haskell-nix.cabalProject {
-      # 'cleanGit' cleans a source directory based on the files known by git
-      src = pkgsNew.haskell-nix.haskellLib.cleanGit { name = "hasktorch"; src = ./.; };
+    libtorchOverlayCuda102 = pkgsNew: pkgsOld: 
+      let libtorch = (libtorchSrc pkgsOld).libtorch_cudatoolkit_10_2; in {
+        c10 = libtorch;
+        torch = libtorch;
+        torch_cpu = libtorch;
+        torch_cuda = libtorch;
+      };
+
+  hsPkgs = 
+    { overlays
+    , pkgs ? (import nixpkgsSrc (haskellNix.nixpkgsArgs // { overlays = haskellNix.overlays ++ overlays; }))
+    }: pkgs.haskell-nix.cabalProject {
+      src = pkgs.haskell-nix.haskellLib.cleanGit { name = "hasktorch"; src = ./.; };
       compiler-nix-name = haskellCompiler;
-      # pkg-def-extras = [{
-      #   packages = { "c10" = pkgs.libtorch_cpu; };
-      # }];
       modules = [
         ({ config, ... }: {
-          packages.libtorch-ffi.configureFlags = [ "--extra-include-dirs=${pkgsNew."libtorch_cpu"}/include/torch/csrc/api/include" ];
+          packages.libtorch-ffi.configureFlags = [ "--extra-include-dirs=${pkgs.torch}/include/torch/csrc/api/include" ];
         })
       ];
-      # modules = [{
-      #   # Specific package overrides would go here for example:
-      #   packages.cbors.package.ghcOptions = "-Werror";
-      #   packages.cbors.patches = [ ./one.patch ];
-      #   packages.cbors.flags.optimize-gmp = false;
-      #   # It may be better to set flags in `cabal.project` instead
-      #   # (`plan-to-nix` will include them as defaults).
-      # }];
     };
 
-  shell = hsPkgs.shellFor {
-    withHoogle = true;
-    buildInputs = [
-      pkgs.cabal-install
-      pkgs.haskellPackages.ghcid
-      pkgs.haskellPackages.hpack
-      pkgs.haskellPackages.brittany
-      pkgs.haskellPackages.dhall
-      # ghcide
-    ];
-  };
-
+  hsPkgsCpu = hsPkgs { overlays = [ libtorchOverlayCpu ]; };
+  hsPkgsCuda92 = hsPkgs { overlays =  [ libtorchOverlayCuda92 ]; };
+  hsPkgsCuda102 = hsPkgs { overlays =  [ libtorchOverlayCuda102 ]; };
 in
-  rec {
-    inherit hsPkgs;
-
-    codegen = hsPkgs.codegen;
-
-    libtorch-ffi_cpu = hsPkgs.libtorch-ffi;
-  }
+{
+  libtorchFfiCpu = hsPkgsCpu.libtorch-ffi;
+}
 
 
 # let
