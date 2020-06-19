fce7d57cadcec6dae255363eb9ee9fd67159ab6e
Kiara Grouwstra <tycho01@pm.me>
2020-06-19 15:09:22 +0200

    upgrade a few more versions

--- setup-cabal.sh
+++ setup-cabal.sh
@@ -5,12 +5,12 @@ set -xe
 if ghc --version | grep 8.10 ; then
 ln -s cabal.project.freeze.ghc810 cabal.project.freeze
 else
-curl https://www.stackage.org/lts-15.0/cabal.config | \
-sed -e 's/inline-c ==.*,/inline-c ==0.9.0.0,/g' \
-    -e 's/inline-c-cpp ==.*,/inline-c-cpp ==0.4.0.0,/g' \
-    -e 's/ghc-typelits-natnormalise ==.*,/ghc-typelits-natnormalise ==0.7,/g' \
-    -e 's/ghc-typelits-knownnat ==.*,/ghc-typelits-knownnat ==0.7,/g' \
-    -e 's/ghc-typelits-extra ==.*,/ghc-typelits-extra ==0.4,/g' \
+curl https://www.stackage.org/nightly-2020-06-18/cabal.config | \
+# sed -e 's/inline-c ==.*,/inline-c ==0.9.0.0,/g' \
+    # -e 's/inline-c-cpp ==.*,/inline-c-cpp ==0.4.0.0,/g' \
+    # -e 's/ghc-typelits-natnormalise ==.*,/ghc-typelits-natnormalise ==0.7.2,/g' \
+    # -e 's/ghc-typelits-knownnat ==.*,/ghc-typelits-knownnat ==0.7.2,/g' \
+    # -e 's/ghc-typelits-extra ==.*,/ghc-typelits-extra ==0.4,/g' \
     > cabal.project.freeze
 fi
 
