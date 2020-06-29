735a284465c7a0eb07e052df766d30ce1e711e58
Torsten Scholak <torsten.scholak@googlemail.com>
2020-06-29 11:09:36 -0400

    remove cborg version pin from cabal project file and add version constraint for primitive to haskell.nix

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -37,6 +37,12 @@ let
     # on top of the package set derived from cabal resolution.
     pkg-def-extras = [
       extras
+      (hackage: {
+        packages = {
+          # see https://github.com/well-typed/cborg/issues/242
+          "primitive" = (((hackage.primitive)."0.7.0.0").revisions).default;
+        };
+      })
     ];
 
     modules = [
