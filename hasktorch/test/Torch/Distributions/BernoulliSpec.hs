7e982e4556762538b441e20c58a576a36ab26ca0
Kiara Grouwstra <tycho01@pm.me>
2020-07-03 22:20:51 +0200

    retry logProb old way

--- hasktorch/test/Torch/Distributions/BernoulliSpec.hs
+++ hasktorch/test/Torch/Distributions/BernoulliSpec.hs
@@ -61,9 +61,10 @@ spec = do
     toList (Just t) `shouldBe` [True, True]
 
   it "logProb" $ do
-    putStrLn . show $ logProb d $ D.asTensor [[0.3, 0.5 :: Float]]
+    -- putStrLn . show $ logProb d $ D.asTensor [[0.3, 0.5 :: Float]]
     let t :: Tnsr 'D.Float '[2] = UnsafeMkTensor $ logProb d $ D.asTensor [[0.3, 0.5 :: Float]]
-    F.allclose (toDynamic t) (D.asTensor [-0.6749387 :: Float]) 0.001 0.001 False `shouldBe` True
+    toList (Just t) `shouldBe` [-0.6749387]
+    -- F.allclose (toDynamic t) (D.asTensor [-0.6749387 :: Float]) 0.001 0.001 False `shouldBe` True
 
   it "entropy" $ do
     -- putStrLn . show $ entropy d
