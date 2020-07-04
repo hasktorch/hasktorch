ce37d15b68158f8b1f4ad5b3d9041ca995501df9
Kiara Grouwstra <tycho01@pm.me>
2020-07-04 16:31:08 +0200

    add shape check

--- hasktorch/test/Torch/Distributions/BernoulliSpec.hs
+++ hasktorch/test/Torch/Distributions/BernoulliSpec.hs
@@ -63,6 +63,7 @@ spec = do
   it "logProb" $ do
     -- putStrLn . show $ logProb d $ D.asTensor [[0.3, 0.5 :: Float]]
     let t :: Tnsr 'D.Float '[1] = UnsafeMkTensor $ logProb d $ D.asTensor [[0.3, 0.5 :: Float]]
+    (D.shape $ toDynamic t) `shouldBe` (D.shape $ D.asTensor [-0.6749387 :: Float])
     F.allclose (toDynamic t) (D.asTensor [-0.6749387 :: Float]) 0.001 0.001 False `shouldBe` True
 
   it "entropy" $ do
