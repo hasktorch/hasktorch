9d53cc237e14ac77e184a59c50b485cd1a04cc32
Torsten Scholak <torsten.scholak@googlemail.com>
2020-07-17 18:24:31 -0400

    add a bunch of boilerplate for FreshT

--- experimental/action-transition-system/Torch/Data/ActionTransitionSystem.hs
+++ experimental/action-transition-system/Torch/Data/ActionTransitionSystem.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DeriveFunctor #-}
@@ -36,11 +37,11 @@ import Control.Applicative (liftA2, pure, Alternative(..), empty, (<|>))
 import Control.Monad (mfilter, MonadPlus(..))
 import Data.Text (pack, Text)
 import GHC.IO (unsafePerformIO)
-import qualified Control.Monad.Gen as MGen (runGen, runGenT, gen, Gen)
+-- import qualified Control.Monad.Gen as Fresh (GenT(..), runGen, runGenT, gen, Gen)
 import Control.Monad.Yoctoparsec (Parser)
 import Control.Monad.Trans.Maybe (MaybeT(..))
 import Control.Monad.Trans.Free (wrap, iterTM, runFreeT, Free, FreeT(..), FreeF(..))
-import Control.Monad.State (StateT (..), runStateT, get, put, modify)
+import Control.Monad.State (evalStateT, MonadState(..), StateT (..), runStateT, get, put, modify)
 import Control.Monad (ap, void)
 import Control.Monad.Trans (MonadTrans(lift))
 import Data.Map as Map (elems, toList, (!), adjust, update, keys, null, insertWith, singleton, fromList, unionWith, Map, insert, lookup)
@@ -48,16 +49,36 @@ import Data.Set as Set (filter, cartesianProduct, unions, toList, fromList, memb
 import qualified Data.Set as Set (empty)
 import qualified Data.Map as Map (empty)
 import Data.List as List (filter, sort, nub)
-import Control.Monad.Reader (ask, local, runReaderT, ReaderT)
-import Hedgehog (discover, checkParallel, PropertyT, check, Property, (===), forAll, property, Gen, MonadGen)
+import Control.Monad.Reader (MonadReader(..), ask, local, runReaderT, ReaderT)
+import Hedgehog (distributeT, discover, checkParallel, PropertyT, check, Property, (===), forAll, property, Gen, MonadGen(..))
 import qualified Hedgehog.Gen as Gen
 import qualified Hedgehog.Range as Range
 import Control.Monad (guard)
 import Data.Maybe (fromMaybe)
 import Control.Monad (join)
 import Bound (fromScope, toScope, instantiate1, abstract1, (>>>=), Scope)
-import Data.Functor.Classes (Eq1(..), Show1(..))
+import Data.Functor.Classes (Eq1(..), Ord1(..), Show1(..))
+import Data.Deriving (deriveEq1, deriveOrd1, deriveShow1)
 import Bound (Var)
+import Data.Functor.Classes (eq1)
+import Data.Functor.Classes (compare1)
+import Data.Functor.Classes (showsPrec1)
+import Control.Monad.Morph (MFunctor, hoist)
+import Hedgehog.Internal.Distributive (Transformer, MonadTransDistributive)
+-- import Control.Monad.Gen (Successor)
+import Control.Monad.Writer.Class (MonadWriter(..))
+import Control.Monad.IO.Class (MonadIO(..))
+import Control.Monad.Cont.Class (MonadCont(..))
+import Control.Monad.Error.Class (MonadError(..))
+import Control.Monad.Fix (MonadFix(..))
+import Control.Monad.Trans.Identity (IdentityT)
+import Control.Monad.Trans.Cont (ContT)
+import Control.Monad.Trans.RWS (RWST)
+import Control.Monad.Trans.Except (ExceptT)
+import Control.Monad.Trans.Writer (WriterT)
+import qualified Control.Monad.Writer.Strict as SW
+import qualified Control.Monad.State.Strict as SS
+import Control.Monad.Trans.Reader (asks)
 
 -- https://stackoverflow.com/questions/17675054/deserialising-with-ghc-generics?rq=1
 -- http://hackage.haskell.org/package/cereal-0.5.8.1/docs/Data-Serialize.html
@@ -563,7 +584,7 @@ instance FromActions [] Expr
 data Ty =
     Arr Ty Ty
   | Nat
-  deriving (Eq, Show, Generic)
+  deriving (Eq, Ord, Show, Generic)
 
 instance ToActions [] Ty
 instance FromActions [] Ty
@@ -575,23 +596,37 @@ data Exp a =
   | Exp a :@ Exp a -- ^ Term application.
   | Suc (Exp a)
   | Zero
-  deriving (Eq, Show, Functor, Foldable, Traversable, Generic)
+  deriving (Functor, Foldable, Traversable, Generic)
 
 -- https://github.com/ekmett/bound/commit/6ab44d9a84a24e376bf29548ea61c1baf9ce3063
-instance Eq1 Exp where
-  liftEq eq (Var a) (Var b) = eq a b
-  liftEq eq (Lam ty b) (Lam ty' b') = liftEq eq b b' && ty == ty'
-  liftEq eq (a :@ a') (b :@ b') = liftEq eq a b && liftEq eq a' b'
-  liftEq eq (Suc a) (Suc b) = liftEq eq a b
-  liftEq _ Zero Zero = True
-  liftEq _ _ _ = False
+-- instance Eq1 Exp where
+--   liftEq eq (Var a) (Var b) = eq a b
+--   liftEq eq (Lam ty b) (Lam ty' b') = ty == ty' && liftEq eq b b'
+--   liftEq eq (a :@ a') (b :@ b') = liftEq eq a b && liftEq eq a' b'
+--   liftEq eq (Suc a) (Suc b) = liftEq eq a b
+--   liftEq _ Zero Zero = True
+--   liftEq _ _ _ = False
 
-instance Show1 Exp where
-  liftShowsPrec s _ d (Var a) = showParen (d > 10) $ showString "Var " . s 11 a
-  liftShowsPrec s sl d (a :@ b) = showParen (d > 9) $ liftShowsPrec s sl 9 a . showString " :@ " . liftShowsPrec s sl 10 b
-  liftShowsPrec s sl d (Lam _ b) = showParen (d > 10) $ showString "Lam " . liftShowsPrec s sl 11 b
-  liftShowsPrec s sl d (Suc b) = showParen (d > 10) $ showString "Suc " . liftShowsPrec s sl 11 b
-  liftShowsPrec _ _ d Zero = showParen (d > 10) $ showString "Zero"
+
+-- instance Ord1 Exp where
+--   liftCompare comp (Var a) (Var b) = comp a b
+--   liftCompare _ (Var _) (Lam _ _) = LT
+--   liftCompare _ (Var _) (_ :@ _) = LT
+--   liftCompare _ (Var _) (Suc _) = LT
+--   liftCompare _ (Var _) Zero = LT
+--   liftCompare _ (Lam _ _) (Var _) = GT
+--   liftCompare comp (Lam ty b) (Lam ty' b') = compare ty ty' `mappend` liftCompare comp b b'
+--   liftCompare _ (Lam _ _) (_ :@ _) = LT
+--   liftCompare _ (Lam _ _) (Suc _) = LT
+--   liftCompare _ (Lam _ _) Zero = LT
+--   ...
+
+-- instance Show1 Exp where
+--   liftShowsPrec s _ d (Var a) = showParen (d > 10) $ showString "Var " . s 11 a
+--   liftShowsPrec s sl d (a :@ b) = showParen (d > 9) $ liftShowsPrec s sl 9 a . showString " :@ " . liftShowsPrec s sl 10 b
+--   liftShowsPrec s sl d (Lam _ b) = showParen (d > 10) $ showString "Lam " . liftShowsPrec s sl 11 b
+--   liftShowsPrec s sl d (Suc b) = showParen (d > 10) $ showString "Suc " . liftShowsPrec s sl 11 b
+--   liftShowsPrec _ _ d Zero = showParen (d > 10) $ showString "Zero"
 
 instance Applicative Exp where
   pure = Var
@@ -605,12 +640,25 @@ instance Monad Exp where
   Suc e >>= f = Suc (e >>= f)
   Zero >>= _ = Zero
 
+deriveEq1 ''Exp
+deriveOrd1 ''Exp
+deriveShow1 ''Exp
+
+instance Eq a => Eq (Exp a) where (==) = eq1
+instance Ord a => Ord (Exp a) where compare = compare1
+instance Show a => Show (Exp a) where showsPrec = showsPrec1
+
 instance ToActions [] a => ToActions [] (Var () (Exp a))
 instance ToActions [] a => ToActions [] (Scope () Exp a)
 instance ToActions [] a => ToActions [] (Exp a)
 
 lam :: Eq a => Ty -> a -> Exp a -> Exp a
-lam ty v b = Lam ty (abstract1 v b)
+lam ty varName b = Lam ty (abstract1 varName b)
+
+-- | Smart constructor that converts the given positive integer to a corresponding Nat.
+nat :: (Num n, Eq n) => n -> Exp a
+nat 0 = Zero
+nat n = Suc $ nat (n-1)
 
 -- | Compute the normal form of an expression.
 nf :: Exp a -> Exp a
@@ -618,7 +666,7 @@ nf e@Var{} = e
 nf (Lam ty b) = Lam ty (toScope . nf . fromScope $ b)
 nf (f :@ a) = case whnf f of
   Lam ty b -> nf (instantiate1 a b)
-  f'    -> nf f' :@ nf a
+  f' -> nf f' :@ nf a
 nf e@Suc{} = e
 nf e@Zero = e
 
@@ -627,12 +675,12 @@ whnf :: Exp a -> Exp a
 whnf e@Var{} = e
 whnf e@Lam{} = e
 whnf (f :@ a) = case whnf f of
-  Lam ty b -> whnf (instantiate1 a b)
-  f'    -> f' :@ a
+  Lam _ b -> whnf (instantiate1 a b)
+  f' -> f' :@ a
 whnf e@Suc{} = e
 whnf e@Zero = e
 
-type TyM a = MaybeT (MGen.Gen a)
+type TyM a = MaybeT (Fresh a)
 
 assertTy :: Ord a => Map a Ty -> Exp a -> Ty -> TyM a ()
 assertTy env e t = (== t) <$> typeCheck env e >>= guard
@@ -645,17 +693,92 @@ typeCheck env (f :@ a) = typeCheck env f >>= \case
   Arr fTy tTy -> assertTy env a fTy >> return tTy
   _ -> mzero
 typeCheck env (Lam ty bind) = do
-  v <- MGen.gen
-  Arr ty <$> typeCheck (Map.insert v ty env) (instantiate1 (Var v) bind)
+  varName <- fresh
+  Arr ty <$> typeCheck (Map.insert varName ty env) (instantiate1 (Var varName) bind)
 
 testBound :: IO ()
 testBound = do
   let term :: Exp Int = (lam (Nat) 0 (Var 0)) :@ Zero
   print term -- Lam Nat (Scope (Var (B ()))) :@ Zero
-  print $ MGen.runGen . runMaybeT . typeCheck Map.empty $ term -- Just Nat
+  print $ runFresh . runMaybeT . typeCheck Map.empty $ term -- Just Nat
   print $ toActions @[] $ term -- [R,L,L,R,R,L,L,L,R,R,R]
-  print $ nf term  -- Zero
+  print $ nf term -- Zero
   print $ toActions @[] $ nf term -- [R,R,R]
+
+instance MFunctor (FreshT e) where
+  hoist nat m = FreshT $ hoist (hoist nat) (unFreshT m)
+
+instance MonadGen m => MonadGen (FreshT e m) where
+  type GenBase (FreshT e m) = FreshT e (GenBase m)
+  toGenT = hoist FreshT . distributeT . hoist distributeT . unFreshT . hoist toGenT
+  fromGenT = hoist fromGenT . distributeT
+
+genTy :: MonadGen m => m Ty
+genTy =
+  Gen.recursive Gen.choice [
+      pure Nat
+    ] [
+      Arr <$> genTy <*> genTy
+    ]
+
+genWellTypedExp :: (Eq a, Enum a) => Ty -> Gen (Exp a)
+genWellTypedExp ty = runFreshT $ runReaderT (genWellTypedExp' ty) mempty
+
+genWellTypedExp' :: Eq a => Ty -> GTyM a (Exp a)
+genWellTypedExp' ty =
+  Gen.shrink shrinkExp $
+  Gen.recursive Gen.choice [
+      genWellTypedExp'' ty
+    ] [
+      genWellTypedPath' ty <|> genWellTypedApp' ty
+    , genWellTypedApp' ty
+    ]
+
+shrinkExp :: Exp a -> [Exp a]
+shrinkExp (f :@ a) = case whnf f of
+  Lam _ b -> [whnf (instantiate1 a b)]
+  _ -> []
+shrinkExp _ = []
+
+type GTyM a = ReaderT (Map Ty [Exp a]) (FreshT a Gen)
+
+genWellTypedExp'' :: Eq a => Ty -> GTyM a (Exp a)
+genWellTypedExp'' (Arr ty ty') = do
+  varName <- fresh
+  lam ty varName <$> local (insertVar' varName ty) (genWellTypedExp' ty')
+genWellTypedExp'' Nat = nat <$> Gen.int (Range.linear 0 10)
+
+insertVar' :: Eq a => a -> Ty -> Map Ty [Exp a] -> Map Ty [Exp a]
+insertVar' varName ty =
+  Map.insertWith (<>) ty [Var varName] . fmap (List.filter (/= Var varName))
+
+genWellTypedApp' :: Eq a => Ty -> GTyM a (Exp a)
+genWellTypedApp' ty = do
+  tg <- genKnownTypeMaybe'
+  eg <- genWellTypedExp' tg
+  let tf = Arr tg ty
+  ef <- genWellTypedExp' tf
+  pure (ef :@ eg)
+
+-- | This tries to look up a known expression of the desired type from the env.
+-- It does not always succeed, throwing `empty` when unavailable.
+genWellTypedPath' :: Ty -> GTyM a (Exp a)
+genWellTypedPath' ty = do
+  paths <- ask
+  case fromMaybe [] (Map.lookup ty paths) of
+    [] -> empty
+    es -> Gen.element es
+
+genKnownTypeMaybe' :: GTyM a Ty
+genKnownTypeMaybe' = do
+  known <- ask
+  if Map.null known then
+    genTy
+  else
+    Gen.frequency [
+        (2, Gen.element $ Map.keys known)
+      , (1, genTy)
+      ]
 
 ------------------------------------------------------------------------
 
@@ -957,3 +1080,120 @@ prop_roundTrip = property $ do
 
 testSTLC :: IO Bool
 testSTLC = checkParallel $$(discover)
+
+-------------------------
+
+newtype Successor a = Successor {suc :: a -> a}
+
+-- | The monad transformer for generating fresh values.
+data FreshT e m a = FreshT { unFreshT :: ReaderT (Successor e) (StateT e m) a }
+                    deriving (Functor)
+
+instance Monad m => MonadFresh e (FreshT e m) where
+  fresh = FreshT $ do
+    s <- asks suc
+    modify s
+    get
+
+instance Monad m => Monad (FreshT e m) where
+  return = FreshT . return
+  (FreshT m) >>= f = FreshT $ m >>= unFreshT . f
+instance MonadPlus m => MonadPlus (FreshT e m) where
+  mzero = FreshT mzero
+  mplus (FreshT m) (FreshT m') = FreshT $ mplus m m'
+instance (Functor f, Monad f) => Applicative (FreshT e f) where
+  pure = FreshT . pure
+  (FreshT f) <*> (FreshT a) = FreshT $ f <*> a
+instance (Monad m, Functor m, MonadPlus m) => Alternative (FreshT e m) where
+  empty = mzero
+  (<|>) = mplus
+
+type Fresh e = FreshT e Identity
+
+instance MonadTrans (FreshT e) where
+  lift = FreshT . lift . lift
+
+instance MonadReader r m => MonadReader r (FreshT e m) where
+  local f m = FreshT $ ask >>= lift . local f . runReaderT (unFreshT m)
+  ask     = FreshT (lift ask)
+instance MonadState s m => MonadState s (FreshT e m) where
+  get    = FreshT $ (lift . lift) get
+  put    = FreshT . lift . lift . put
+instance (MonadWriter w m) => MonadWriter w (FreshT e m) where
+  tell m = lift $ tell m
+  listen = FreshT . listen . unFreshT
+  pass   = FreshT . pass . unFreshT
+instance MonadFix m => MonadFix (FreshT e m) where
+  mfix = FreshT . mfix . (unFreshT .)
+instance MonadIO m => MonadIO (FreshT e m) where
+  liftIO = FreshT . liftIO
+instance MonadCont m => MonadCont (FreshT e m) where
+  callCC f = FreshT $ callCC (unFreshT . f . (FreshT .))
+instance MonadError e m => MonadError e (FreshT e' m) where
+  throwError = FreshT . throwError
+  catchError m h = FreshT $ catchError (unFreshT m) (unFreshT . h)
+
+successor :: (e -> e) -> Successor e
+successor = Successor
+
+enumSucc :: Enum e => Successor e
+enumSucc = Successor succ
+
+-- | Run a @GenT@ computation starting from the value
+-- @toEnum 0@
+runFreshT :: (Enum e, Monad m) => FreshT e m a -> m a
+runFreshT = runFreshTFrom (toEnum 0)
+
+-- | Run a @Gen@ computation starting from the value
+-- @toEnum 0@
+runFresh :: Enum e => Fresh e a -> a
+runFresh = runFreshFrom (toEnum 0)
+
+-- | Run a @FreshT@ computation starting from a specific value @e@.
+runFreshTFrom :: (Monad m, Enum e) => e -> FreshT e m a -> m a
+runFreshTFrom e = runFreshTWith enumSucc e
+
+-- | Run a @Fresh@ computation starting from a specific value @e@.
+runFreshFrom :: Enum e => e -> Fresh e a -> a
+runFreshFrom e = runFreshWith enumSucc e
+
+-- | Run a @FreshT@ computation starting from a specific value @e@ with
+-- a the next fresh value determined by @Successor e@.
+runFreshTWith :: Monad m => Successor e -> e -> FreshT e m a -> m a
+runFreshTWith s e = flip evalStateT e
+                  . flip runReaderT s
+                  . unFreshT
+
+-- | Run a @FreshT@ computation starting from a specific value @e@ with
+-- a the next fresh value determined by @Successor e@.
+runFreshWith :: Successor e -> e -> Fresh e a -> a
+runFreshWith s e = runIdentity . runFreshTWith s e
+
+---------------------------
+
+-- | The MTL style class for generating fresh values
+class Monad m => MonadFresh e m | m -> e where
+  -- | Generate a fresh value @e@, @fresh@ should never produce the
+  -- same value within a monadic computation.
+  fresh :: m e
+
+instance MonadFresh e m => MonadFresh e (IdentityT m) where
+  fresh = lift fresh
+instance MonadFresh e m => MonadFresh e (StateT s m) where
+  fresh = lift fresh
+instance MonadFresh e m => MonadFresh e (ReaderT s m)  where
+  fresh = lift fresh
+instance (MonadFresh e m, Monoid s) => MonadFresh e (WriterT s m)  where
+  fresh = lift fresh
+instance MonadFresh e m => MonadFresh e (MaybeT m) where
+  fresh = lift fresh
+instance MonadFresh e m => MonadFresh e (ContT r m) where
+  fresh = lift fresh
+instance (Monoid w, MonadFresh e m) => MonadFresh e (RWST r w s m) where
+  fresh = lift fresh
+instance MonadFresh e m => MonadFresh e (SS.StateT s m) where
+  fresh = lift fresh
+instance (Monoid w, MonadFresh e m) => MonadFresh e (SW.WriterT w m) where
+  fresh = lift fresh
+instance (MonadFresh e m) => MonadFresh e (ExceptT e' m) where
+  fresh = lift fresh
