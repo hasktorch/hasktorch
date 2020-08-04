c54f48d003e4f86843d8f6a24e4333ccc377976f
austinvhuang <austinh@alum.mit.edu>
2020-08-03 23:14:17 -0400

    refactor data into its own module

---
+++ examples/covid/CovidData.hs
@@ -0,0 +1,114 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module CovidData where
+
+import qualified Data.ByteString.Lazy as BL
+import qualified Data.Vector as V
+import qualified Data.Map as M
+import qualified Data.Set as S
+import Data.Time
+import GHC.Generics (Generic)
+import Data.Csv
+import Torch as T
+
+data UsCounties = UsCounties
+  { date :: String,
+    county :: String,
+    state :: String,
+    fips :: String,
+    cases :: Int,
+    deaths :: Int
+  }
+  deriving (Eq, Generic, Show)
+
+instance FromRecord UsCounties
+-- instance FromNamedRecord UsCounties
+
+data ModelData = ModelData
+  { timePoints :: [Int],
+    fipsStrs :: [String],
+    fipsIdxs :: [Int],
+    fipsMap :: M.Map String Int,
+    caseCounts :: [Int],
+    deathCounts :: [Int]
+  }
+  deriving (Eq, Generic, Show)
+
+data TensorData = TensorData
+  { tTimes :: Tensor,
+    tFips :: Tensor,
+    tCases :: Tensor,
+    tDeaths :: Tensor
+  }
+  deriving (Eq, Generic, Show)
+
+loadDataset fileName = do
+  csvData <- BL.readFile fileName
+  case decode HasHeader csvData of
+    Left err -> error err
+    Right (v :: V.Vector UsCounties) -> pure v
+
+
+prepData :: V.Vector UsCounties -> IO ModelData
+prepData dataset = do
+  let fipsSet = S.fromList . V.toList $ fips <$> dataset
+  let idxMap = M.fromList $ zip (S.toList fipsSet)  [0 .. length fipsSet - 1] 
+  let indices = V.toList $ ((M.!) idxMap) <$> (fips <$> dataset)
+  times <- datesToTimepoints "2020-01-21" (V.toList $ date <$> dataset)
+  pure ModelData {
+    timePoints=times,
+    fipsStrs=V.toList $ fips <$> dataset,
+    fipsIdxs=indices,
+    fipsMap=idxMap,
+    caseCounts=V.toList $ cases <$> dataset,
+    deathCounts=V.toList $ deaths <$> dataset
+  }
+
+prepTensors :: ModelData -> TensorData
+prepTensors modelData =
+  TensorData {
+    tTimes = (asTensor . timePoints $ modelData),
+    tFips = (asTensor . fipsIdxs $ modelData),
+    tCases = (asTensor . caseCounts $ modelData),
+    tDeaths = (asTensor . deathCounts $ modelData)
+    }
+
+filterOn 
+  :: (TensorData -> Tensor) -- getter
+  -> (Tensor -> Tensor) -- predicate
+  -> TensorData -- input data
+  -> TensorData -- filtered data
+filterOn getter pred tData =
+  TensorData {
+    tTimes = selector (tTimes tData),
+    tFips = selector (tFips tData),
+    tCases = selector (tCases tData),
+    tDeaths = selector (tDeaths tData)
+    }
+  where
+    selector :: Tensor -> Tensor
+    selector = indexSelect 0 (squeezeAll . nonzero . pred . getter $ tData)
+  
+
+-- | Convert date strings to days since 1/21/2020 (the first date of this dataset)
+datesToTimepoints :: String -> [String] -> IO [Int]
+datesToTimepoints day0 dateStrings = do
+  firstDay :: Day <- parseTimeM False defaultTimeLocale "%F" day0
+  days :: [Day] <- sequence $ parseTimeM False defaultTimeLocale "%F" <$> dateStrings
+  pure $ fromIntegral <$> flip diffDays firstDay <$> days
+
+-- | calculate first differences of a tensor
+diff t = (indexSelect' 0 [1..len-1] t) - (indexSelect' 0 [0..len-2] t)
+  where len = shape t !! 0
+
+-- | trim leading zeros from a tensor
+trim t = 
+  if hasVal then
+    let firstNonzero = asValue $ nz ! (0 :: Int)
+      in indexSelect' 0 [firstNonzero..len-1] t
+  else t
+  where 
+    len = shape t !! 0
+    nz = squeezeAll . nonzero $ t 
+    hasVal = T.all $ toDType Bool nz
--- examples/covid/Main.hs
+++ examples/covid/Main.hs
@@ -7,13 +7,11 @@ module Main where
 import Control.Arrow (Arrow (first))
 import Control.Monad ((>=>), foldM, when)
 import Control.Monad.Cont (ContT (ContT), runCont, runContT)
-import qualified Data.ByteString.Lazy as BL
 
 import Data.Csv
 import qualified Data.Map as M
 import qualified Data.Set as S
 import Data.Time
-import Data.Time.Calendar.OrdinalDate (toOrdinalDate)
 import qualified Data.Vector as V
 import GHC.Generics (Generic)
 import qualified Graphics.Vega.VegaLite as VL hiding (sample, shape)
@@ -28,50 +26,12 @@ import Torch.Data.Pipeline (FoldM (FoldM))
 -- import Torch.Data.StreamedPipeline (MonadBaseControl, pmap, makeListT)
 import Torch.Data.StreamedPipeline
 
-data UsCounties = UsCounties
-  { date :: String,
-    county :: String,
-    state :: String,
-    fips :: String,
-    cases :: Int,
-    deaths :: Int
-  }
-  deriving (Eq, Generic, Show)
-
-
-data ModelData = ModelData
-  { timePoints :: [Int],
-    fipsStrs :: [String],
-    fipsIdxs :: [Int],
-    fipsMap :: M.Map String Int,
-    caseCounts :: [Int],
-    deathCounts :: [Int]
-  }
-  deriving (Eq, Generic, Show)
-
-data TensorData = TensorData
-  { tTimes :: Tensor,
-    tFips :: Tensor,
-    tCases :: Tensor,
-    tDeaths :: Tensor
-  }
-  deriving (Eq, Generic, Show)
-
-instance FromRecord UsCounties
-
--- instance FromNamedRecord UsCounties
+import CovidData
 
 modelTrain = undefined
 
 -- TODO - use this
 -- counties = (csvDataset @UsCounties "data/us-counties.csv") {batchSize = 4, shuffle = Nothing}
-
-loadDataset fileName = do
-  csvData <- BL.readFile fileName
-  case decode HasHeader csvData of
-    Left err -> error err
-    Right (v :: V.Vector UsCounties) -> pure v
-
 
 initializeEmbedding :: Int -> Tensor -> IO Tensor
 initializeEmbedding embedDim t =
@@ -79,53 +39,6 @@ initializeEmbedding embedDim t =
   where 
     (uniqT, _, _) = (T.uniqueDim 0 True False False t)
     nUniq = shape uniqT !! 0
-
-prepData :: V.Vector UsCounties -> IO ModelData
-prepData dataset = do
-  let fipsSet = S.fromList . V.toList $ fips <$> dataset
-  let idxMap = M.fromList $ zip (S.toList fipsSet)  [0 .. length fipsSet - 1] 
-  let indices = V.toList $ ((M.!) idxMap) <$> (fips <$> dataset)
-  times <- datesToTimepoints (V.toList $ date <$> dataset)
-  pure ModelData {
-    timePoints=times,
-    fipsStrs=V.toList $ fips <$> dataset,
-    fipsIdxs=indices,
-    fipsMap=idxMap,
-    caseCounts=V.toList $ cases <$> dataset,
-    deathCounts=V.toList $ deaths <$> dataset
-  }
-
-prepTensors :: ModelData -> TensorData
-prepTensors modelData =
-  TensorData {
-    tTimes = (asTensor . timePoints $ modelData),
-    tFips = (asTensor . fipsIdxs $ modelData),
-    tCases = (asTensor . caseCounts $ modelData),
-    tDeaths = (asTensor . deathCounts $ modelData)
-    }
-
-filterOn 
-  :: (TensorData -> Tensor) -- getter
-  -> (Tensor -> Tensor) -- predicate
-  -> TensorData -- input data
-  -> TensorData -- filtered data
-filterOn getter pred tData =
-  TensorData {
-    tTimes = selector (tTimes tData),
-    tFips = selector (tFips tData),
-    tCases = selector (tCases tData),
-    tDeaths = selector (tDeaths tData)
-    }
-  where
-    selector :: Tensor -> Tensor
-    selector = indexSelect 0 (squeezeAll . nonzero . pred . getter $ tData)
-  
-
-datesToTimepoints :: [String] -> IO [Int]
-datesToTimepoints dateStrings = do
-  firstDay :: Day <- parseTimeM False defaultTimeLocale "%F" "2020-01-21"
-  days :: [Day] <- sequence $ parseTimeM False defaultTimeLocale "%F" <$> dateStrings
-  pure $ fromIntegral <$> flip diffDays firstDay <$> days
 
 -- | Convert a series into a sparkline string (from clisparkline library)
 series2sparkline :: RealFrac a => [a] -> String
@@ -145,19 +58,6 @@ tensorSparkline t = putStrLn $ (series2sparkline (asValue t' :: [Float])) ++ (" 
     t' = toDType Float t
     maxValue = T.max t'
 
-diff t = (indexSelect' 0 [1..len-1] t) - (indexSelect' 0 [0..len-2] t)
-  where len = shape t !! 0
-
-trim t = 
-  if hasVal then
-    let firstNonzero = asValue $ nz ! (0 :: Int)
-      in indexSelect' 0 [firstNonzero..len-1] t
-  else t
-  where 
-    len = shape t !! 0
-    nz = squeezeAll . nonzero $ t 
-    hasVal = T.all $ toDType Bool nz
-
 main :: IO ()
 main = do
   -- let counties = (csvDataset @UsCounties "data/us-counties.csv") { batchSize = 4 , shuffle = Nothing}
@@ -175,14 +75,15 @@ main = do
   let locEmbed = embedding' weights tIndices
   print $ indexSelect' 0 [0..10] locEmbed
 
-  -- Autoencoder define fipsSpace
+  -- define fipsSpace
   let fipsList = M.keys . fipsMap $ modelData
 
+  -- check are there different numbers of points for different fips? yes
   print $ length $ Prelude.filter (\x -> x == 2102) (fipsIdxs modelData)
   print $ length $ Prelude.filter (\x -> x == 739) (fipsIdxs modelData)
 
-  pPrint $ filterOn tTimes (eq $ asTensor (20 :: Int)) tensorData 
-  pPrint $ filterOn tFips (eq $ asTensor (1 :: Int)) tensorData
+  -- pPrint $ filterOn tTimes (eq $ asTensor (20 :: Int)) tensorData 
+  -- pPrint $ filterOn tFips (eq $ asTensor (1 :: Int)) tensorData
 
   -- index 1222 = FIPS 25025
   let newCases = trim
