5fe40fdb7ce216f8adfb1b76b786bb3f018f759e
austinvhuang <austinh@alum.mit.edu>
2020-08-04 22:55:45 -0400

    t2v-lstm setup

--- examples/covid/TimeSeriesModel.hs
+++ examples/covid/TimeSeriesModel.hs
@@ -1,17 +1,46 @@
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 
 module TimeSeriesModel where
 
+import GHC.Generics
 import Torch as T
+import Torch.NN.Recurrent.Cell.LSTM
+
+
+data Time2VecSpec = Time2VecSpec {
+  t2vDim :: Int -- note output dimensions is +1 of this value due to non-periodic term
+} deriving (Eq, Show)
 
 data Time2Vec  = Time2Vec {
-  w0 :: Tensor, -- 0 dim
-  b0 :: Tensor, -- 0 dim
-  w :: Tensor, -- 1 dim
-  b :: Tensor -- 1 dim
-}
+  w0 :: Parameter, -- 0 dim
+  b0 :: Parameter, -- 0 dim
+  w :: Parameter,
+  b :: Parameter
+} deriving (Generic, Show)
 
--- t2v :: Float -> (Tensor, Tensor)
--- t2v t Time2Vec{..} = (mulScalar w0 t + b, T.sin (mulScalar w t _b))
+instance Randomizable Time2VecSpec Time2Vec where
+  sample Time2VecSpec{..} = do
+    w0' <- makeIndependent =<< randIO' [1]
+    b0' <- makeIndependent =<< randIO' [1]
+    w' <- makeIndependent =<< randIO' [t2vDim]
+    b' <- makeIndependent =<< randIO' [t2vDim]
+    pure $ Time2Vec {
+        w0=w0',
+        b0=b0',
+        w=w',
+        b=b' }
 
-model = undefined
+t2vForward :: Time2Vec -> Float -> Tensor
+t2vForward Time2Vec{..} t =
+  T.cat (Dim 0) [mulScalar t w0' + b0',
+                 T.sin $ mulScalar t w' + b']
+  where (w0', b0', w', b') = (toDependent w0, toDependent b0, 
+                              toDependent w, toDependent b)
+
+checkModel = do
+  t2v <- sample $ Time2VecSpec 10
+  lstmLayer <- sample $ LSTMSpec (10 + 1) 2
+  let result = t2vForward t2v 3.0
+  print result
