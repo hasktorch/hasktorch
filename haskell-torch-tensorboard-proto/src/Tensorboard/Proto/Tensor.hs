0a724eeee0b328da9bd97454f9e781fa73b48c02
Kiara Grouwstra <tycho01@pm.me>
2020-08-06 22:31:50 +0200

    initial tensorboard port

---
+++ haskell-torch-tensorboard-proto/src/Tensorboard/Proto/Tensor.hs
@@ -0,0 +1,11 @@
+module Tensorboard.Proto.Tensor(
+           module Proto.Tensorboard.Src.Tensor
+          ,module Proto.Tensorboard.Src.Tensor_Fields
+          ,module Proto.Tensorboard.Src.TensorShape
+          ,module Proto.Tensorboard.Src.TensorShape_Fields
+          )
+where
+import Proto.Tensorboard.Src.Tensor
+import Proto.Tensorboard.Src.Tensor_Fields
+import Proto.Tensorboard.Src.TensorShape
+import Proto.Tensorboard.Src.TensorShape_Fields
