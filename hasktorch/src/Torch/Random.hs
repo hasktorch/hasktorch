d913ef4327b9705b3d2f3d06023b5bc4a5a14f40
Flavio Corpa <flavio.corpa@47deg.com>
2020-10-07 14:13:32 +0200

    Apply feedback by @austinvhuang

--- hasktorch/src/Torch/Random.hs
+++ hasktorch/src/Torch/Random.hs
@@ -36,12 +36,11 @@ import Torch.TensorOptions
 instance Show (TVar (Maybe (ForeignPtr ATen.Generator))) where
   show _ = "_"
 
-data Generator
-  = Generator
-      { device :: Device,
-        seed :: Word64,
-        generator :: TVar (Maybe (ForeignPtr ATen.Generator))
-      }
+data Generator = Generator
+  { device :: Device,
+    seed :: Word64,
+    generator :: TVar (Maybe (ForeignPtr ATen.Generator))
+  }
   deriving (Eq, Show)
 
 mkGenerator :: Device -> Word64 -> IO Generator
@@ -82,26 +81,98 @@ generatorFactory func size options Generator {..} =
     nextGenenerator <- newTVarIO (Just genPtr)
     return (tensor, Generator device nextSeed nextGenenerator)
 
-randn :: [Int] -> TensorOptions -> Generator -> (Tensor, Generator)
+randn ::
+  -- | size
+  [Int] ->
+  -- | options
+  TensorOptions ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 randn = generatorFactory LibTorch.randn_lGo
 
-randn' :: [Int] -> Generator -> (Tensor, Generator)
+randn' ::
+  -- | size
+  [Int] ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 randn' size = randn size defaultOpts
 
-rand :: [Int] -> TensorOptions -> Generator -> (Tensor, Generator)
+rand ::
+  -- | size
+  [Int] ->
+  -- | options
+  TensorOptions ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 rand = generatorFactory LibTorch.rand_lGo
 
-rand' :: [Int] -> Generator -> (Tensor, Generator)
+rand' ::
+  -- | size
+  [Int] ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 rand' size = rand size defaultOpts
 
-randint :: Int -> Int -> [Int] -> TensorOptions -> Generator -> (Tensor, Generator)
+randint ::
+  -- | low
+  Int ->
+  -- | high
+  Int ->
+  -- | size
+  [Int] ->
+  -- | options
+  TensorOptions ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 randint low high = generatorFactory (LibTorch.randint_lllGo (fromIntegral low) (fromIntegral high))
 
-randint' :: Int -> Int -> [Int] -> Generator -> (Tensor, Generator)
+randint' ::
+  -- | low
+  Int ->
+  -- | high
+  Int ->
+  -- | size
+  [Int] ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 randint' low high size = randint low high size defaultOpts
 
-normal :: Double -> Double -> [Int] -> TensorOptions -> Generator -> (Tensor, Generator)
+normal ::
+  -- | mean
+  Double ->
+  -- | std
+  Double ->
+  -- | size
+  [Int] ->
+  -- | options
+  TensorOptions ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 normal mean std = generatorFactory (LibTorch.normal_ddlGo (realToFrac mean) (realToFrac std))
 
-normal' :: Double -> Double -> [Int] -> Generator -> (Tensor, Generator)
+normal' ::
+  -- | mean
+  Double ->
+  -- | std
+  Double ->
+  -- | size
+  [Int] ->
+  -- | generator
+  Generator ->
+  -- | output
+  (Tensor, Generator)
 normal' mean std size = normal mean std size defaultOpts
