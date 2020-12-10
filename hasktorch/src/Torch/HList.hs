0d62b6b5d1633b042c2b6663ff44b41aeca46971
Torsten Scholak <torsten.scholak@googlemail.com>
2020-12-10 09:03:41 -0500

    Update HList.hs

--- hasktorch/src/Torch/HList.hs
+++ hasktorch/src/Torch/HList.hs
@@ -237,7 +237,7 @@ instance
 
 hunfoldr ::
   forall f res (xs :: [Type]) a.
-  (Apply f a res, HUnfold f res xs) =>
+  (Apply f a res, HUnfold f res xs, res ~ HUnfoldRes s xs) =>
   f ->
   a ->
   HList xs
