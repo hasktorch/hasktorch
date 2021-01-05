d16c42bda4fb00ac576ce9d8c01e0d2f755f6b0a
Torsten Scholak <torsten.scholak@googlemail.com>
2021-01-05 13:43:31 -0500

    bump ghc

--- nix/haskell.nix
+++ nix/haskell.nix
@@ -5,7 +5,7 @@
 , buildPackages
 , config ? {}
 # GHC attribute name
-, compiler ? config.haskellNix.compiler or "ghc8102"
+, compiler ? config.haskellNix.compiler or "ghc8103"
 # Enable profiling
 , profiling ? config.haskellNix.profiling or false
 # Version info, to be passed when not building from a git work tree
