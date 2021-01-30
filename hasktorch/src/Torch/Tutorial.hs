794c89b56256925ae8948e94ec56a719c8b4bad9
Matt Wittmann <mcwitt@gmail.com>
2021-01-30 12:12:55 -0800

    Add tutorial outline

---
+++ hasktorch/src/Torch/Tutorial.hs
@@ -0,0 +1,53 @@
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+{- | Hasktorch is a library for tensor math and differentiable
+     programming in Haskell. It shares the backend C++ libtorch
+     library used by PyTorch and serves three primary objectives:
+
+    * Research on new functional programming methodology for
+      developing and representing models that are more productive or
+      lead to algorithm innovations.
+
+    * Building machine learning systems that are more reliable for the
+      model and its integration with the software in which the model
+      is embedded.
+
+    * Dissemination of new ideas from typed pure functional
+      programming for machine learning to other languages and machine
+      learning ecosystems.
+-}
+module Torch.Tutorial (
+    -- * Introduction
+    -- $introduction
+
+    -- * Usage
+    -- $usage
+
+    -- ** Tensors
+    -- $tensors
+
+    -- ** Typed Tensors
+    -- $typed-tensors
+) where
+
+{- $introduction
+   TODO
+-}
+
+{- $usage
+   In the following section we introduce Hasktorch usage at a high
+   level from an end-user perspective. In the sections that follow we
+   will delve into the underlying implementation concepts.
+
+   The sequence of topics and examples here is loosely based on the
+   PyTorch tutorial "Deep Learning with PyTorch: A 60 Minute Blitz" by
+   Soumith Chintala.
+-}
+
+{- $tensors
+   TODO
+-}
+
+{- $typed-tensors
+   TODO
+-}
