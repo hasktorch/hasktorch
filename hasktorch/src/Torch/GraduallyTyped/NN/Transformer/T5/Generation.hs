66256dd814fd486c5f36b5494829ec1e36d65fb5
Torsten Scholak <torsten.scholak@googlemail.com>
2021-02-11 12:10:31 -0500

    work towards backtracking

--- hasktorch/src/Torch/GraduallyTyped/NN/Transformer/T5/Generation.hs
+++ hasktorch/src/Torch/GraduallyTyped/NN/Transformer/T5/Generation.hs
@@ -6,6 +6,7 @@
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -15,9 +16,10 @@
 module Torch.GraduallyTyped.NN.Transformer.T5.Generation where
 
 import Control.Applicative (Alternative (many, (<|>)), Applicative (liftA2), empty)
-import Control.Monad (MonadPlus, guard, mfilter)
-import Control.Monad.State (MonadState (..), StateT (..), evalStateT)
-import Control.Monad.Trans.Free (FreeF (Free, Pure), FreeT (FreeT), iterTM)
+import Control.Monad (MonadPlus, mfilter)
+import Control.Monad.Identity (runIdentityT)
+import Control.Monad.State (modify, MonadState (..), MonadTrans, StateT (..), evalStateT, lift)
+import Control.Monad.Trans.Free (FreeF (Free, Pure), FreeT (FreeT), iterTM, runFreeT)
 import Data.Foldable (asum)
 import Data.Functor (($>))
 import Data.Kind (Type)
@@ -38,9 +40,11 @@ import Torch.GraduallyTyped.RequiresGradient (RequiresGradient (..))
 import Torch.GraduallyTyped.Shape.Class (BroadcastShapesF)
 import Torch.GraduallyTyped.Shape.Type (By (..), Dim (..), KnownShape, Name (..), SelectDim (..), Shape (..), Size (..))
 import Torch.GraduallyTyped.Tensor.IndexingSlicingJoining (expand)
+import Torch.GraduallyTyped.Tensor.MathOperations.Comparison (Order (..), Sorted (..), sort)
 import Torch.GraduallyTyped.Tensor.Type (Tensor (..), shape)
 import qualified Torch.Tensor
 import Prelude hiding (Word, words)
+import System.IO.Unsafe (unsafePerformIO)
 
 data IsFinished = Finished | Unfinished
 
@@ -190,47 +194,47 @@ foo ::
   input ->
   generator ->
   IO [Beams Int [Int]]
-foo maxSteps beamSize model input g = do
-  let cont :: [Hypothesis 'Unfinished Int [Int]] -> StateT (Maybe (encoderOutput, inputPaddingMask), generator) IO [SomeHypothesis Int [Int]]
-      cont previousHypotheses = do
-        let previousHypotheses' = nub previousHypotheses
-        decoderInput :: decoderInput <- do
-          let tokens = reverse . getTokens <$> previousHypotheses'
-              batchSize = fromIntegral $ length previousHypotheses'
-              seqSize =
-                let go :: Hypothesis 'Unfinished Int [Int] -> Int
-                    go InitialHypothesis = 0
-                    go (UnfinishedHypothesis _ _ previousHypothesis') = 1 + go previousHypothesis'
-                 in fromIntegral . maximum $ go <$> previousHypotheses'
-          input <- mkT5Input @( 'Dim ( 'Name "*") 'UncheckedSize) @( 'Dim ( 'Name "*") 'UncheckedSize) batchSize seqSize tokens
-          -- liftIO . print $ ((t5Vocab Map.!) <$>) <$> tokens
-          -- liftIO . print $ shape input
-          -- liftIO . print $ input
-          pure input
-        logProbs :: [[[Float]]] <- do
-          (maybeStuff, g) <- get
-          let (T5Output decoderOutput encoderOutput inputPaddingMask, g') = case maybeStuff of
-                Nothing -> forward model (T5Input input decoderInput) g
-                Just (encoderOutput, inputPaddingMask) ->
-                  let decoderInputBatchDim : _ = shape decoderInput
-                      _encoderOutputBatchDim : encoderOutputDims = shape encoderOutput
-                      encoderOutput' = expand @ 'UncheckedShape (decoderInputBatchDim : encoderOutputDims) encoderOutput
-                   in case forward model (T5GenerationInput decoderInput encoderOutput' inputPaddingMask) g of
-                        (T5Output decoderOutput _ _, g') -> (T5Output decoderOutput encoderOutput inputPaddingMask, g')
-          put (Just (encoderOutput, inputPaddingMask), g')
-          case logSoftmax @( 'SelectDim ( 'ByIndex 2)) decoderOutput of
-            UnsafeTensor t -> pure . Torch.Tensor.asValue . Torch.Tensor.Unsafe $ t
-        pure $ zip previousHypotheses' logProbs >>= uncurry (\previousHypothesis -> zipWith (mkHypothesis previousHypothesis) [0, 1 ..] . last)
-      mkHypothesis :: Hypothesis 'Unfinished Int [Int] -> Int -> Float -> SomeHypothesis Int [Int]
-      mkHypothesis previousHypothesis token logProb
-        | token == t5EosTokenId =
-          let finalValue = reverse $ token : getTokens previousHypothesis
-              finalScore = logProb + getScore previousHypothesis
-           in SomeHypothesis (FinishedHypothesis token finalScore previousHypothesis finalValue)
-        | otherwise =
-          let score = logProb + getScore previousHypothesis
-           in SomeHypothesis (UnfinishedHypothesis token score previousHypothesis)
+foo maxSteps beamSize model input g =
   evalStateT (beamSearch maxSteps beamSize cont) (Nothing, g)
+  where
+    cont :: [Hypothesis 'Unfinished Int [Int]] -> StateT (Maybe (encoderOutput, inputPaddingMask), generator) IO [SomeHypothesis Int [Int]]
+    cont previousHypotheses = do
+      let previousHypotheses' = nub previousHypotheses
+      decoderInput :: decoderInput <- do
+        let tokens = reverse . getTokens <$> previousHypotheses'
+            batchSize = fromIntegral $ length previousHypotheses'
+            seqSize =
+              let go :: Hypothesis 'Unfinished Int [Int] -> Int
+                  go InitialHypothesis = 0
+                  go (UnfinishedHypothesis _ _ previousHypothesis') = 1 + go previousHypothesis'
+               in fromIntegral . maximum $ go <$> previousHypotheses'
+        -- liftIO . print $ ((t5Vocab Map.!) <$>) <$> tokens
+        mkT5Input @( 'Dim ( 'Name "*") 'UncheckedSize) @( 'Dim ( 'Name "*") 'UncheckedSize) batchSize seqSize tokens
+      logProbs <- getLogProbs decoderInput
+      pure $ zip previousHypotheses' logProbs >>= uncurry (\previousHypothesis -> zipWith (mkHypothesis previousHypothesis) [0, 1 ..] . last)
+    getLogProbs :: decoderInput -> StateT (Maybe (encoderOutput, inputPaddingMask), generator) IO [[[Float]]]
+    getLogProbs decoderInput = do
+      (maybeStuff, g) <- get
+      let (T5Output decoderOutput encoderOutput inputPaddingMask, g') = case maybeStuff of
+            Nothing -> forward model (T5Input input decoderInput) g
+            Just (encoderOutput, inputPaddingMask) ->
+              let decoderInputBatchDim : _ = shape decoderInput
+                  _encoderOutputBatchDim : encoderOutputDims = shape encoderOutput
+                  encoderOutput' = expand @ 'UncheckedShape (decoderInputBatchDim : encoderOutputDims) encoderOutput
+               in case forward model (T5GenerationInput decoderInput encoderOutput' inputPaddingMask) g of
+                    (T5Output decoderOutput _ _, g') -> (T5Output decoderOutput encoderOutput inputPaddingMask, g')
+      put (Just (encoderOutput, inputPaddingMask), g')
+      case logSoftmax @( 'SelectDim ( 'ByIndex 2)) decoderOutput of
+        UnsafeTensor t -> pure . Torch.Tensor.asValue . Torch.Tensor.Unsafe $ t
+    mkHypothesis :: Hypothesis 'Unfinished Int [Int] -> Int -> Float -> SomeHypothesis Int [Int]
+    mkHypothesis previousHypothesis token logProb
+      | token == t5EosTokenId =
+        let finalValue = reverse $ token : getTokens previousHypothesis
+            finalScore = logProb + getScore previousHypothesis
+         in SomeHypothesis (FinishedHypothesis token finalScore previousHypothesis finalValue)
+      | otherwise =
+        let score = logProb + getScore previousHypothesis
+         in SomeHypothesis (UnfinishedHypothesis token score previousHypothesis)
 
 bar = do
   input <- do
@@ -247,53 +251,217 @@ bar = do
       "/Users/tscholak/Projects/thirdParty/hasktorch/hasktorch/src/Torch/GraduallyTyped/NN/Transformer/t5-small.pt"
   g <- mkGenerator @( 'Device CPU) 0
   Beams finished _ <- last <$> foo 50 1 model input g
-  let tmp = fmap (WordPiece . (t5Vocab Map.!)) . finalValue <$> finished
-      tmp' = parseString @[] words <$> tmp
-  print tmp'
+  print $ finalValue <$> finished
+  let tmp = parseString @[] words . finalValue <$> finished
+  print tmp
 
 newtype WordPiece = WordPiece {unWordPiece :: String} deriving (Eq, Ord, Show)
 
 newtype Word = Word {unWord :: String} deriving (Eq, Ord, Show)
 
-word :: forall b. MonadPlus b => Parser b WordPiece Word
+word :: forall b. MonadPlus b => Parser b Int Word
 word =
   let f (c, _)
         | c == '▁' = True
         | otherwise = False
       p (WordPiece ",") = True
+      p (WordPiece ":") = True
+      p (WordPiece ".") = True
+      p (WordPiece ";") = True
+      p (WordPiece "!") = True
+      p (WordPiece "-") = True
+      p (WordPiece "?") = True
+      p (WordPiece "...") = True
       p (WordPiece s) = maybe False f (uncons s)
-      begin = predicate p
-      append = predicate (not . p)
+      p' (WordPiece "</s>") = False
+      p' _ = True
+      predicate p = mfilter p (WordPiece . (t5Vocab Map.!) <$> token)
+      begin = predicate (\wp -> p' wp && p wp)
+      append = predicate (\wp -> p' wp && not (p wp))
       strip ('▁' : s) = s
       strip s = s
       concat = strip . mconcat . (unWordPiece <$>)
    in Word . concat <$> liftA2 (:) begin (many append)
 
-words :: forall b. MonadPlus b => Parser b WordPiece [Word]
+words :: forall b. MonadPlus b => Parser b Int [Word]
 words =
-  let end = is (WordPiece "</s>")
+  let predicate p' = mfilter p' (WordPiece . (t5Vocab Map.!) <$> token)
+      is t = predicate (== t)
+      end = is (WordPiece "</s>")
    in manyTill word end
 
-type Parser (b :: Type -> Type) (t :: Type) (a :: Type) = FreeT ((->) t) b a
+type Parser (b :: Type -> Type) (i :: Type) (a :: Type) = FreeT ((->) i) b a
 
-parseStream :: Monad b => (s -> b (t, s)) -> Parser b t a -> s -> b (a, s)
+-- | recurse parser
+-- @
+-- recurse next parser = next parser (\parser' -> next parser' (\parser'' -> next parser'' (\parser''' -> next parser''' (...))))
+-- @
+recurse ::
+  forall t b i a.
+  (Parser b i a -> (Parser b i a -> t b a) -> t b a) ->
+  Parser b i a ->
+  t b a
+recurse next parser =
+  let cont = recurse next
+   in next parser cont
+
+next ::
+  forall t b i a.
+  (MonadTrans t, Monad b, Monad (t b)) =>
+  Parser b i a ->
+  (Parser b i a -> t b a) ->
+  t b a
+next parser cont = do
+  val <- lift . runFreeT $ parser
+  case val of
+    Pure a -> pure a
+    Free feed ->
+      let i = undefined
+       in cont (feed i)
+
+next' ::
+  forall t b i a.
+  _ =>
+  Parser (StateT [i] b) i a ->
+  (Parser (StateT [i] b) i a -> t (StateT [i] b) a) ->
+  t (StateT [i] b) a
+next' parser cont = do
+  val <- lift $ runFreeT parser
+  case val of
+    Pure a -> pure a
+    Free feed ->
+      let is :: StateT [i] b i = undefined
+       in cont . feed =<< lift is
+
+testNext' :: Parser (StateT [i] []) i a -> [(a, [i])]
+testNext' = flip runStateT [] . runIdentityT . recurse next'
+
+next'' ::
+  forall s a model input decoderInput encoderOutput decoderOutput inputPaddingMask generator.
+  ( s ~ (Maybe (encoderOutput, inputPaddingMask), generator),
+    decoderInput
+      ~ Tensor
+          'WithoutGradient
+          ( 'Layout 'Dense)
+          ( 'Device 'CPU)
+          ( 'DataType 'Int64)
+          ( 'Shape
+              '[ 'Dim ( 'Name "*") ( 'Size 1), 'Dim ( 'Name "*") 'UncheckedSize]
+          ),
+    decoderOutput
+      ~ Tensor
+          'WithGradient
+          ( 'Layout 'Dense)
+          ( 'Device 'CPU)
+          T5DataType
+          'UncheckedShape,
+    HasForward
+      model
+      (T5Input input decoderInput)
+      generator
+      (T5Output decoderOutput encoderOutput inputPaddingMask)
+      generator,
+    HasForward
+      model
+      (T5GenerationInput decoderInput encoderOutput inputPaddingMask)
+      generator
+      (T5Output decoderOutput encoderOutput inputPaddingMask)
+      generator
+  ) =>
+  model ->
+  input ->
+  Parser (StateT [Int] []) Int a ->
+  (Parser (StateT [Int] []) Int a -> StateT s (StateT [Int] []) a) ->
+  StateT s (StateT [Int] []) a
+next'' model input parser cont = do
+  -- currently every submitted token is tried for the current parser until moving on to the next parser.
+  -- what we need to do instead is trying all parsers on each token before moving on to the next token.
+  val <- lift $ runFreeT parser
+  case val of
+    Pure a -> pure a
+    Free feed -> do
+      i <- is
+      let j = unsafePerformIO $ do
+                let k = i
+                putStrLn $ "current i: " <> show k
+                pure k
+      lift $ modify (j:)
+      cont . feed $ j
+  where
+    is :: StateT s (StateT [Int] []) Int
+    is = do
+      -- tokens <- reverse <$> lift get
+      tokens <- do
+        ts <- reverse <$> lift get
+        let ts' = unsafePerformIO $ do
+              putStrLn $ "tokens: " <> show ((t5Vocab Map.!) <$> ts)
+              pure ts
+        pure ts'
+      decoderInput :: decoderInput <- mkT5Input @( 'Dim ( 'Name "*") ( 'Size 1)) @( 'Dim ( 'Name "*") 'UncheckedSize) (fromIntegral $ length tokens) [tokens]
+      decoderOutput <- do
+        (mTensors, g) <- get
+        let (T5Output decoderOutput encoderOutput inputPaddingMask, g') =
+              case mTensors of
+                Nothing -> forward model (T5Input input decoderInput) g
+                Just (encoderOutput, inputPaddingMask) ->
+                  forward model (T5GenerationInput decoderInput encoderOutput inputPaddingMask) g
+        put (Just (encoderOutput, inputPaddingMask), g')
+        pure decoderOutput
+      case sort @( 'SelectDim ( 'ByIndex 2)) Descending
+        . logSoftmax @( 'SelectDim ( 'ByIndex 2))
+        $ decoderOutput of
+        Sorted _ (UnsafeTensor indices) ->
+          let indices' = -- take 3 . last . head . Torch.Tensor.asValue @[[[Int]]] . Torch.Tensor.Unsafe $ indices
+                unsafePerformIO $ do
+                  let tmp = take 15 . last . head . Torch.Tensor.asValue @[[[Int]]] . Torch.Tensor.Unsafe $ indices
+                  putStrLn $ "top-2: " <> show (take 2 tmp)
+                  -- putStrLn $ "bottom-5: " <> show (reverse $ take 5 $ reverse tmp)
+                  pure tmp
+          in  lift . lift $ indices'
+
+testNext'' ::
+  forall model input generator a encoderOutput inputPaddingMask.
+  _ =>
+  model ->
+  input ->
+  generator ->
+  Parser (StateT [Int] []) Int a ->
+  [((a, (Maybe (encoderOutput, inputPaddingMask), generator)), [Int])]
+testNext'' model input g = flip runStateT [] . flip runStateT (Nothing, g) . recurse (next'' model input)
+
+bar' = do
+  input <- do
+    let tokens = [[13959, 1566, 12, 2968, 10, 6536, 43, 2008, 24, 293, 53, 3, 9, 1782, 19, 207, 21, 25, 1]]
+    -- let tokens = [[13959, 1566, 12, 2968, 10, 148, 31, 60, 423, 13, 3, 7, 10536, 55, 1]]
+    print $ ((t5Vocab Map.!) <$>) <$> tokens
+    mkT5Input
+      @( 'Dim ( 'Name "*") ( 'Size 1))
+      @( 'Dim ( 'Name "*") ( 'Size 19))
+      tokens
+  model <-
+    initialize
+      @(T5Small 'WithLMHead ( 'Device 'CPU))
+      "/Users/tscholak/Projects/thirdParty/hasktorch/hasktorch/src/Torch/GraduallyTyped/NN/Transformer/t5-small.pt"
+  g <- mkGenerator @( 'Device CPU) 0
+  let outputs = testNext'' model input g words
+  print (take 1 $ fst . fst <$> outputs)
+
+parseStream :: forall s b i a. Monad b => (s -> b (i, s)) -> Parser b i a -> s -> b (a, s)
 parseStream next = runStateT . iterTM (StateT next >>=)
 
--- | Parse a string. When the end of the string is encountered, 'empty' is
--- yielded into the non-determinism monad.
-parseString :: forall b t a. MonadPlus b => Parser b t a -> [t] -> b (a, [t])
+parseString :: forall b i a. MonadPlus b => Parser b i a -> [i] -> b (a, [i])
 parseString = parseStream (maybe empty pure . uncons)
 
-token :: forall b t. Applicative b => Parser b t t
+token :: forall b i. Applicative b => Parser b i i
 token = FreeT . pure . Free $ FreeT . pure . Pure
 
-predicate :: forall b t. (MonadPlus b) => (t -> Bool) -> Parser b t t
+predicate :: forall b i. (MonadPlus b) => (i -> Bool) -> Parser b i i
 predicate p = mfilter p token
 
-is :: forall b t. (MonadPlus b, Eq t) => t -> Parser b t t
+is :: forall b i. (MonadPlus b, Eq i) => i -> Parser b i i
 is t = predicate (== t)
 
-isNot :: forall b t. (MonadPlus b, Eq t) => t -> Parser b t t
+isNot :: forall b i. (MonadPlus b, Eq i) => i -> Parser b i i
 isNot t = predicate (/= t)
 
 choice :: Alternative f => [f a] -> f a
--- hasktorch/src/Torch/GraduallyTyped/Tensor/MathOperations/Comparison.hs
+++ hasktorch/src/Torch/GraduallyTyped/Tensor/MathOperations/Comparison.hs
@@ -1,20 +1,30 @@
+{-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 module Torch.GraduallyTyped.Tensor.MathOperations.Comparison where
 
+import GHC.TypeLits (Nat, Symbol)
 import System.IO.Unsafe (unsafePerformIO)
 import Torch.DType (DType (..))
 import Torch.GraduallyTyped.DType (DataType (..))
 import Torch.GraduallyTyped.Prelude (Seq)
 import Torch.GraduallyTyped.RequiresGradient (RequiresGradient (..))
-import Torch.GraduallyTyped.Shape.Class (BroadcastShapesF)
+import Torch.GraduallyTyped.Shape.Class (BroadcastShapesF, GetDimImplF)
+import Torch.GraduallyTyped.Shape.Type (By (..), Dim (..), Name (..), SelectDim (..), Shape (..), Size (..), WithSelectDimC (..))
 import Torch.GraduallyTyped.Tensor.Type (Tensor)
 import Torch.GraduallyTyped.Unify (type (<+>))
-import Torch.Internal.Cast (cast2)
+import Torch.Internal.Cast (cast2, cast3)
 import qualified Torch.Internal.Managed.Native as ATen
+import qualified Torch.Internal.Managed.Type.Tuple as ATen ()
+import Type.Errors.Pretty (TypeError, type (%), type (<>))
 
 gt,
   lt,
@@ -49,3 +59,56 @@ a `ne` b = unsafePerformIO $ cast2 ATen.ne_tt a b
 (<=.) = le
 (==.) = eq
 (/=.) = ne
+
+data Order = Ascending | Descending deriving (Show, Eq, Ord)
+
+data Sorted requiresGradient layout device dataType shape where
+  Sorted ::
+    forall requiresGradient layout device dataType shape.
+    { sorted :: Tensor requiresGradient layout device dataType shape,
+      indices :: Tensor 'WithoutGradient layout device ( 'DataType 'Int64) shape
+    } ->
+    Sorted requiresGradient layout device dataType shape
+
+type SortErrorMessage (by :: By Symbol Nat) (dims :: [Dim (Name Symbol) (Size Nat)]) =
+  "Cannot apply sort on the dimension matching"
+    % ""
+    % "    '" <> by <> "'"
+    % ""
+    % "in the shape"
+    % ""
+    % "    '" <> dims <> "'."
+    % ""
+
+type family SortCheckF (by :: By Symbol Nat) (dims :: [Dim (Name Symbol) (Size Nat)]) (result :: Maybe (Dim (Name Symbol) (Size Nat))) :: [Dim (Name Symbol) (Size Nat)] where
+  SortCheckF by dims 'Nothing = TypeError (SortErrorMessage by dims)
+  SortCheckF _ dims ( 'Just _) = dims
+
+type family SortF (selectDim :: SelectDim (By Symbol Nat)) (shape :: Shape [Dim (Name Symbol) (Size Nat)]) :: Shape [Dim (Name Symbol) (Size Nat)] where
+  SortF 'UncheckedSelectDim _ = 'UncheckedShape
+  SortF _ 'UncheckedShape = 'UncheckedShape
+  SortF ( 'SelectDim by) ( 'Shape dims) = 'Shape (SortCheckF by dims (GetDimImplF by dims))
+
+sort ::
+  forall selectDim requiresGradient layout device dataType shape.
+  WithSelectDimC
+    selectDim
+    ( Order ->
+      Tensor requiresGradient layout device dataType shape ->
+      Sorted requiresGradient layout device dataType (SortF selectDim shape)
+    ) =>
+  WithSelectDimF
+    selectDim
+    ( Order ->
+      Tensor requiresGradient layout device dataType shape ->
+      Sorted requiresGradient layout device dataType (SortF selectDim shape)
+    )
+sort = withSelectDim @selectDim
+  @( Order ->
+     Tensor requiresGradient layout device dataType shape ->
+     Sorted requiresGradient layout device dataType (SortF selectDim shape)
+   )
+  $ \by order tensor ->
+    uncurry Sorted $ case by of
+      ByName name -> unsafePerformIO $ cast3 ATen.sort_tnb tensor name (order == Descending)
+      ByIndex index -> unsafePerformIO $ cast3 ATen.sort_tlb tensor (fromInteger index :: Int) (order == Descending)
