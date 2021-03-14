129be3f2ae23e20521859baa70e739066e8db490
Matt Wittmann <mcwitt@gmail.com>
2021-03-14 15:44:56 -0700

    Remove hls from custom-tools

--- nix/default.nix
+++ nix/default.nix
@@ -36,31 +36,6 @@ let
         haskell-nix = pkgsOld.haskell-nix // {
           hackageSrc = sources.hackage-nix;
           stackageSrc = sources.stackage-nix;
-          custom-tools = pkgsOld.haskell-nix.custom-tools // {
-            haskell-language-server."1.0.0" = args:
-              let
-                project = pkgsOld.haskell-nix.project' (args // {
-                  src = pkgsOld.evalPackages.fetchgit {
-                    url = "https://github.com/haskell/haskell-language-server.git";
-                    fetchSubmodules = true;
-                    rev = "4cd1cf934638881e52b3eba9f70157a4b799c0e9";
-                    sha256 = "0p0rhhc6pldzan85qp3nhc54zwabah8r3dvxdzw49i32dvy4xxgs";
-                  };
-                  projectFileName = "cabal.project";
-                  sha256map = {
-                    "https://github.com/bubba/brittany.git"."c59655f10d5ad295c2481537fc8abf0a297d9d1c" = "1rkk09f8750qykrmkqfqbh44dbx1p8aq1caznxxlw8zqfvx39cxl";
-                    "https://github.com/bubba/hie-bios.git"."cec139a1c3da1632d9a59271acc70156413017e7" = "1iqk55jga4naghmh8zak9q7ssxawk820vw8932dhympb767dfkha";
-                  };
-                  cabalProjectLocal = ''
-                    allow-newer: diagrams-svg:base, monoid-extras:base, svg-builder:base,
-                      diagrams-lib:base, dual-tree:base, active:base, diagrams-core:base,
-                      diagrams-contrib:base, force-layout:base, diagrams-postscript:base,
-                      statestack:base
-                  '';
-                });
-              in
-                project.hsPkgs.haskell-language-server.components.exes.haskell-language-server;
-          };
         };
       })
     ]
