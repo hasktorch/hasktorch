3ce378c263a69896785fe69cccfedad6fe354c5f
austinvhuang <austinh@alum.mit.edu>
2021-05-16 15:23:14 -0400

    WIP python interop

---
+++ examples/model-serving/04-python-interop/pymodel.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class Test(nn.Module):
+    def __init__(self):
+        super(Test, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+model = Test()
+
+print("state_dict:")
+for param_tensor in model.state_dict():
+    print(param_tensor, "\t", model.state_dict()[param_tensor].size())
