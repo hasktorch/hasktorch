e5ab252e85b5554de62f5decbb6dde9ef403151e
austinvhuang <austinh@alum.mit.edu>
2021-05-22 22:38:51 -0400

    state_dict and torchscript working

--- examples/model-serving/04-python-interop/Main.hs
+++ examples/model-serving/04-python-interop/Main.hs
@@ -4,23 +4,31 @@
 import Torch
 import Torch.Script as S
 
-loadFile file = do
-  print file
-  S.IVGenericDict vals <- pickleLoad file
-  print vals
-  pure vals
-
-modelFile = "test2sd.pt"
-
-main = do
-  vals <- loadFile modelFile
+testSimple = do
+  S.IVGenericDict vals <- pickleLoad "test2sd.pt"
   let (_, IVTensor weight) = vals !! 0
   let (_, IVTensor bias) = vals !! 1
   weight' <- makeIndependent weight
   bias' <- makeIndependent bias
-  -- init <- sample $ LinearSpec { in_features = 2, out_features = 1 }
   let model = Linear weight' bias'
   let x = asTensor [1.0 :: Float, 2.0]
   print model
   print $ linear model x
+
+main  = do
+  -- load parameters
+  S.IVGenericDict params <- pickleLoad "mnist.pt"
+
+  -- load example data
+  S.IVGenericDict example <- pickleLoad "mnist.example.pt"
+  let ivt = snd $ example !! 0
+  let S.IVTensor t = ivt
+
+  -- load torchscript
+  tsModule <- S.load WithoutRequiredGrad "mnist.ts.pt"
+  
+  -- compute forward function with torchscript
+  let result = S.forward tsModule [ivt] 
+  print result
+
   putStrLn "Done"
