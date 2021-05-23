802be3e5281f4cdbdbb1b6bc721ed73171a10caa
Torsten Scholak <torsten.scholak@googlemail.com>
2021-05-23 18:25:15 -0400

    fix tokenizers

--- hasktorch/src/Torch/GraduallyTyped/NN/Transformer/BART.hs
+++ hasktorch/src/Torch/GraduallyTyped/NN/Transformer/BART.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
@@ -7,10 +8,15 @@ module Torch.GraduallyTyped.NN.Transformer.BART
   ( module Torch.GraduallyTyped.NN.Transformer.BART.Common,
     module Torch.GraduallyTyped.NN.Transformer.BART.Base,
     testForwardBARTBase,
+    testBARTInput,
+    testBARTDecoderInput,
   )
 where
 
+import Data.List (maximumBy, sortBy)
+import Data.Ord (comparing, Down (..))
 import Test.HUnit.Approx (assertApproxEqual)
+import qualified Tokenizers (Tokenizer, decode, encode, getIDs, withTokenizerFromConfigFile)
 import Torch.GraduallyTyped.Device (Device (..), DeviceType (..))
 import Torch.GraduallyTyped.NN.Class (HasForward (..), HasInitialize (..))
 import Torch.GraduallyTyped.NN.Transformer.BART.Base
@@ -20,21 +26,36 @@ import Torch.GraduallyTyped.Shape.Type (Dim (..), Name (..), Size (..))
 import Torch.GraduallyTyped.Tensor.Type (Tensor (..))
 import qualified Torch.Tensor as Tensor (Tensor (..), asValue)
 
+-- https://github.com/hasktorch/tokenizers/blob/master/bindings/haskell/tokenizers-haskell/src/Tokenizers.hs
+-- https://github.com/hasktorch/tokenizers/blob/master/bindings/haskell/tokenizers-haskell/test/Spec.hs#L127
+-- https://github.com/hasktorch/tokenizers/blob/master/nix/rust.nix
+
+withTokenizer :: (Tokenizers.Tokenizer -> IO a) -> IO a
+withTokenizer =
+  Tokenizers.withTokenizerFromConfigFile
+    "/Users/tscholak/Projects/thirdParty/hasktorch/hasktorch/src/Torch/GraduallyTyped/NN/Transformer/bart-tokenizer.json"
+
 testBARTInput :: IO _
-testBARTInput =
-  mkBARTInput
-    @('Dim ('Name "*") ('Size 1))
-    @('Dim ('Name "*") ('Size 7))
-    [ [0, 31414, 127, 766, 16, 50264, 2]
-    ]
+testBARTInput = do
+  withTokenizer $ \tokenizer -> do
+    encoding <- Tokenizers.encode tokenizer "<s>Haskell: I<mask></s>"
+    ids <- Tokenizers.getIDs encoding
+    print ids
+    mkBARTInput
+      @('Dim ('Name "*") ('Size 1))
+      @('Dim ('Name "*") ('Size 6))
+      [ids]
 
 testBARTDecoderInput :: IO _
-testBARTDecoderInput =
-  mkBARTInput
-    @('Dim ('Name "*") ('Size 1))
-    @('Dim ('Name "*") ('Size 7))
-    [ [0, 31414, 127, 766, 16, 1573, 2]
-    ]
+testBARTDecoderInput = do
+  withTokenizer $ \tokenizer -> do
+    encoding <- Tokenizers.encode tokenizer "<s>Haskell: I"
+    ids <- Tokenizers.getIDs encoding
+    print ids
+    mkBARTInput
+      @('Dim ('Name "*") ('Size 1))
+      @('Dim ('Name "*") ('Size 5))
+      [ids]
 
 testForwardBARTBase :: IO ()
 testForwardBARTBase =
@@ -53,14 +74,28 @@ testForwardBARTBase =
           firstPositions <- take 3 firstBatch
           take 3 firstPositions
     print firstEncoderHiddenStates
-    let firstEncoderHiddenStates' = [-0.0324, 0.0121, -0.0036, 0.0885, 0.1154, -0.2264, 0.3947, 0.1037, 0.0503]
-    mapM_ (uncurry (assertApproxEqual "failed approximate equality check" 0.001)) $ zip firstEncoderHiddenStates firstEncoderHiddenStates'
+    -- let firstEncoderHiddenStates' = [-0.0324, 0.0121, -0.0036, 0.0885, 0.1154, -0.2264, 0.3947, 0.1037, 0.0503]
+    -- mapM_ (uncurry (assertApproxEqual "failed approximate equality check" 0.001)) $ zip firstEncoderHiddenStates firstEncoderHiddenStates'
     let decoderOutput = case bartDecoderOutput of
           UnsafeTensor t -> Tensor.asValue (Tensor.Unsafe t) :: [[[Float]]]
+    let lastLogits = do
+          firstBatch <- take 1 decoderOutput
+          let lastPosition = last firstBatch
+          fmap fst . sortBy (comparing $ Down . snd) $ zip [0 :: Int ..] lastPosition
+    withTokenizer $ \tokenizer -> do
+      stuff <-
+        traverse
+          ( \case
+              50108 -> pure "boom!"
+              idx -> Tokenizers.decode tokenizer . pure $ idx
+          )
+          (take 10 lastLogits)
+      print stuff
     let firstLogits = do
           firstBatch <- take 1 decoderOutput
           firstPositions <- take 3 firstBatch
           take 3 firstPositions
     print firstLogits
-    let firstLogits' = [33.9049, 6.7412, 17.0702, 7.3159, -2.1131, 17.2696, -5.6340, -5.8494, 6.4185]
-    mapM_ (uncurry (assertApproxEqual "failed approximate equality check" 0.001)) $ zip firstLogits firstLogits'
+
+-- let firstLogits' = [33.9049, 6.7412, 17.0702, 7.3159, -2.1131, 17.2696, -5.6340, -5.8494, 6.4185]
+-- mapM_ (uncurry (assertApproxEqual "failed approximate equality check" 0.001)) $ zip firstLogits firstLogits'
