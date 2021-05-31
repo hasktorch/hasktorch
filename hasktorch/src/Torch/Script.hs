c4962000448355172fc06319f2246ece800ced52
Junji Hashimoto <junji.hashimoto@gmail.com>
2021-05-31 10:21:34 +0900

    Fix duplicated definitions for both save and load

--- hasktorch/src/Torch/Script.hs
+++ hasktorch/src/Torch/Script.hs
@@ -138,11 +138,11 @@ instance Castable Graph (ForeignPtr (ATen.SharedPtr ATen.JitGraph)) where
 newModule :: String -> IO RawModule
 newModule = cast1 LibTorch.newModule
 
-save :: ScriptModule -> FilePath -> IO ()
-save = cast2 LibTorch.save
+saveScript :: ScriptModule -> FilePath -> IO ()
+saveScript = cast2 LibTorch.save
 
-save' :: RawModule -> FilePath -> IO ()
-save' = cast2 LibTorch.save
+saveScript' :: RawModule -> FilePath -> IO ()
+saveScript' = cast2 LibTorch.save
 
 data LoadMode
   = WithoutRequiredGrad
@@ -150,17 +150,17 @@ data LoadMode
   deriving (Show, Eq)
 
 -- | Load a torchscript file
-load :: LoadMode -> FilePath -> IO ScriptModule
-load WithoutRequiredGrad file = cast1 LibTorch.load file
-load WithRequiredGrad file = do
+loadScript :: LoadMode -> FilePath -> IO ScriptModule
+loadScript WithoutRequiredGrad file = cast1 LibTorch.load file
+loadScript WithRequiredGrad file = do
   module'@(UnsafeRawModule rmodule) <- cast1 LibTorch.load file
   params <- getParametersIO module'
   paramsWithRequiredGrad <- forM params makeIndependent
   setParameters module' (map toDependent paramsWithRequiredGrad)
   return (UnsafeScriptModule rmodule)
 
-load' :: FilePath -> IO RawModule
-load' = cast1 LibTorch.load
+loadScript' :: FilePath -> IO RawModule
+loadScript' = cast1 LibTorch.load
 
 instance HasForward ScriptModule [IValue] IValue where
   forward module' = unsafePerformIO . forwardStoch module'
