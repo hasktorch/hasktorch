1787c45dfb9276fe2a967dc04580c9b7407e19c6
Julius Marozas <marozas.julius@gmail.com>
2021-07-29 15:26:45 +0300

    dims -> getDims

--- experimental/gradually-typed/test/IndexingSpec.hs
+++ experimental/gradually-typed/test/IndexingSpec.hs
@@ -33,60 +33,60 @@ tensor = sReshape (SShape $ SName @"*" :&: SSize @2 :|: SName @"*" :&: SSize @2 
 spec :: Spec
 spec = describe "Indexing" $ do
   it "indexes a value" $ do
-    r <- tensor ! SIndices (SSliceAt (SIndex @1) :|: SSliceAt (SIndex @0) :|: SSliceAt (SIndex @2) :|: SNil)
+    x <- tensor ! SIndices (SSliceAt (SIndex @1) :|: SSliceAt (SIndex @0) :|: SSliceAt (SIndex @2) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` []
-    fromTensor @Int r `shouldBe` 8
+    dimSize <$> getDims x `shouldBe` []
+    fromTensor @Int x `shouldBe` 8
 
   it "slices a tensor" $ do
-    r <- tensor ! SIndices (SSliceAt (SIndex @1) :|: SSliceAt (SIndex @0) :|: SNil)
+    x <- tensor ! SIndices (SSliceAt (SIndex @1) :|: SSliceAt (SIndex @0) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [3]
-    fromTensor @(Int, Int, Int) r `shouldBe` (6, 7, 8)
+    dimSize <$> getDims x `shouldBe` [3]
+    fromTensor @(Int, Int, Int) x `shouldBe` (6, 7, 8)
 
   it "slices only the last dimension" $ do
-    r <- tensor ! SIndices (SSliceAll :|: SSliceAll :|: SSliceAt (SIndex @1) :|: SNil)
+    x <- tensor ! SIndices (SSliceAll :|: SSliceAll :|: SSliceAt (SIndex @1) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [2, 2]
-    fromTensor @((Int, Int), (Int, Int)) r `shouldBe` ((1, 4), (7, 10))
+    dimSize <$> getDims x `shouldBe` [2, 2]
+    fromTensor @((Int, Int), (Int, Int)) x `shouldBe` ((1, 4), (7, 10))
 
   it "slices with ellipsis" $ do
-    r <- tensor ! SIndices (SEllipsis :|: SSliceAt (SIndex @1) :|: SNil)
+    x <- tensor ! SIndices (SEllipsis :|: SSliceAt (SIndex @1) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [2, 2]
-    fromTensor @((Int, Int), (Int, Int)) r `shouldBe` ((1, 4), (7, 10))
+    dimSize <$> getDims x `shouldBe` [2, 2]
+    fromTensor @((Int, Int), (Int, Int)) x `shouldBe` ((1, 4), (7, 10))
 
   it "slices with multiple ellipses" $ do
-    r <- tensor ! SIndices (SEllipsis :|: SSliceAt (SIndex @2) :|: SEllipsis :|: SNil)
+    x <- tensor ! SIndices (SEllipsis :|: SSliceAt (SIndex @2) :|: SEllipsis :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [2, 2]
-    fromTensor @((Int, Int), (Int, Int)) r `shouldBe` ((2, 5), (8, 11))
+    dimSize <$> getDims x `shouldBe` [2, 2]
+    fromTensor @((Int, Int), (Int, Int)) x `shouldBe` ((2, 5), (8, 11))
 
   it "slices with SSliceFrom" $ do
-    r <- tensor ! SIndices (SSliceAll :|: SSliceFrom (SIndex @1) :|: SNil)
+    x <- tensor ! SIndices (SSliceAll :|: SSliceFrom (SIndex @1) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [2, 1, 3]
-    fromTensor @(SV.Vector 2 (SV.Vector 1 (SV.Vector 3 Int))) r
+    dimSize <$> getDims x `shouldBe` [2, 1, 3]
+    fromTensor @(SV.Vector 2 (SV.Vector 1 (SV.Vector 3 Int))) x
       `shouldBe` SV.fromTuple
         ( SV.singleton $ SV.fromTuple (3, 4, 5),
           SV.singleton $ SV.fromTuple (9, 10, 11)
         )
 
   it "slices with SliceFromUpTo" $ do
-    r <- tensor ! SIndices (SSliceAll :|: SSliceFrom (SIndex @1) :|: SSliceFromUpTo (SIndex @0) (SIndex @1) :|: SNil)
+    x <- tensor ! SIndices (SSliceAll :|: SSliceFrom (SIndex @1) :|: SSliceFromUpTo (SIndex @0) (SIndex @1) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [2, 1, 1]
-    fromTensor @(SV.Vector 2 (SV.Vector 1 (SV.Vector 1 Int))) r
+    dimSize <$> getDims x `shouldBe` [2, 1, 1]
+    fromTensor @(SV.Vector 2 (SV.Vector 1 (SV.Vector 1 Int))) x
       `shouldBe` SV.fromTuple
         ( SV.singleton $ SV.singleton 3,
           SV.singleton $ SV.singleton 9
         )
 
   it "slices with SliceFromUpToWithStride" $ do
-    r <- tensor ! SIndices (SSliceAll :|: SSliceFrom (SIndex @1) :|: SSliceFromUpToWithStride (SIndex @0) (SIndex @1) (SIndex @2) :|: SNil)
+    x <- tensor ! SIndices (SSliceAll :|: SSliceFrom (SIndex @1) :|: SSliceFromUpToWithStride (SIndex @0) (SIndex @1) (SIndex @2) :|: SNil)
 
-    dimSize <$> dims r `shouldBe` [2, 1, 1]
-    fromTensor @(SV.Vector 2 (SV.Vector 1 (SV.Vector 1 Int))) r
+    dimSize <$> getDims x `shouldBe` [2, 1, 1]
+    fromTensor @(SV.Vector 2 (SV.Vector 1 (SV.Vector 1 Int))) x
       `shouldBe` SV.fromTuple
         ( SV.singleton $ SV.singleton 3,
           SV.singleton $ SV.singleton 9
