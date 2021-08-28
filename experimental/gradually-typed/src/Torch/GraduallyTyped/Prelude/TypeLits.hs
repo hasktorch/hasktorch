04ac5327add144b72361c368c59997e5ce0bc6b7
Matt Wittmann <mcwitt@gmail.com>
2021-08-28 14:12:02 -0700

    Fix typo

--- experimental/gradually-typed/src/Torch/GraduallyTyped/Prelude/TypeLits.hs
+++ experimental/gradually-typed/src/Torch/GraduallyTyped/Prelude/TypeLits.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE CPP #-}
 
 #if MIN_VERSION_singletons(3,0,0)
-module Torch.GraduallyTyped.Prelude.TypeLits (module Data.TypeLits.Singletons) where
+module Torch.GraduallyTyped.Prelude.TypeLits (module GHC.TypeLits.Singletons) where
 import GHC.TypeLits.Singletons
 #else
 module Torch.GraduallyTyped.Prelude.TypeLits (module Data.Singletons.TypeLits) where
