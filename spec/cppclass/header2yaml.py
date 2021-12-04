0b3ce2fe429371402807490b95e801c70ef2d589
Junji Hashimoto <junji.hashimoto@gmail.com>
2021-12-05 02:59:54 +0900

    Update

--- spec/cppclass/header2yaml.py
+++ spec/cppclass/header2yaml.py
@@ -9,6 +9,7 @@ from clang.cindex import Config, Index, TranslationUnit
 
 header = sys.argv[1]
 namespace = sys.argv[2]
+classname = sys.argv[3]
 ns = ''
 cl = ''
 
@@ -18,23 +19,24 @@ def dump(cursor, indent=0):
     global ns,cl
     if cursor.kind.name == 'NAMESPACE' :
             ns = cursor.spelling;
-    if cursor.kind.name == 'CLASS_DECL' :
+    if cursor.kind.name == 'CLASS_DECL' and ns == namespace:
             cl = cursor.spelling;
-            print('signature: ' + cl)
-            print('cppname: ' + ns + '::' + cl)
-            print('hsname: ' + cl)
-            print('headers:')
-            print('- ' + header)
-            print('functions: []')
-            print('constructors:')
-            print('methods:')
+            if classname == cl: 
+                print('signature: ' + cl)
+                print('cppname: ' + ns + '::' + cl)
+                print('hsname: ' + cl)
+                print('headers:')
+                print('- ' + header)
+                print('functions: []')
+                print('constructors:')
+                print('methods:')
     
-    if cursor.kind.name == 'CXX_METHOD' and ns == namespace:
+    if cursor.kind.name == 'CXX_METHOD' and ns == namespace and cl == classname:
             print('- name: ' + cursor.spelling)
             ts = cursor.type.spelling
             pos = ts.find('(')
             print('- ' + cursor.spelling + ' ' + ts[pos:] + ' -> ' + ts[0:pos-1])
-    if cursor.kind.name == 'FUNCTION_DECL' and ns == namespace:
+    if cursor.kind.name == 'FUNCTION_DECL' and ns == namespace and cl == classname:
             print('- name: ' + cursor.spelling)
             ts = cursor.type.spelling
             pos = ts.find('(')
