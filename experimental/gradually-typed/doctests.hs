9f115b32b4815683ff305cb3a89ccb638a876842
Junji Hashimoto <junji.hashimoto@gmail.com>
2022-10-22 03:30:24 +0900

    Fix git-hashes of external libraries

--- experimental/gradually-typed/doctests.hs
+++ experimental/gradually-typed/doctests.hs
@@ -27,8 +27,7 @@ main = do
               "-fplugin GHC.TypeLits.Normalise",
               "-fplugin GHC.TypeLits.KnownNat.Solver",
               "-fplugin GHC.TypeLits.Extra.Solver",
-              "-fconstraint-solver-iterations=0",
-              "-package hasktorch-gradually-typed"
+              "-fconstraint-solver-iterations=0"
             ],
             module_sources
           ]
