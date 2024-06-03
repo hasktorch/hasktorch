e40dcd28bf0fb8724f351ead509f92601c70f9b4
Junji Hashimoto <junji.hashimoto@gree.net>
2024-06-03 09:05:00 +0900

    Fix the hostname of mnist

--- examples/datasets/download-mnist.sh
+++ examples/datasets/download-mnist.sh
@@ -2,9 +2,11 @@
 
 mkdir mnist
 pushd mnist
-wget http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
-wget http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+#MNIST_HOST=http://yann.lecun.com/exdb/mnist
+MNIST_HOST=https://ossci-datasets.s3.amazonaws.com/mnist
+wget $MNIST_HOST/train-images-idx3-ubyte.gz
+wget $MNIST_HOST/train-labels-idx1-ubyte.gz
+wget $MNIST_HOST/t10k-images-idx3-ubyte.gz
+wget $MNIST_HOST/t10k-labels-idx1-ubyte.gz
 popd
 
